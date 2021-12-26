b6290f37652ace84902b46ca6a98725ada2e6361
0xlain <basedlainpilled@gmail.com>
2021-12-25 21:46:19 -0500

    update login page

--- apps/site/pages/home/login.tsx
+++ apps/site/pages/home/login.tsx
@@ -1,20 +1,18 @@
 import { useContext, useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import {
   Alert,
   Button,
+  Divider,
   Grid,
-  IconButton,
-  InputAdornment,
   Snackbar,
   TextField,
-  Tooltip,
   Typography,
 } from "@mui/material";
-import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
-import Link from "next/link";
-import { useRouter } from "next/router";
 
 import { MatrixContext } from "../../src/matrix/MatrixProvider";
+import HomeserverDialog from "../../src/components/HomeserverDialog";
 
 export default function Login() {
   const router = useRouter();
@@ -26,6 +24,8 @@ export default function Login() {
   const [password, setPassword] = useState("");
   const [openError, setOpenError] = useState(false);
   const [error, setError] = useState("");
+
+  const [openHomeserver, setOpenHomeserver] = useState(false);
 
   function handleErrorClose() {
     setOpenError(false);
@@ -43,99 +43,124 @@ export default function Login() {
     }
   }
 
+  function handleOpenHomeserver() {
+    setOpenHomeserver(true);
+  }
+
+  function handleCloseHomeserver() {
+    setOpenHomeserver(false);
+  }
+
   return (
-    <Grid container>
-      <Grid
-        className="container underNavbar"
-        item
-        sm={8}
-        xl={5}
-        container
-        direction="column"
-        rowSpacing={4}
-      >
-        <Grid item>
-          <Typography variant="h2">Login</Typography>
-        </Grid>
+    <div>
+      <HomeserverDialog
+        open={openHomeserver}
+        handleClose={handleCloseHomeserver}
+        setHomeserver={setHomeserver}
+      />
 
-        <Grid item container direction="column" rowSpacing={2}>
-          <Grid item>
-            <TextField
-              label="Homeserver"
-              style={{ width: "100%" }}
-              value={homeserver}
-              onChange={(e) => setHomeserver(e.target.value)}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <Tooltip title="Learn More" placement="right">
-                      <IconButton
-                        href="https://the-wired.gitbook.io/the-wired/guides/logging-in"
-                        target="_blank"
-                      >
-                        <HelpOutlineIcon />
-                      </IconButton>
-                    </Tooltip>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid item>
-            <TextField
-              label="Username"
-              style={{ width: "100%" }}
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </Grid>
-          <Grid item>
-            <TextField
-              label="Password"
-              type="password"
-              style={{ width: "100%" }}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Grid>
-        </Grid>
-
-        <Grid item>
-          <Button
-            variant="contained"
-            onClick={handleLogin}
-            style={{ width: "100%" }}
-          >
-            Log In
-          </Button>
-        </Grid>
-
-        <Grid item xs container alignItems="baseline" columnSpacing={0.5}>
-          <Grid item>
-            <Typography variant="body2">New?</Typography>
-          </Grid>
-          <Grid item>
-            <Typography variant="body2" className="link" color="secondary">
-              <Link href="/home/register">Create account</Link>
-            </Typography>
-          </Grid>
-        </Grid>
-
-        <Snackbar
-          open={openError}
-          autoHideDuration={6000}
-          onClose={handleErrorClose}
-          anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+      <Grid container>
+        <Grid
+          className="container underNavbar"
+          item
+          sm={8}
+          xl={5}
+          container
+          direction="column"
+          rowSpacing={4}
         >
-          <Alert
+          <Grid item container direction="column" rowSpacing={1}>
+            <Grid item>
+              <Typography variant="h4">Sign in</Typography>
+            </Grid>
+
+            <Grid item>
+              <Typography variant="h6">Homeserver</Typography>
+            </Grid>
+
+            <Grid
+              item
+              container
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              <Grid item>
+                <Typography>{homeserver}</Typography>
+              </Grid>
+              <Grid item>
+                <Typography
+                  onClick={handleOpenHomeserver}
+                  className="link"
+                  color="secondary"
+                >
+                  Edit
+                </Typography>
+              </Grid>
+            </Grid>
+
+            <Grid item>
+              <Divider />
+            </Grid>
+          </Grid>
+
+          <Grid item container direction="column" rowSpacing={2}>
+            <Grid item>
+              <TextField
+                label="Username"
+                style={{ width: "100%" }}
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </Grid>
+
+            <Grid item>
+              <TextField
+                label="Password"
+                type="password"
+                style={{ width: "100%" }}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid item>
+            <Button
+              variant="contained"
+              onClick={handleLogin}
+              style={{ width: "100%" }}
+            >
+              Sign in
+            </Button>
+          </Grid>
+
+          <Grid item xs container alignItems="baseline" columnSpacing={0.5}>
+            <Grid item>
+              <Typography variant="body2">New?</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body2" className="link" color="secondary">
+                <Link href="/home/register">Create account</Link>
+              </Typography>
+            </Grid>
+          </Grid>
+
+          <Snackbar
+            open={openError}
+            autoHideDuration={6000}
             onClose={handleErrorClose}
-            severity="error"
-            sx={{ width: "100%" }}
+            anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
           >
-            {error}
-          </Alert>
-        </Snackbar>
+            <Alert
+              onClose={handleErrorClose}
+              severity="error"
+              sx={{ width: "100%" }}
+            >
+              {error}
+            </Alert>
+          </Snackbar>
+        </Grid>
       </Grid>
-    </Grid>
+    </div>
   );
 }

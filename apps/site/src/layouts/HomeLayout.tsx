a0020ad74ffeb69a12521aed2e00e0b33e2f5da9
0xlain <basedlainpilled@gmail.com>
2022-01-09 18:55:51 -0500

    update page titles

--- apps/site/src/layouts/HomeLayout.tsx
+++ apps/site/src/layouts/HomeLayout.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Head from "next/head";
+
 import HomeNavbar from "../components/HomeNavbar";
 
 export default function HomeLayout({ children }) {
   return (
     <div>
+      <Head>
+        <title>The Wired - Home</title>
+      </Head>
+
       <HomeNavbar />
       {children}
     </div>

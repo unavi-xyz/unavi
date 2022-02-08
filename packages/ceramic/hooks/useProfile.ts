4234d65dfac40fbf2b742a46f6e07265a3b4fd61
0xlain <basedlainpilled@gmail.com>
2022-02-07 20:15:52 -0500

    fix error

--- packages/ceramic/hooks/useProfile.ts
+++ packages/ceramic/hooks/useProfile.ts
@@ -5,23 +5,23 @@ import { BasicProfile, ceramic, ceramicRead } from "..";
 const model = require("../models/BasicProfile/model.json");
 
 export function useProfile(did: string | undefined) {
-  async function fetcher() {
-    if (!did) return;
-    const store = new DIDDataStore({ ceramic: ceramicRead, model });
-    const data = (await store.get("basicProfile", did)) as BasicProfile;
-
-    return data;
-  }
-
-  const { data } = useSWR(`basicProfile-${did}`, fetcher);
-
   async function merge(data: any) {
     const store = new DIDDataStore({ ceramic, model });
     await store.merge("basicProfile", data, { pin: true });
   }
 
-  const imageHash = data?.image?.original.src.replace("ipfs://", "");
-  const imageUrl = `https://ipfs.infura.io:5001/api/v0/cat?arg=${imageHash}`;
+  async function fetcher() {
+    if (!did) return;
+    const store = new DIDDataStore({ ceramic: ceramicRead, model });
+    const data = (await store.get("basicProfile", did)) as BasicProfile;
 
-  return { profile: data, imageUrl, merge };
+    const imageHash = data?.image?.original.src.replace("ipfs://", "");
+    const imageUrl = `https://ipfs.infura.io:5001/api/v0/cat?arg=${imageHash}`;
+
+    return { profile: data, imageUrl };
+  }
+
+  const { data } = useSWR(`basicProfile-${did}`, fetcher);
+
+  return { profile: data?.profile, imageUrl: data?.imageUrl, merge };
 }

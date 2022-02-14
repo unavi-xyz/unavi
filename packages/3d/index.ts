de1285cb4674dc9671323e44e2b948f653b44bb1
0xlain <basedlainpilled@gmail.com>
2022-02-14 00:44:09 -0500

    folder rename

--- packages/3d/index.ts
+++ packages/3d/index.ts
@@ -1,15 +1,15 @@
+export * from "./assets/classes/Asset";
+export * from "./assets/classes/EditorObject";
+export * from "./assets/classes/SceneObject";
+export * from "./assets/InstancedObject";
+export * from "./assets/assets";
+export * from "./assets/packs";
+
 export * from "./components/multiplayer/Multiplayer";
 export * from "./components/player/Player";
 export * from "./components/scene/Scene";
 export * from "./components/scene/Ground";
 
 export * from "./contexts/MultiplayerContext";
-
-export * from "./packs/classes/Asset";
-export * from "./packs/classes/EditorObject";
-export * from "./packs/classes/SceneObject";
-export * from "./packs/InstancedObject";
-export * from "./packs/assets";
-export * from "./packs/packs";
 
 export * from "./constants";

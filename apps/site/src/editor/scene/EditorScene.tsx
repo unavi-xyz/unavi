969e2f254a054c69df138a5f366e0c4543a2a96e
0xlain <basedlainpilled@gmail.com>
2022-02-16 01:50:31 -0500

    remove selected on Ground click

--- apps/site/src/editor/scene/EditorScene.tsx
+++ apps/site/src/editor/scene/EditorScene.tsx
@@ -3,22 +3,30 @@ import { useThree } from "@react-three/fiber";
 import { OrbitControls, Sky } from "@react-three/drei";
 import { Ground } from "3d";
 
+import { useStore } from "../hooks/useStore";
 import EditorObjects from "./EditorObjects";
 import Gizmo from "./Gizmo";
 
 export default function EditorScene() {
+  const setSelected = useStore((state) => state.setSelected);
+
   const { camera } = useThree();
 
   useEffect(() => {
     camera.position.set(10, 10, 10);
   }, [camera]);
 
+  function handleClick(e) {
+    e.stopPropagation();
+    setSelected(null);
+  }
+
   return (
     <group>
       <ambientLight intensity={0.1} />
       <directionalLight intensity={0.5} />
       <Sky />
-      <group onClick={(e) => e.stopPropagation()}>
+      <group onClick={handleClick}>
         <Ground />
       </group>
 

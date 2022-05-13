73c187fce612f7a069c6b7d99299c76786ee123b
0xlain <basedlainpilled@gmail.com>
2022-05-13 17:35:31 -0400

    delete old files

--- apps/client/src/app/AppOverlay/SidePanel/helpers/atoms.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from "jotai";
-import { Page } from "./types";
-
-export const pageAtom = atom<Page>("Space");
-export const spaceAtom = atom<string>("");
-export const userAtom = atom<string>("");

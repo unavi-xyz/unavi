3e8cd6a8fd9b17300157ecb236eba802bdb82874
0xlain <basedlainpilled@gmail.com>
2022-05-13 20:39:38 -0400

    add chat

--- apps/client/src/helpers/app/store.ts
+++ apps/client/src/helpers/app/store.ts
@@ -1,16 +1,19 @@
+import { RefObject, createRef } from "react";
 import create from "zustand";
 
 import { Message } from "./types";
 
 export interface AppStore {
+  chatInputRef: RefObject<HTMLInputElement>;
   messages: Message[];
   addMessage: (message: Message) => void;
 }
 
 export const useAppStore = create<AppStore>((set, get) => ({
+  chatInputRef: createRef(),
   messages: [],
   addMessage: (message: Message) => {
     const messages = get().messages;
-    set({ messages: [...messages, message] });
+    set({ messages: [message, ...messages] });
   },
 }));

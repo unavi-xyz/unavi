5be089d6b23b48690d7ece7393d6ebf150533e59
0xlain <basedlainpilled@gmail.com>
2022-06-02 07:24:30 -0400

    update shadow params

--- packages/scene/src/components/entities/DirectionalLight.tsx
+++ packages/scene/src/components/entities/DirectionalLight.tsx
@@ -7,5 +7,17 @@ export function DirectionalLight({
   color = "#ffffff",
   intensity = 1,
 }: DirectionalLightProps) {
-  return <directionalLight castShadow args={[color, intensity]} />;
+  return (
+    <directionalLight
+      castShadow
+      args={[color, intensity]}
+      shadow-mapSize-width={8192}
+      shadow-mapSize-height={8192}
+      shadow-camera-far={100}
+      shadow-camera-left={-30}
+      shadow-camera-right={30}
+      shadow-camera-top={30}
+      shadow-camera-bottom={-30}
+    />
+  );
 }

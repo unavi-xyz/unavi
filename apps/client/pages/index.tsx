1e20bba9603a73fc637cf181dde3e6c6d7103bfc
0xlain <basedlainpilled@gmail.com>
2022-06-02 20:20:13 -0400

    add social media links to welcome page

--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -1,4 +1,8 @@
+import { FaDiscord } from "react-icons/fa";
+import { VscGithubInverted, VscTwitter } from "react-icons/vsc";
+
 import { getNavbarLayout } from "../src/components/layouts/NavbarLayout/NavbarLayout";
+import { DISCORD_URL, GITHUB_URL, TWITTER_URL } from "../src/helpers/constants";
 
 export default function Index() {
   return (
@@ -6,10 +10,46 @@ export default function Index() {
       <div className="max-w space-y-8">
         <div
           className="flex flex-col justify-center items-center h-72 rounded-3xl
-                   bg-primaryContainer text-onPrimaryContainer"
+                   bg-primaryContainer text-onPrimaryContainer space-y-4"
         >
-          <div className="text-4xl font-black text-center">
-            Welcome to The Wired 👋
+          <div className="text-4xl font-black">Welcome to The Wired 👋</div>
+
+          <div className="flex space-x-2">
+            <a href={TWITTER_URL} target="_blank" rel="noreferrer">
+              <div
+                className="rounded-full hover:ring-1 hover:ring-onPrimaryContainer px-3 py-0.5
+                           cursor-pointer select-none transition"
+              >
+                <div className="flex items-center space-x-2">
+                  <VscTwitter />
+                  <div>Twitter</div>
+                </div>
+              </div>
+            </a>
+
+            <a href={DISCORD_URL} target="_blank" rel="noreferrer">
+              <div
+                className="rounded-full hover:ring-1 hover:ring-onPrimaryContainer px-3 py-0.5
+                           cursor-pointer select-none transition"
+              >
+                <div className="flex items-center space-x-2">
+                  <FaDiscord />
+                  <div>Discord</div>
+                </div>
+              </div>
+            </a>
+
+            <a href={GITHUB_URL} target="_blank" rel="noreferrer">
+              <div
+                className="rounded-full hover:ring-1 hover:ring-onPrimaryContainer px-3 py-0.5
+                           cursor-pointer select-none transition"
+              >
+                <div className="flex items-center space-x-2">
+                  <VscGithubInverted />
+                  <div>GitHub</div>
+                </div>
+              </div>
+            </a>
           </div>
         </div>
 

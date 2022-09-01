91198ae5a87307f8ab0a3f93f8a931401fb93000
0xlain <basedlainpilled@gmail.com>
2022-09-01 18:33:25 -0400

    fix trpc

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -96,15 +96,20 @@ function App({ Component, pageProps: { session, ...pageProps } }: any) {
 
 // tRPC router
 export default withTRPC<AppRouter>({
-  config() {
-    const url =
-      process.env.NODE_ENV === "development"
-        ? "http://localhost:3000/api/trpc"
-        : `https://${process.env.VERCEL_URL}/api/trpc`;
+  config({ ctx }) {
+    const url = `${getBaseUrl()}/api/trpc`;
 
     return {
       url,
       queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
     };
+
+    ssr: true;
   },
 })(App);
+
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  return `http://localhost:3000`; // dev SSR should use localhost
+};

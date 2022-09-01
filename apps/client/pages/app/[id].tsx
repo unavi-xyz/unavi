7e7c4c72f352aef98f405f8b7522f8b0b96871d3
0xlain <basedlainpilled@gmail.com>
2022-09-01 12:42:17 -0400

    fix build

--- apps/client/pages/app/[id].tsx
+++ apps/client/pages/app/[id].tsx
@@ -31,28 +31,28 @@ interface Props extends PublicationProps {
 }
 
 export default function App({ id, metadata, publication }: Props) {
-  const ownerHost = publication?.profile.attributes?.find(
-    (item) => item.key === "host"
-  )?.value;
+  // const ownerHost = publication?.profile.attributes?.find(
+  //   (item) => item.key === "host"
+  // )?.value;
 
-  const host =
-    process.env.NODE_ENV === "development"
-      ? "ws://localhost:4000"
-      : ownerHost
-      ? `wss://${ownerHost}`
-      : DEFAULT_HOST;
+  // const host =
+  //   process.env.NODE_ENV === "development"
+  //     ? "ws://localhost:4000"
+  //     : ownerHost
+  //     ? `wss://${ownerHost}`
+  //     : DEFAULT_HOST;
 
-  useAppHotkeys();
-  useSetIdentity();
+  // useAppHotkeys();
+  // useSetIdentity();
 
-  useEffect(() => {
-    //send an analytics event when the user joins the room
-    //so we can show popular spaces on the explore page
-    //idk if this is a good way to do it but it works for now
-    if (process.env.NODE_ENV === "production") {
-      // fetch(`/api/space/${id}/add-view`);
-    }
-  }, [id]);
+  // useEffect(() => {
+  //   //send an analytics event when the user joins the room
+  //   //so we can show popular spaces on the explore page
+  //   //idk if this is a good way to do it but it works for now
+  //   if (process.env.NODE_ENV === "production") {
+  //     // fetch(`/api/space/${id}/add-view`);
+  //   }
+  // }, [id]);
 
   return (
     <>

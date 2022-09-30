30f717a77c96d92c224392321a932ea9d78b76a0
Lean <basedlainpilled@gmail.com>
2022-09-30 18:05:01 -0400

    fix wallets

--- apps/client/src/auth/theme.ts
+++ apps/client/src/auth/theme.ts
@@ -1,9 +1,12 @@
 import { lightTheme, Theme } from "@rainbow-me/rainbowkit";
+import { ThemeOptions } from "@rainbow-me/rainbowkit/dist/themes/baseTheme";
 
-export const theme: Theme = lightTheme({
+const options: ThemeOptions = {
   accentColor: "#52DAFF",
   accentColorForeground: "#000000",
   fontStack: "system",
   borderRadius: "large",
   overlayBlur: "small",
-});
+};
+
+export const theme: Theme = lightTheme(options);

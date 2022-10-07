d11dfc65625a3a5a8ad304b1f0633633a321bac8
Lean <basedlainpilled@gmail.com>
2022-10-07 06:20:15 -0400

    fix host dev mode

--- apps/host/src/server.ts
+++ apps/host/src/server.ts
@@ -6,14 +6,18 @@ import { Players } from "./Players";
 const PORT = parseInt(process.env.PORT || "4000");
 const textDecoder = new TextDecoder();
 
+const cert_file_name = process.env.SSL_CERT;
+const key_file_name = process.env.SSL_KEY;
+
 // Create WebSocket server
+// Use SSL if cert and key are provided
 const server =
-  process.env.NODE_ENV === "development"
-    ? uWS.App()
-    : uWS.SSLApp({
+  cert_file_name && key_file_name
+    ? uWS.SSLApp({
         key_file_name: process.env.SSL_KEY,
         cert_file_name: process.env.SSL_CERT,
-      });
+      })
+    : uWS.App();
 
 // Create player manager
 const players = new Players(server);

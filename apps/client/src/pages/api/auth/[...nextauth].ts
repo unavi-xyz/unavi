c25749bf222ece6c917a8f6d2740a8945bb05965
Lean <basedlainpilled@gmail.com>
2022-10-09 08:35:04 -0400

    better JWT refreshing / handling

--- apps/client/src/pages/api/auth/[...nextauth].ts
+++ apps/client/src/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,4 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import {
   RefreshDocument,
   RefreshMutation,
@@ -9,51 +10,62 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { env } from "../../../env/server.mjs";
 import { authenticate, verifyJWT } from "../../../server/jwt";
 import { lensClient } from "../../../server/lens";
+import { prisma } from "../../../server/prisma";
 import { parseJWT } from "../../../utils/parseJWT";
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   callbacks: {
-    jwt({ token, user }) {
+    async jwt({ token, user }) {
+      // Initial sign in
       if (user) {
-        token.accessToken = user.accessToken;
-        token.refreshToken = user.refreshToken;
-        user.accessToken = undefined;
-        user.refreshToken = undefined;
+        return {
+          address: user.id,
+          accessToken: user.accessToken,
+          refreshToken: user.refreshToken,
+        };
       }
+
+      const { exp } = parseJWT(token.accessToken as string);
+      const currentTime = Math.floor(Date.now() / 1000);
+      const minutes = (exp - currentTime) / 60;
+
+      // Refresh token lasts 7 days
+      const refreshExpired = exp + 60 * 60 * 24 * 7 < currentTime;
+
+      // Log user out if refresh token is expired
+      if (refreshExpired) {
+        token.address = null;
+        token.accessToken = null;
+        token.refreshToken = null;
+      }
+
+      // Refresh JWT token if it expires soon
+      if (minutes < 10) {
+        const { data, error } = await lensClient
+          .mutation<RefreshMutation, RefreshMutationVariables>(
+            RefreshDocument,
+            {
+              request: {
+                refreshToken: token.refreshToken,
+              },
+            }
+          )
+          .toPromise();
+
+        if (error) throw error;
+
+        if (data) {
+          token.accessToken = data.refresh.accessToken;
+          token.refreshToken = data.refresh.refreshToken;
+        }
+      }
+
       return token;
     },
-    async session({ session, token }) {
-      const { exp } = parseJWT(token.accessToken as string);
-      const seconds = exp - Math.floor(Date.now() / 1000);
-      const minutes = seconds / 60;
-
-      // If token has expired, user needs to re-authenticate
-      if (minutes < 0) {
-        session.user = undefined;
-        session.address = undefined;
-        session.accessToken = undefined;
-        return session;
-      }
-
-      // Refresh lens JWT token
-      const { data, error } = await lensClient
-        .mutation<RefreshMutation, RefreshMutationVariables>(RefreshDocument, {
-          request: {
-            refreshToken: token.refreshToken,
-          },
-        })
-        .toPromise();
-
-      if (error) throw error;
-
-      if (data) {
-        token.accessToken = data.refresh.accessToken;
-        token.refreshToken = data.refresh.refreshToken;
-      }
-
-      session.address = token.sub;
+    session({ session, token }) {
+      session.address = token.address;
       session.accessToken = token.accessToken;
-      session.user = { name: token.sub };
       return session;
     },
   },

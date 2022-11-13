3df7a370d80f021382bae39055c78b10ef8c3f1e
Lean <basedlainpilled@gmail.com>
2022-11-13 04:57:45 -0500

    gltf mesh menu styling

--- apps/client/src/editor/components/InspectMenu/mesh/GLTFMeshComponent.tsx
+++ apps/client/src/editor/components/InspectMenu/mesh/GLTFMeshComponent.tsx
@@ -4,6 +4,7 @@ import FileInput from "../../../../ui/FileInput";
 import { updateMesh } from "../../../actions/UpdateMeshAction";
 import { useSubscribeValue } from "../../../hooks/useSubscribeValue";
 import { updateGltfColliders } from "../../../utils/updateGltfColliders";
+import MenuRows from "../MenuRows";
 
 interface Props {
   nodeId: string;
@@ -14,28 +15,30 @@ export default function GLTFMeshComponent({ nodeId, mesh }: Props) {
   const name = useSubscribeValue(mesh.name$);
 
   return (
-    <FileInput
-      displayName={name}
-      accept=".glb,.gltf"
-      onChange={(e) => {
-        if (!e.target.files) return;
+    <MenuRows titles={["Model"]}>
+      <FileInput
+        displayName={name}
+        accept=".glb,.gltf"
+        onChange={(e) => {
+          if (!e.target.files) return;
 
-        const file = e.target.files[0];
-        if (!file) return;
+          const file = e.target.files[0];
+          if (!file) return;
 
-        const isGlb = file.name.endsWith(".glb");
-        const type = isGlb ? "model/gltf-binary" : "model/gltf+json";
-        const blob = new Blob([file], { type });
+          const isGlb = file.name.endsWith(".glb");
+          const type = isGlb ? "model/gltf-binary" : "model/gltf+json";
+          const blob = new Blob([file], { type });
 
-        updateMesh(mesh.id, {
-          name: file.name,
-          uri: URL.createObjectURL(blob),
-        });
+          updateMesh(mesh.id, {
+            name: file.name,
+            uri: URL.createObjectURL(blob),
+          });
 
-        setTimeout(() => {
-          updateGltfColliders(nodeId);
-        }, 1000);
-      }}
-    />
+          setTimeout(() => {
+            updateGltfColliders(nodeId);
+          }, 1000);
+        }}
+      />
+    </MenuRows>
   );
 }

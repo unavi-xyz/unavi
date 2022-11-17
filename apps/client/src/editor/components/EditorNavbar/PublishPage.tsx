adc34ff2e860c1e809028a6b0b3c93930da09dec
Lean <basedlainpilled@gmail.com>
2022-11-16 22:30:10 -0500

    styling

--- apps/client/src/editor/components/EditorNavbar/PublishPage.tsx
+++ apps/client/src/editor/components/EditorNavbar/PublishPage.tsx
@@ -321,6 +321,7 @@ export default function PublishPage() {
           autoComplete="off"
           title="Description"
           outline
+          rows={4}
           defaultValue={description}
         />
 

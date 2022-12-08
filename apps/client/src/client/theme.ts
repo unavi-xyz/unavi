129cf4d60a2f86371694bb561b80607f729a63b2
Lean <basedlainpilled@gmail.com>
2022-12-07 19:41:47 -0500

    update rainbowkit theme

--- apps/client/src/client/theme.ts
+++ apps/client/src/client/theme.ts
@@ -2,7 +2,7 @@ import { lightTheme, Theme } from "@rainbow-me/rainbowkit";
 import { ThemeOptions } from "@rainbow-me/rainbowkit/dist/themes/baseTheme";
 
 const options: ThemeOptions = {
-  accentColor: "#38bdf8",
+  accentColor: "#7dd3fc",
   accentColorForeground: "#000000",
   fontStack: "system",
   borderRadius: "large",

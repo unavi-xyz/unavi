f2782612f7338268c05a8914ede846a06f8ce545
Lean <basedlainpilled@gmail.com>
2022-12-18 04:18:08 -0500

    set up new authentication / profile system

--- apps/client/src/constants.ts
+++ apps/client/src/constants.ts
@@ -2,3 +2,5 @@ export const DISCORD_URL = "https://discord.gg/cazUfCCgHJ";
 export const TWITTER_URL = "https://twitter.com/TheWiredXR";
 export const GITHUB_URL = "https://github.com/wired-labs/wired";
 export const DOCS_URL = "https://docs.thewired.space";
+
+export const PROFILE_ADDRESS = "0xF758c4893667ac69F16134a66F19AED8827B1914";

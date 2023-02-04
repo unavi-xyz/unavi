e7311276331aab9f4f5bb9a266fc1ce42a9edb2c
Lean <basedlainpilled@gmail.com>
2023-02-04 12:56:01 -0500

    add import gltf button

--- apps/client/src/editor/components/TreeMenu/SpecialsMenu.tsx
+++ apps/client/src/editor/components/TreeMenu/SpecialsMenu.tsx
@@ -1,16 +1,21 @@
 import { SPAWN_TITLES, SpawnPointExtension } from "engine";
+import { useId } from "react";
 
 import { useSpawn } from "../../hooks/useSpawn";
 import { useEditorStore } from "../../store";
 
 const OBJECT_NAME = {
   Spawn: "Spawn",
+  GLTF: "Import glTF",
 } as const;
 
 type ObjectName = (typeof OBJECT_NAME)[keyof typeof OBJECT_NAME];
 
 export default function SpecialsMenu() {
+  const id = useId();
   const spawn = useSpawn();
+
+  const engine = useEditorStore((state) => state.engine);
 
   function addObject(name: ObjectName) {
     const { engine } = useEditorStore.getState();
@@ -33,16 +38,32 @@ export default function SpecialsMenu() {
 
   return (
     <div className="space-y-0.5 p-2">
-      {Object.values(OBJECT_NAME).map((name) => (
-        <button
-          key={name}
-          onClick={() => addObject(name)}
-          className="flex w-full items-center space-x-2 whitespace-nowrap rounded-lg px-4 py-0.5 transition hover:bg-neutral-200"
-        >
-          <span>{name}</span>
-          <span className={spawn ? "font-bold" : ""}>({spawn ? 1 : 0}/1)</span>
-        </button>
-      ))}
+      <button
+        onClick={() => addObject("Spawn")}
+        className="flex w-full items-center space-x-2 whitespace-nowrap rounded-lg px-4 py-0.5 transition hover:bg-neutral-200"
+      >
+        <span>Spawn</span>
+        <span className={spawn ? "font-bold" : ""}>({spawn ? 1 : 0}/1)</span>
+      </button>
+
+      <label onPointerUp={(e) => e.stopPropagation()} htmlFor={id}>
+        <div className="flex w-full cursor-pointer items-center whitespace-nowrap rounded-lg px-4 py-0.5 transition hover:bg-neutral-200">
+          Import glTF
+        </div>
+      </label>
+
+      <input
+        id={id}
+        type="file"
+        accept=".gltf,.glb"
+        className="hidden"
+        onChange={(e) => {
+          const file = e.target.files?.[0];
+          if (!file) return;
+
+          engine?.scene.addFile(file);
+        }}
+      />
     </div>
   );
 }

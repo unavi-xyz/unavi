87343dff0f523026fefa65f8b52585defa12930d
Lean <basedlainpilled@gmail.com>
2023-02-19 15:04:15 -0500

    remove explore page ssr

--- apps/client/src/pages/explore.tsx
+++ apps/client/src/pages/explore.tsx
@@ -1,45 +1,14 @@
-import { createProxySSGHelpers } from "@trpc/react-query/ssg";
 import Link from "next/link";
-import { GetServerSidePropsContext } from "next/types";
 
 import { trpc } from "../client/trpc";
 import MetaTags from "../home/MetaTags";
 import { getNavbarLayout } from "../home/NavbarLayout/NavbarLayout";
 import SpaceCard from "../home/SpaceCard";
-import { prisma } from "../server/prisma";
-import { appRouter } from "../server/router/_app";
 import { numberToHexDisplay } from "../utils/numberToHexDisplay";
-
-export const getServerSideProps = async ({ res }: GetServerSidePropsContext) => {
-  const ONE_MINUTE_IN_SECONDS = 60;
-  const ONE_MONTH_IN_SECONDS = 60 * 60 * 24 * 30;
-
-  res.setHeader(
-    "Cache-Control",
-    `public, max-age=0, s-maxage=${ONE_MINUTE_IN_SECONDS}, stale-while-revalidate=${ONE_MONTH_IN_SECONDS}`
-  );
-
-  const ssg = await createProxySSGHelpers({
-    router: appRouter,
-    ctx: {
-      prisma,
-      res,
-      session: null,
-    },
-  });
-
-  await ssg.space.latest.prefetch({ limit: 25 });
-
-  return {
-    props: {
-      trpcState: ssg.dehydrate(),
-    },
-  };
-};
 
 export default function Explore() {
   const { data: spaces } = trpc.space.latest.useQuery(
-    { limit: 25 },
+    { limit: 40 },
     {
       refetchOnWindowFocus: false,
       refetchOnMount: false,
@@ -56,13 +25,22 @@ export default function Explore() {
           <div className="text-center text-3xl font-black">Explore</div>
 
           <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {spaces?.map(({ id, metadata }) => {
-              return (
-                <Link href={`/space/${numberToHexDisplay(id)}`} key={id} className="rounded-xl">
-                  <SpaceCard id={id} metadata={metadata} animateEnter />
-                </Link>
-              );
-            })}
+            {spaces
+              ? spaces.map(({ id, metadata }) => {
+                  return (
+                    <Link key={id} href={`/space/${numberToHexDisplay(id)}`} className="rounded-xl">
+                      <SpaceCard id={id} metadata={metadata} animateEnter />
+                    </Link>
+                  );
+                })
+              : new Array(4)
+                  .fill(0)
+                  .map((_, i) => (
+                    <div
+                      key={i}
+                      className="aspect-card h-full w-full animate-pulse rounded-xl bg-neutral-300"
+                    />
+                  ))}
           </div>
         </div>
       </div>

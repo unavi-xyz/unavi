5bf2042d6514e72b537e42617abc8eff6a6f4d1f
Lean <basedlainpilled@gmail.com>
2023-02-19 17:17:43 -0500

    remove old files

--- apps/client/src/utils/numberToCommas.ts
+++
@@ -1,3 +0,0 @@
-export function numberToCommas(value: number): string {
-  return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
--- packages/engine/src/utils/toHex.ts
+++
@@ -1,3 +0,0 @@
-export function toHex(num: number) {
-  return `0x${num.toString(16).padStart(2, "0")}`;
-}

f40742ca14fa8da10c275e7f4485046e10cdb9b9
Lean <basedlainpilled@gmail.com>
2023-03-15 13:37:08 -0400

    change svg to react-icon

--- apps/client/app/(navbar)/explore/Search.tsx
+++ apps/client/app/(navbar)/explore/Search.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useId } from "react";
+import { MdSearch } from "react-icons/md";
 
 import { useExploreStore } from "../../../src/editor/store";
 
@@ -23,21 +24,7 @@ export default function Search() {
       />
 
       <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-        <svg
-          aria-hidden="true"
-          className="h-5 w-5 text-neutral-500 dark:text-neutral-400"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
+        <MdSearch className="text-2xl text-neutral-500 dark:text-neutral-400" />
       </div>
     </div>
   );

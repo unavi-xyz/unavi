6e949ae7e445d0f3182187f079c3dee175c6cf7b
Lean <basedlainpilled@gmail.com>
2023-03-15 13:37:43 -0400

    remove suspense boundary

--- apps/client/app/(navbar)/explore/page.tsx
+++ apps/client/app/(navbar)/explore/page.tsx
@@ -1,7 +1,5 @@
 import { Metadata } from "next";
-import { Suspense } from "react";
 
-import Card from "../../../src/ui/Card";
 import Search from "./Search";
 import Spaces from "./Spaces";
 
@@ -17,19 +15,11 @@ export default function Explore() {
       <div className="max-w-content mx-4 flex flex-col items-center space-y-8 py-8">
         <div className="text-center text-3xl font-black">Explore</div>
 
-        <Suspense fallback={<Card loading />}>
-          <Search />
-        </Suspense>
+        <Search />
 
         <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          <Suspense
-            fallback={Array.from({ length: 4 }).map((_, i) => (
-              <Card key={i} loading />
-            ))}
-          >
-            {/* @ts-expect-error Server Component */}
-            <Spaces />
-          </Suspense>
+          {/* @ts-expect-error Server Component */}
+          <Spaces />
         </div>
       </div>
     </div>

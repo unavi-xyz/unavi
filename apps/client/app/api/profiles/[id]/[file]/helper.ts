2b4d3db7c7b1c8eff210aeb3948a70b8a8b10e4e
Lean <basedlainpilled@gmail.com>
2023-03-23 00:06:33 -0400

    fix file api urls

--- apps/client/app/api/profiles/[id]/[file]/helper.ts
+++ apps/client/app/api/profiles/[id]/[file]/helper.ts
@@ -2,7 +2,7 @@ import { ProfileFile } from "../../files";
 import { GetFileUploadResponse } from "./types";
 
 export async function getProfileFileUpload(id: number, file: ProfileFile) {
-  const response = await fetch(`/api/profiles/${id}/${file}/upload`, { method: "PUT" });
+  const response = await fetch(`/api/profiles/${id}/${file}`, { method: "PUT" });
   const { url } = (await response.json()) as GetFileUploadResponse;
   return url;
 }
--- apps/client/app/api/publications/[id]/[file]/helper.ts
+++ apps/client/app/api/publications/[id]/[file]/helper.ts
@@ -2,7 +2,7 @@ import { PublicationFile } from "../../files";
 import { GetFileUploadResponse } from "./types";
 
 export async function getPublicationFileUpload(id: string, file: PublicationFile) {
-  const response = await fetch(`/api/publications/${id}/${file}/upload`, { method: "PUT" });
+  const response = await fetch(`/api/publications/${id}/${file}`, { method: "PUT" });
   const { url } = (await response.json()) as GetFileUploadResponse;
   return url;
 }

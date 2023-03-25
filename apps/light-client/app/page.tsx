ea1d355cd5cba30155a19a51e96785ae3f4ae715
Lean <basedlainpilled@gmail.com>
2023-03-25 14:30:59 -0400

    move light-client space id  to env

--- apps/light-client/app/page.tsx
+++ apps/light-client/app/page.tsx
@@ -4,7 +4,7 @@ import { notFound } from "next/navigation";
 import App from "./App";
 import { fetchMetadata } from "./fetchMetadata";
 
-const SPACE_ID = 13; // Change this to your space ID
+const SPACE_ID = parseInt(process.env.NEXT_PUBLIC_SPACE_ID || "13");
 
 export const metadata: Metadata = {
   title: `Space ${SPACE_ID.toString(16).padStart(4, "0x")} / The Wired`,

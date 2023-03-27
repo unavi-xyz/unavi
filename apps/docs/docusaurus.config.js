facc8f77ef9b3a8b85c0b88234f72e025f60544c
Lean <basedlainpilled@gmail.com>
2023-03-26 21:43:30 -0400

    update docs edit url

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -109,7 +109,7 @@ const config = {
 
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          editUrl: "https://github.com/wired-labs/wired/tree/main/apps/docs/docs",
+          editUrl: "https://github.com/wired-labs/wired/tree/main/apps/docs",
           routeBasePath: "/",
         },
 

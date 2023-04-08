8cb22a2f90d7c3afc3a50b43a14c2bc83e63a9da
Lean <basedlainpilled@gmail.com>
2023-04-08 03:38:53 -0400

    prevent nanoid from starting with 0x

--- apps/client/src/server/nanoid.ts
+++ apps/client/src/server/nanoid.ts
@@ -2,7 +2,16 @@ import { customAlphabet } from "nanoid";
 
 export const NANOID_LENGTH = 12;
 
-export const nanoidShort = customAlphabet(
+const nanoid = customAlphabet(
   "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
   NANOID_LENGTH
 );
+
+/**
+ * Generate a nanoid that does not start with 0x
+ */
+export const nanoidShort = (): string => {
+  const id = nanoid();
+  if (id.startsWith("0x")) return nanoidShort();
+  return id;
+};

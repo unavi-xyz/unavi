26c7808a0252af42b4d1ee1c4a7c997eada81b86
Lean <basedlainpilled@gmail.com>
2023-04-21 00:36:58 -0400

    add type annotation

--- apps/client/src/editor/components/ScriptMenu/utils/getSockets.ts
+++ apps/client/src/editor/components/ScriptMenu/utils/getSockets.ts
@@ -1,10 +1,10 @@
-import { NodeSpecJSON } from "@unavi/behave-graph-core";
+import { InputSocketSpecJSON, NodeSpecJSON, OutputSocketSpecJSON } from "@unavi/behave-graph-core";
 
 export function getSockets(
   nodes: NodeSpecJSON[],
   nodeType: string | undefined,
   handleType: "source" | "target" | null
-) {
+): OutputSocketSpecJSON[] | InputSocketSpecJSON[] | undefined {
   const nodeSpec = nodes.find((node) => node.type === nodeType);
   if (nodeSpec === undefined) return;
   return handleType === "source" ? nodeSpec.outputs : nodeSpec.inputs;

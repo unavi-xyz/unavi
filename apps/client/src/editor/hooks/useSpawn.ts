75b91baa7822180d12714b8e081ea10d3eedee6e
Lean <basedlainpilled@gmail.com>
2023-04-21 01:01:32 -0400

    add type annotation

--- apps/client/src/editor/hooks/useSpawn.ts
+++ apps/client/src/editor/hooks/useSpawn.ts
@@ -1,8 +1,11 @@
+import { Node } from "@gltf-transform/core";
 import { SPAWN_TITLE } from "@unavi/gltf-extensions";
 
 import { useEditor } from "../components/Editor";
 
-export function useSpawn(type: (typeof SPAWN_TITLE)[keyof typeof SPAWN_TITLE] = "Default") {
+export function useSpawn(
+  type: (typeof SPAWN_TITLE)[keyof typeof SPAWN_TITLE] = "Default"
+): Node | undefined {
   const { engine } = useEditor();
 
   const spawn = engine?.scene.getSpawn(type);

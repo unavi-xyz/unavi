e0cb6136670de17eac631e00450f5aa9c3894293
Lean <basedlainpilled@gmail.com>
2023-04-26 06:48:06 -0400

    fix prisma connection errors

--- apps/client/src/server/prisma.ts
+++ apps/client/src/server/prisma.ts
@@ -1,3 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient({ log: ["error"] });
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;

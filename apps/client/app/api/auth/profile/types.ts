1f6244eb9bc018f5c3d71262918117e14fe708d4
Lean <basedlainpilled@gmail.com>
2023-06-19 16:14:22 -0400

    fix bio max length

--- apps/client/app/api/auth/profile/types.ts
+++ apps/client/app/api/auth/profile/types.ts
@@ -2,14 +2,14 @@ import { z } from "zod";
 
 import {
   FILE_KEY_LENGTH,
-  MAX_PROFILE_NAME_LENGTH,
+  MAX_PROFILE_BIO_LENGTH,
   MAX_USERNAME_LENGTH,
   MIN_USERNAME_LENGTH,
 } from "@/src/server/db/constants";
 
 export const UpdateProfileSchema = z.object({
   backgroundKey: z.string().length(FILE_KEY_LENGTH).optional(),
-  bio: z.string().max(MAX_PROFILE_NAME_LENGTH).optional(),
+  bio: z.string().max(MAX_PROFILE_BIO_LENGTH).optional(),
   imageKey: z.string().length(FILE_KEY_LENGTH).optional(),
   username: z
     .string()

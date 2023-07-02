88371aaeeca84bc6d6c40edc11ebff6df74516fb
Lean <basedlainpilled@gmail.com>
2023-07-02 06:05:14 -0400

    add physics plugin

--- packages/react-client/src/plugin.ts
+++ packages/react-client/src/plugin.ts
@@ -1,6 +1,7 @@
 import { LatticeSchedules } from "lattice-engine/core";
 import { gltfPlugin } from "lattice-engine/gltf";
 import { orbitPlugin } from "lattice-engine/orbit";
+import { physicsPlugin } from "lattice-engine/physics";
 import { playerPlugin } from "lattice-engine/player";
 import { defaultPlugin } from "lattice-engine/utils";
 import { vrmPlugin } from "lattice-engine/vrm";
@@ -14,10 +15,11 @@ import { setWorld } from "./systems/setWorld";
 export function clientPlugin(builder: WorldBuilder) {
   builder
     .addPlugin(defaultPlugin)
+    .addPlugin(gltfPlugin)
     .addPlugin(orbitPlugin)
+    .addPlugin(physicsPlugin)
     .addPlugin(playerPlugin)
     .addPlugin(vrmPlugin)
-    .addPlugin(gltfPlugin)
     .addSystemsToSchedule(LatticeSchedules.Startup, initApp)
     .addSystems(loadWorldModels, setSkybox, setWorld);
 }

fdfdad493a9cf65a4cf0c04f986580e11b92e185
Lean <basedlainpilled@gmail.com>
2023-07-19 15:18:00 -0400

    update transform select logic

--- packages/react-client/src/editor/systems/syncTransformTarget.ts
+++ packages/react-client/src/editor/systems/syncTransformTarget.ts
@@ -13,7 +13,26 @@ export function syncTransformTarget(
 
     if (targetId !== lastTransformTarget) {
       // Set UI from transform controls
-      useSceneStore.setState({ selectedId: targetId });
+      const { sceneTreeId, rootId, items } = useSceneStore.getState();
+      const usedId = sceneTreeId ?? rootId;
+      if (!usedId) continue;
+
+      // Continue up tree until we reach a child of usedId
+      let currentId = targetId;
+
+      while (currentId !== usedId) {
+        const parentId: bigint | undefined = items.get(currentId)?.parentId;
+        if (!parentId) break;
+
+        if (parentId === usedId) {
+          // We've reached a child of usedId
+          break;
+        }
+
+        currentId = parentId;
+      }
+
+      useSceneStore.setState({ selectedId: currentId });
     } else {
       // Set transform controls from UI
       transformControl.targetId = useSceneStore.getState().selectedId ?? 0n;

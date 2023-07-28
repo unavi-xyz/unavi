f5a91f90668173f6011c9e3946a7419314c10db9
kayhhh <basedlainpilled@gmail.com>
2023-07-28 19:16:06 -0400

    update card grid col count

--- apps/client/src/ui/CardGrid.tsx
+++ apps/client/src/ui/CardGrid.tsx
@@ -7,7 +7,7 @@ interface Props {
  */
 export default function CardGrid({ children }: Props) {
   return (
-    <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
       {children}
     </div>
   );

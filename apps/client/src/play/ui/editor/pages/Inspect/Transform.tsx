cb7821318045ca1b4c2ed971085f7a57211315c1
Kayh <basedlainpilled@gmail.com>
2023-09-17 15:31:34 -0400

    add back in transform inputs

--- apps/client/src/play/ui/editor/pages/Inspect/Transform.tsx
+++ apps/client/src/play/ui/editor/pages/Inspect/Transform.tsx
@@ -2,6 +2,8 @@ import { SyncedNode } from "@unavi/engine";
 
 import { DeepReadonly } from "@/src/play/utils/types";
 
+import Rotation from "./Rotation";
+import Scale from "./Scale";
 import Translation from "./Translation";
 
 interface Props {
@@ -12,6 +14,8 @@ export default function Transform({ node }: Props) {
   return (
     <div className="space-y-1">
       <Translation node={node} />
+      <Rotation node={node} />
+      <Scale node={node} />
     </div>
   );
 }

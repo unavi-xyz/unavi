1e9445f74af60377094ec5916cad81e1cf73df29
Kayh <basedlainpilled@gmail.com>
2023-10-18 11:01:42 -0400

    setup basic bevy app

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,7 @@
-pub fn hello_world() {
-    println!("Hello, world!");
+use bevy::prelude::*;
+
+pub fn main() {
+    println!("unavi_core::main");
+
+    App::new().add_plugins(DefaultPlugins).run();
 }

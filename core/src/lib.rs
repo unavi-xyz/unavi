bf9517e1897e953f662abe3ec99eb0ee7b61a568
Kayh <basedlainpilled@gmail.com>
2023-10-19 07:40:47 -0400

    basic 3d rendering

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,7 +1,49 @@
 use bevy::prelude::*;
 
 pub fn main() {
-    println!("unavi_core::main");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, rotate)
+        .run();
+}
 
-    App::new().add_plugins(DefaultPlugins).run();
+#[derive(Component)]
+struct Shape;
+
+pub fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let material = materials.add(StandardMaterial {
+        base_color: Color::rgb(0.5, 0.5, 1.0),
+        ..Default::default()
+    });
+
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(shape::Cube::default().into()),
+            material,
+            ..Default::default()
+        },
+        Shape,
+    ));
+
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(50.0).into()),
+        material: materials.add(Color::SILVER.into()),
+        ..default()
+    });
+
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_translation(Vec3::new(0.0, 2.0, 10.0)),
+        ..Default::default()
+    });
+}
+
+fn rotate(mut query: Query<&mut Transform, With<Shape>>, time: Res<Time>) {
+    for mut transform in &mut query {
+        transform.rotate_y(time.delta_seconds() / 2.);
+    }
 }
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,3 +1,3 @@
 pub fn main() {
-    println!("unavi_wasm::main");
+    unavi_core::main();
 }

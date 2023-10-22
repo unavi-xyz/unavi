958c6b829f51ebeb699eedc4d534903d34ca1355
Kayh <basedlainpilled@gmail.com>
2023-10-22 10:00:04 -0400

    add web feature to server

--- server/src/web/fileserve.rs
+++ server/src/web/fileserve.rs
@@ -37,8 +37,7 @@ async fn get_static_file(uri: Uri, root: &str) -> Result<Response<BoxBody>, (Sta
         .uri(uri.clone())
         .body(Body::empty())
         .unwrap();
-    // `ServeDir` implements `tower::Service` so we can call it with `tower::ServiceExt::oneshot`
-    // This path is relative to the cargo root
+
     match ServeDir::new(root).oneshot(req).await {
         Ok(res) => Ok(res.map(boxed)),
         Err(err) => Err((

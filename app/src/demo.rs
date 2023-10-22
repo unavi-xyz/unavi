44295f92496fedd4b87d38f908b069ee324be131
Kayh <basedlainpilled@gmail.com>
2023-10-22 16:03:27 -0400

    better wasm optimizations

---
+++ app/src/demo.rs
@@ -0,0 +1,42 @@
+use bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
+
+pub struct DemoPlugin;
+
+impl Plugin for DemoPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins((
+            RapierPhysicsPlugin::<NoUserData>::default(),
+            RapierDebugRenderPlugin::default(),
+        ))
+        .add_systems(Startup, (setup_graphics, setup_physics))
+        .add_systems(Update, print_ball_altitude);
+    }
+}
+
+fn setup_graphics(mut commands: Commands) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-3.0, 3.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+}
+
+fn setup_physics(mut commands: Commands) {
+    /* Create the ground. */
+    commands
+        .spawn(Collider::cuboid(100.0, 0.1, 100.0))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, -2.0, 0.0)));
+
+    /* Create a bouncing ball. */
+    commands
+        .spawn(RigidBody::Dynamic)
+        .insert(Collider::ball(0.5))
+        .insert(Restitution::coefficient(0.7))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, 4.0, 0.0)));
+}
+
+fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
+    for transform in positions.iter() {
+        println!("Ball altitude: {}", transform.translation.y);
+    }
+}
--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,55 +1,18 @@
 use bevy::prelude::*;
 
+mod demo;
+
 pub fn start() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                fit_canvas_to_parent: true,
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    fit_canvas_to_parent: true,
+                    ..Default::default()
+                }),
                 ..Default::default()
             }),
-            ..Default::default()
-        }))
-        .add_systems(Startup, setup)
-        .add_systems(Update, rotate)
+            demo::DemoPlugin,
+        ))
         .run();
-}
-
-#[derive(Component)]
-struct Shape;
-
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    let material = materials.add(StandardMaterial {
-        base_color: Color::rgb(0.5, 0.5, 1.0),
-        ..Default::default()
-    });
-
-    commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(shape::Cube::default().into()),
-            material,
-            ..Default::default()
-        },
-        Shape,
-    ));
-
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(shape::Plane::from_size(50.0).into()),
-        material: materials.add(Color::SILVER.into()),
-        ..default()
-    });
-
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_translation(Vec3::new(0.0, 2.0, 10.0)),
-        ..Default::default()
-    });
-}
-
-fn rotate(mut query: Query<&mut Transform, With<Shape>>, time: Res<Time>) {
-    for mut transform in &mut query {
-        transform.rotate_y(time.delta_seconds() / 2.);
-    }
 }

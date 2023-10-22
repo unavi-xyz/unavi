d15cc2464020c2202355c42596d5921c9acac840
Kayh <basedlainpilled@gmail.com>
2023-10-22 16:52:35 -0400

    platform configuration

--- web/frontend/src/lib.rs
+++ web/frontend/src/lib.rs
@@ -3,6 +3,10 @@ use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
 pub fn hydrate() {
+    #[cfg(target_arch = "wasm32")]
+    console_error_panic_hook::set_once();
+
     unavi_app::start();
+
     leptos::mount_to_body(unavi_web_app::App);
 }

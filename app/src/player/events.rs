1ad0dec6f00c70ef5e5a3cca8c2e5dfa8113b545
Kayh <basedlainpilled@gmail.com>
2023-10-25 21:12:24 -0400

    clean up

--- app/src/player/events.rs
+++ app/src/player/events.rs
@@ -2,46 +2,6 @@ use bevy::prelude::*;
 use std::ops::Deref;
 
 #[derive(Event, Debug, Default)]
-pub struct LookDeltaEvent {
-    rotation_delta: Vec3,
-}
-
-impl LookDeltaEvent {
-    pub fn new(other: &Vec3) -> Self {
-        Self {
-            rotation_delta: *other,
-        }
-    }
-}
-
-impl Deref for LookDeltaEvent {
-    type Target = Vec3;
-
-    fn deref(&self) -> &Self::Target {
-        &self.rotation_delta
-    }
-}
-
-#[derive(Event, Debug, Default)]
-pub struct LookEvent {
-    rotation: Vec3,
-}
-
-impl LookEvent {
-    pub fn new(other: &Vec3) -> Self {
-        Self { rotation: *other }
-    }
-}
-
-impl Deref for LookEvent {
-    type Target = Vec3;
-
-    fn deref(&self) -> &Self::Target {
-        &self.rotation
-    }
-}
-
-#[derive(Event, Debug, Default)]
 pub struct PitchEvent {
     pitch: f32,
 }

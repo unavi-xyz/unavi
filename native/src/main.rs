0aa806803db189aa7a56786bfb5c916d02258775
Kayh <basedlainpilled@gmail.com>
2023-10-27 13:37:41 -0400

    add skybox

--- native/src/main.rs
+++ native/src/main.rs
@@ -1,5 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    unavi_app::start();
+    unavi_app::start(unavi_app::StartOptions {
+        asset_folder: "../assets".to_string(),
+    });
 }
--- server/src/web/mod.rs
+++ server/src/web/mod.rs
@@ -1,11 +1,14 @@
 mod fileserve;
 
+use axum::routing::get;
 use axum::routing::post;
 use axum::Router;
 use fileserve::file_and_error_handler;
 use leptos::*;
 use leptos_axum::{generate_route_list, LeptosRoutes};
 use unavi_web_app::*;
+
+use self::fileserve::assets_file_handler;
 
 pub async fn router() -> Router {
     let conf = get_configuration(None).await.unwrap();
@@ -15,6 +18,7 @@ pub async fn router() -> Router {
 
     let router: Router = Router::new()
         .route("/api/*fn_name", post(leptos_axum::handle_server_fns))
+        .route("/assets/*path", get(assets_file_handler))
         .leptos_routes(&leptos_options, routes, App)
         .fallback(file_and_error_handler)
         .with_state(leptos_options);

71ee75539d054d55dad6fb511f022ac3071bf33c
Kayh <basedlainpilled@gmail.com>
2023-11-01 16:09:52 -0400

    update bevy_vrm

--- app/src/world/mod.rs
+++ app/src/world/mod.rs
@@ -1,5 +1,4 @@
 use bevy::prelude::*;
-use bevy_rapier3d::prelude::{Collider, RigidBody};
 
 mod skybox;
 
@@ -12,22 +11,20 @@ impl Plugin for WorldPlugin {
     }
 }
 
-fn setup_world(
-    mut commands: Commands,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut ambient: ResMut<AmbientLight>,
-) {
-    commands.spawn(DirectionalLightBundle {
-        directional_light: DirectionalLight {
-            shadows_enabled: true,
-            illuminance: 10_000.0,
-            color: Color::rgb(1.0, 1.0, 0.98),
+fn setup_world(mut commands: Commands, mut ambient: ResMut<AmbientLight>) {
+    commands.spawn((
+        DirectionalLightBundle {
+            directional_light: DirectionalLight {
+                shadows_enabled: true,
+                illuminance: 10_000.0,
+                color: Color::rgb(1.0, 1.0, 0.98),
+                ..default()
+            },
+            transform: Transform::from_xyz(-4.5, 10.0, 7.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..default()
         },
-        transform: Transform::from_xyz(-4.5, 10.0, 7.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
+        bevy_vrm::mtoon::MtoonSun,
+    ));
 
     ambient.color = Color::rgb(0.95, 0.95, 1.0);
     ambient.brightness = 0.1;

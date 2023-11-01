9b9ce27086cc5440a554c81eea811c856392f1d0
Kayh <basedlainpilled@gmail.com>
2023-11-01 15:43:00 -0400

    move demo world to example

--- web/frontend/src/lib.rs
+++ web/frontend/src/lib.rs
@@ -4,8 +4,6 @@ use wasm_bindgen::prelude::wasm_bindgen;
 #[wasm_bindgen]
 pub fn hydrate() {
     console_error_panic_hook::set_once();
-
     unavi_app::start(unavi_app::StartOptions::default());
-
     leptos::mount_to_body(unavi_web_app::App);
 }

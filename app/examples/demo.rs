4f01e84be4d39420d7b3b948c78924ff9e305638
Kayh <basedlainpilled@gmail.com>
2023-11-01 18:31:48 -0400

    add dev texture to demo world

--- app/examples/demo.rs
+++ app/examples/demo.rs
@@ -1,6 +1,13 @@
 use std::f32::consts::PI;
 
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    render::{
+        mesh::VertexAttributeValues,
+        render_resource::{AddressMode, Extent3d, SamplerDescriptor},
+        texture::ImageSampler,
+    },
+};
 use bevy_rapier3d::prelude::*;
 
 fn main() {
@@ -14,11 +21,15 @@ fn main() {
 }
 
 fn callback(app: &mut App) {
-    app.add_systems(Startup, setup_world);
+    app.add_systems(Startup, setup_world)
+        .add_systems(Update, set_texture_tiled);
 }
 
 const GROUND_SIZE: f32 = 40.0;
-const GROUND_THICKNESS: f32 = 0.1;
+const GROUND_THICKNESS: f32 = 0.01;
+
+#[derive(Component, Default)]
+pub struct RepeatTexture;
 
 fn setup_world(
     asset_server: Res<AssetServer>,
@@ -26,25 +37,38 @@ fn setup_world(
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
 ) {
+    let dev_texture = asset_server.load("images/dev/texture_07.png");
+
+    let ground_texture_scale = GROUND_SIZE / 4.0;
+
+    let mut ground_mesh = Mesh::from(shape::Plane {
+        size: GROUND_SIZE,
+        ..default()
+    });
+
+    match ground_mesh.attribute_mut(Mesh::ATTRIBUTE_UV_0).unwrap() {
+        VertexAttributeValues::Float32x2(uvs) => {
+            for uv in uvs {
+                uv[0] *= ground_texture_scale;
+                uv[1] *= ground_texture_scale;
+            }
+        }
+        _ => panic!(),
+    }
+
     commands.spawn((
         RigidBody::Fixed,
         Collider::cuboid(GROUND_SIZE / 2.0, GROUND_THICKNESS / 2.0, GROUND_SIZE / 2.0),
         PbrBundle {
-            mesh: meshes.add(
-                (shape::Box {
-                    min_x: -GROUND_SIZE / 2.0,
-                    max_x: GROUND_SIZE / 2.0,
-                    min_y: -GROUND_THICKNESS / 2.0,
-                    max_y: GROUND_THICKNESS / 2.0,
-                    min_z: -GROUND_SIZE / 2.0,
-                    max_z: GROUND_SIZE / 2.0,
-                })
-                .into(),
-            ),
-            material: materials.add(Color::rgb(0.8, 0.8, 0.8).into()),
+            mesh: meshes.add(ground_mesh),
+            material: materials.add(StandardMaterial {
+                base_color_texture: Some(dev_texture.clone()),
+                ..default()
+            }),
             transform: Transform::from_xyz(0.0, -0.1, 0.0),
             ..default()
         },
+        RepeatTexture,
     ));
 
     commands.spawn((SceneBundle {
@@ -98,6 +122,41 @@ fn setup_world(
             ),
             ColliderMassProperties::Density(8.0),
         ));
+    }
+}
+
+// https://github.com/bevyengine/bevy/issues/399
+pub fn set_texture_tiled(
+    mut commands: Commands,
+    repeat_textures: Query<(Entity, &Handle<StandardMaterial>), With<RepeatTexture>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut images: ResMut<Assets<Image>>,
+) {
+    for (entity, handle) in repeat_textures.iter() {
+        if let Some(material) = materials.get_mut(handle) {
+            if let Some(base_color_texture_handle) = &material.base_color_texture {
+                if let Some(image) = images.get_mut(base_color_texture_handle) {
+                    match &mut image.sampler_descriptor {
+                        ImageSampler::Default => {
+                            image.sampler_descriptor =
+                                ImageSampler::Descriptor(SamplerDescriptor {
+                                    address_mode_u: AddressMode::Repeat,
+                                    address_mode_v: AddressMode::Repeat,
+                                    address_mode_w: AddressMode::Repeat,
+                                    ..default()
+                                });
+                        }
+                        ImageSampler::Descriptor(sampler_descriptor) => {
+                            sampler_descriptor.address_mode_u = AddressMode::Repeat;
+                            sampler_descriptor.address_mode_v = AddressMode::Repeat;
+                            sampler_descriptor.address_mode_w = AddressMode::Repeat;
+                        }
+                    }
+
+                    commands.entity(entity).remove::<RepeatTexture>();
+                }
+            }
+        }
     }
 }
 

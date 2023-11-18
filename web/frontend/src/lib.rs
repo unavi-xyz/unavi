3fb9fddbe1dee6273480eac5b5204648b70a02f8
Kayh <basedlainpilled@gmail.com>
2023-11-18 12:47:01 -0500

    remove callback, use plugin

--- web/frontend/src/lib.rs
+++ web/frontend/src/lib.rs
@@ -4,6 +4,10 @@ use wasm_bindgen::prelude::wasm_bindgen;
 #[wasm_bindgen]
 pub fn hydrate() {
     console_error_panic_hook::set_once();
-    unavi_app::start(unavi_app::StartOptions::default());
+
+    unavi_app::App::new()
+        .add_plugins(unavi_app::UnaviPlugin::default())
+        .run();
+
     leptos::mount_to_body(unavi_web_app::App);
 }

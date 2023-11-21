504dec0de12a2bb36d679587d6f3ef5ab7f2d97a
Kayh <basedlainpilled@gmail.com>
2023-11-20 23:09:14 -0500

    add initial protos

--- wired-protocol/build.rs
+++ wired-protocol/build.rs
@@ -1,6 +1,18 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
-    prost_build::compile_protos(&["src/items.proto"], &["src/"])?;
+    prost_build::compile_protos(
+        &[
+            "protos/world/webrtc/common.proto",
+            "protos/world/webrtc/request.proto",
+            "protos/world/webrtc/response.proto",
+            "protos/world/websocket/request.proto",
+            "protos/world/websocket/response.proto",
+            "protos/world/websocket/signaling/common.proto",
+            "protos/world/websocket/signaling/request.proto",
+            "protos/world/websocket/signaling/response.proto",
+        ],
+        &["protos/", "src/"],
+    )?;
     Ok(())
 }
--- wired-protocol/src/lib.rs
+++ wired-protocol/src/lib.rs
@@ -1,5 +1,48 @@
-pub mod snazzy {
-    pub mod items {
-        include!(concat!(env!("OUT_DIR"), "/snazzy.items.rs"));
+pub mod world {
+    pub mod webrtc {
+        pub mod common {
+            include!(concat!(env!("OUT_DIR"), "/world.webrtc.common.rs"));
+        }
+
+        pub mod request {
+            include!(concat!(env!("OUT_DIR"), "/world.webrtc.request.rs"));
+        }
+
+        pub mod response {
+            include!(concat!(env!("OUT_DIR"), "/world.webrtc.response.rs"));
+        }
+    }
+
+    pub mod websocket {
+        pub mod request {
+            include!(concat!(env!("OUT_DIR"), "/world.websocket.request.rs"));
+        }
+
+        pub mod response {
+            include!(concat!(env!("OUT_DIR"), "/world.websocket.response.rs"));
+        }
+
+        pub mod signaling {
+            pub mod common {
+                include!(concat!(
+                    env!("OUT_DIR"),
+                    "/world.websocket.signaling.common.rs"
+                ));
+            }
+
+            pub mod request {
+                include!(concat!(
+                    env!("OUT_DIR"),
+                    "/world.websocket.signaling.request.rs"
+                ));
+            }
+
+            pub mod response {
+                include!(concat!(
+                    env!("OUT_DIR"),
+                    "/world.websocket.signaling.response.rs"
+                ));
+            }
+        }
     }
 }

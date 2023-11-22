0cffe933a9406544d0cafdc15bbff562805c97d3
Kayh <basedlainpilled@gmail.com>
2023-11-22 14:44:59 -0500

    clean up server

--- unavi-server/src/main.rs
+++ unavi-server/src/main.rs
@@ -1,13 +1,14 @@
+use tracing::{error, info};
+
 #[tokio::main]
 async fn main() {
-    println!("Features:");
+    info!("Features:");
     let feat_web = cfg!(feature = "web");
-    println!("- web: {}", feat_web);
+    info!("- web: {}", feat_web);
     let feat_world = cfg!(feature = "world");
-    println!("- world: {}", feat_world);
+    info!("- world: {}", feat_world);
 
-    match unavi_server::start_server(unavi_server::ServerOptions::default()).await {
-        Ok(_) => println!("Server exited successfully"),
-        Err(e) => println!("Server exited with error: {}", e),
+    if let Err(e) = unavi_server::start_server(unavi_server::ServerOptions::default()).await {
+        error!(e);
     }
 }

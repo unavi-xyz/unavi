cf05ca12a463326e197c6618f8fcae32ac3cfb6d
Kayh <basedlainpilled@gmail.com>
2023-11-29 18:35:46 -0500

    set up script asset

--- crates/unavi-wasm-runtime/src/lib.rs
+++ crates/unavi-wasm-runtime/src/lib.rs
@@ -1,3 +1,13 @@
-pub fn hello() {
-    println!("Hello, world!");
+use bevy::prelude::*;
+
+pub mod asset;
+pub mod state;
+
+pub struct WasmRuntimePlugin;
+
+impl Plugin for WasmRuntimePlugin {
+    fn build(&self, app: &mut App) {
+        app.register_asset_loader(asset::ScriptLoader)
+            .init_asset::<asset::Script>();
+    }
 }

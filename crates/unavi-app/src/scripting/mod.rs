6267627df5ffd3637d9932efe202304773819710
Kayh <basedlainpilled@gmail.com>
2023-11-29 18:51:29 -0500

    move unavi-wasm-runtime into unavi-app

--- crates/unavi-app/src/scripting/mod.rs
+++ crates/unavi-app/src/scripting/mod.rs
@@ -3,11 +3,14 @@ use bevy::prelude::*;
 pub mod asset;
 pub mod state;
 
-pub struct WasmRuntimePlugin;
+pub struct ScriptingPlugin;
 
-impl Plugin for WasmRuntimePlugin {
+impl Plugin for ScriptingPlugin {
     fn build(&self, app: &mut App) {
         app.register_asset_loader(asset::WasmLoader)
             .init_asset::<asset::Wasm>();
     }
 }
+
+#[derive(Component)]
+pub struct WasmRuntime;

dad748878c9b44059668252bd217c42c6e85abbf
Kayh <basedlainpilled@gmail.com>
2023-11-30 17:46:24 -0500

    fail wasm build on error

--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -3,6 +3,7 @@
 target="wasm32-unknown-unknown"
 profile="wasm-release"
 
+set -e
 cd wasm
 
 for pkg in *
@@ -10,7 +11,7 @@ do
   echo "build-wasm.sh: $pkg"
   wasm_name="${pkg//-/_}" # Replace - with _
   # Compile to wasm
-  cargo build -p $pkg --target $target --profile $profile && \
+  cargo build -p $pkg --target $target --profile $profile
   # Convert to component + move to assets dir
   wasm-tools component new ../target/$target/$profile/$wasm_name.wasm -o ../assets/scripts/$wasm_name.wasm
 done

6049cbada464471563010191eba95645d481169e
Kayh <basedlainpilled@gmail.com>
2023-11-29 19:28:43 -0500

    fix unavi-native assets dir

--- crates/unavi-native/src/main.rs
+++ crates/unavi-native/src/main.rs
@@ -5,7 +5,7 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(unavi_app::UnaviPlugin {
-            assets_dir: "../assets".to_string(),
+            assets_dir: "../../assets".to_string(),
             ..default()
         })
         .run();

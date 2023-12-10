51210db8d05319ab9c04e64bc72952cf0e0efc0f
Kayh <basedlainpilled@gmail.com>
2023-12-09 20:20:45 -0500

    add main to unavi-server-world

---
+++ crates/unavi-server-world/src/main.rs
@@ -0,0 +1,18 @@
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let address = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let certificate = unavi_server_world::cert::generate_certificate();
+
+    if let Err(e) = unavi_server_world::start_server(unavi_server_world::WorldOptions {
+        address,
+        certificate,
+    })
+    .await
+    {
+        tracing::error!("Server error: {}", e);
+    }
+}

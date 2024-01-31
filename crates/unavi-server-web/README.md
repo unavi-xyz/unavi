36b78699c28be54689b813ee336083ac3060eb5c
Kayh <basedlainpilled@gmail.com>
2024-01-31 14:27:53 -0500

    move trunk site to unavi-app

--- crates/unavi-server-web/README.md
+++ crates/unavi-server-web/README.md
@@ -1,17 +1,3 @@
 # unavi-server-web
 
-Server that hosts a web build of the app.
-
-## Usage
-
-Build the site using `trunk`:
-
-```bash
-trunk build --release
-```
-
-Then start the server:
-
-```bash
-cargo run -p unavi-server-web
-```
+Hosts a web build of the app.
--- crates/unavi-server-web/src/lib.rs
+++ crates/unavi-server-web/src/lib.rs
@@ -2,7 +2,7 @@ use axum::Router;
 use tower_http::services::{ServeDir, ServeFile};
 
 pub async fn router() -> Router {
-    let serve_dir = ServeDir::new("crates/unavi-server-web/dist")
-        .not_found_service(ServeFile::new("crates/unavi-server-web/dist/index.html"));
+    let serve_dir = ServeDir::new("crates/unavi-app/dist")
+        .not_found_service(ServeFile::new("crates/unavi-app/dist/index.html"));
     Router::new().fallback_service(serve_dir)
 }

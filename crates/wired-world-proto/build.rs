d425252e808e4eb87c90f10fea3b6b0ce84c3e81
Kayh <basedlainpilled@gmail.com>
2024-02-02 20:03:27 -0500

    fix check fr this time

--- crates/wired-world-proto/build.rs
+++ crates/wired-world-proto/build.rs
@@ -3,16 +3,16 @@ use std::io::Result;
 fn main() -> Result<()> {
     prost_build::compile_protos(
         &[
-            "protos/webrtc/common.proto",
-            "protos/webrtc/request.proto",
-            "protos/webrtc/response.proto",
-            "protos/websocket/request.proto",
-            "protos/websocket/response.proto",
-            "protos/websocket/signaling/common.proto",
-            "protos/websocket/signaling/request.proto",
-            "protos/websocket/signaling/response.proto",
+            "../../wired-protocol/world/protos/webrtc/common.proto",
+            "../../wired-protocol/world/protos/webrtc/request.proto",
+            "../../wired-protocol/world/protos/webrtc/response.proto",
+            "../../wired-protocol/world/protos/websocket/request.proto",
+            "../../wired-protocol/world/protos/websocket/response.proto",
+            "../../wired-protocol/world/protos/websocket/signaling/common.proto",
+            "../../wired-protocol/world/protos/websocket/signaling/request.proto",
+            "../../wired-protocol/world/protos/websocket/signaling/response.proto",
         ],
-        &["protos/", "src/"],
+        &["../../wired-protocol/world/protos/", "src/"],
     )?;
     Ok(())
 }

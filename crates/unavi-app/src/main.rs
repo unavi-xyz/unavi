f724fdd87cca25211265348f227e166af4bcb353
Kayh <basedlainpilled@gmail.com>
2024-03-03 09:17:40 -0500

    fixes

--- crates/unavi-app/src/main.rs
+++ crates/unavi-app/src/main.rs
@@ -9,23 +9,3 @@ fn main() {
         .add_plugins((DefaultPlugins, UnaviPlugin::default()))
         .run();
 }
-
-#[cfg(target_family = "wasm")]
-use wasm_bindgen::prelude::*;
-
-#[cfg(target_family = "wasm")]
-#[wasm_bindgen(start)]
-fn start() {
-    App::new()
-        .add_plugins((
-            DefaultPlugins.set(WindowPlugin {
-                primary_window: Some(Window {
-                    fit_canvas_to_parent: true,
-                    ..default()
-                }),
-                ..default()
-            }),
-            UnaviPlugin::default(),
-        ))
-        .run();
-}

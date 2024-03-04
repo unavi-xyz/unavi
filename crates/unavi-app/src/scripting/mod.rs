c3d335b777d1a6e27506d680510e0079f5091bf4
Kayh <basedlainpilled@gmail.com>
2024-03-04 17:28:28 -0500

    clean up scripting plugin

--- crates/unavi-app/src/scripting/mod.rs
+++ crates/unavi-app/src/scripting/mod.rs
@@ -8,17 +8,5 @@ impl Plugin for ScriptingPlugin {
     fn build(&self, app: &mut App) {
         app.register_asset_loader(asset::WasmLoader)
             .init_asset::<asset::Wasm>();
-        // .add_systems(OnEnter(AppState::InWorld), setup_runtimes)
-        // .add_systems(
-        //     Update,
-        //     (
-        //         scripts::instantiate_scripts,
-        //         scripts::set_runtime_name,
-        //         lifecycle::init_scripts,
-        //         lifecycle::update_scripts,
-        //         lifecycle::exit_scripts,
-        //     )
-        //         .chain(),
-        // );
     }
 }

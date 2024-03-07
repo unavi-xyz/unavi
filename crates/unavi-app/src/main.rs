0fdaa0cab668a9bba6dcf69454cecc049a48eefc
Kayh <basedlainpilled@gmail.com>
2024-03-07 14:42:31 -0500

    set AssetMetaCheck paths

--- crates/unavi-app/src/main.rs
+++ crates/unavi-app/src/main.rs
@@ -1,11 +1,16 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use bevy::prelude::*;
+use bevy::{asset::AssetMetaCheck, prelude::*, utils::HashSet};
 
 use unavi_app::UnaviPlugin;
 
 fn main() {
+    let mut meta_paths = HashSet::new();
+    meta_paths.insert("images/dev-white.png".into());
+    meta_paths.insert("images/skybox-1-512.png".into());
+
     App::new()
+        .insert_resource(AssetMetaCheck::Paths(meta_paths))
         .add_plugins((DefaultPlugins, UnaviPlugin::default()))
         .run();
 }

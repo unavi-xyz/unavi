e8d4109a390cd12e34ecff8f356e7ef946c11daa
Kayh <basedlainpilled@gmail.com>
2024-03-07 14:20:29 -0500

    add wasm stdout capture

--- crates/unavi-app/src/scripting/script.rs
+++ crates/unavi-app/src/scripting/script.rs
@@ -5,11 +5,15 @@ use bevy::{
     prelude::*,
     tasks::{AsyncComputeTaskPool, Task},
 };
+use bytes::Bytes;
 use wasm_bridge::{
     component::{new_component_async, Linker},
     AsContextMut, Config, Engine, Store,
 };
-use wasm_bridge_wasi::preview2::{command, ResourceTable, WasiCtx, WasiCtxBuilder, WasiView};
+use wasm_bridge_wasi::preview2::{
+    command, HostOutputStream, ResourceTable, StdoutStream, StreamResult, Subscribe, WasiCtx,
+    WasiCtxBuilder, WasiView,
+};
 
 use super::asset::Wasm;
 
@@ -25,6 +29,7 @@ pub struct ScriptLoadQueue(pub Vec<Handle<Wasm>>);
 #[derive(Resource)]
 pub struct WasmRuntime {
     pub store: Arc<Mutex<Store<RuntimeState>>>,
+    pub out_bytes: Arc<Mutex<Vec<u8>>>,
 }
 
 pub struct RuntimeState {
@@ -49,14 +54,22 @@ impl Default for WasmRuntime {
 
         let engine = Engine::new(&config).expect("Failed to create engine");
 
+        let out_bytes = Arc::new(Mutex::new(Vec::<u8>::new()));
+        let out_stream = OutStream {
+            data: out_bytes.clone(),
+            max: 3,
+        };
+
+        let wasi = WasiCtxBuilder::new().stdout(out_stream).build();
+
         let table = ResourceTable::new();
-        let wasi = WasiCtxBuilder::new().build();
         let state = RuntimeState { table, wasi };
 
         let store = Store::new(&engine, state);
 
         Self {
             store: Arc::new(Mutex::new(store)),
+            out_bytes,
         }
     }
 }
@@ -69,6 +82,14 @@ pub fn load_scripts(
     runtime: Res<WasmRuntime>,
     wasm_assets: Res<Assets<Wasm>>,
 ) {
+    if !runtime.out_bytes.lock().unwrap().is_empty() {
+        info!(
+            "Script output: {}",
+            String::from_utf8_lossy(&runtime.out_bytes.lock().unwrap())
+        );
+        runtime.out_bytes.lock().unwrap().clear();
+    }
+
     if load_queue.0.is_empty() {
         return;
     }
@@ -115,5 +136,46 @@ pub fn load_scripts(
 
     for handle in to_remove {
         load_queue.0.retain(|h| h != &handle);
+    }
+}
+
+#[derive(Clone, Debug)]
+struct OutStream {
+    data: Arc<Mutex<Vec<u8>>>,
+    max: usize,
+}
+
+#[wasm_bridge::async_trait]
+impl Subscribe for OutStream {
+    async fn ready(&mut self) {}
+}
+
+impl HostOutputStream for OutStream {
+    fn write(&mut self, buf: Bytes) -> StreamResult<()> {
+        assert!(
+            buf.len() <= self.max,
+            "We specified to write at most {} bytes at a time.",
+            self.max
+        );
+        self.data.try_lock().unwrap().extend(buf);
+        StreamResult::Ok(())
+    }
+
+    fn flush(&mut self) -> StreamResult<()> {
+        StreamResult::Ok(())
+    }
+
+    fn check_write(&mut self) -> StreamResult<usize> {
+        StreamResult::Ok(self.max)
+    }
+}
+
+impl StdoutStream for OutStream {
+    fn stream(&self) -> Box<(dyn HostOutputStream + 'static)> {
+        Box::new((*self).clone())
+    }
+
+    fn isatty(&self) -> bool {
+        false
     }
 }

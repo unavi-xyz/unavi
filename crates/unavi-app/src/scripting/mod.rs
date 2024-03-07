8d315e3f35cf7b8580ae4c52910e2d48355cdca2
Kayh <basedlainpilled@gmail.com>
2024-03-06 23:15:40 -0500

    use wasm-bridge fork to create Store Resource

--- crates/unavi-app/src/scripting/mod.rs
+++ crates/unavi-app/src/scripting/mod.rs
@@ -1,6 +1,9 @@
 use bevy::prelude::*;
 
-use self::script::{load_scripts, ScriptLoadQueue};
+use self::{
+    asset::Wasm,
+    script::{load_scripts, ScriptLoadQueue, WasmRuntime},
+};
 
 mod asset;
 mod script;
@@ -10,8 +13,9 @@ pub struct ScriptingPlugin;
 impl Plugin for ScriptingPlugin {
     fn build(&self, app: &mut App) {
         app.register_asset_loader(asset::WasmLoader)
-            .init_asset::<asset::Wasm>()
-            .insert_resource(ScriptLoadQueue::default())
+            .init_asset::<Wasm>()
+            .init_resource::<ScriptLoadQueue>()
+            .init_resource::<WasmRuntime>()
             .add_systems(Startup, load_unavi_system)
             .add_systems(Update, load_scripts);
     }

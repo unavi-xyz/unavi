ea5889d4bbf184a529598880f2199819d1093629
Kayh <basedlainpilled@gmail.com>
2024-03-07 12:52:21 -0500

    async script loading + wasi

--- components/unavi-system/src/lib.rs
+++ components/unavi-system/src/lib.rs
@@ -1,26 +1,12 @@
+use bindings::exports::wired::script::lifecycle::Guest;
+
 #[allow(warnings)]
 mod bindings;
-
-use bindings::{
-    exports::wired::script::lifecycle::Guest as Script, wired::log::api::LogLevel, Guest,
-};
-
-use crate::bindings::wired::log::api::log;
 
 struct Component;
 
 impl Guest for Component {
-    fn hello_world() -> String {
-        "Hello, World!".to_string()
-    }
-}
-
-impl Script for Component {
     fn init() {
-        bindings::unavi::ui::api::new_bubble();
-
-        log(LogLevel::Info, "unavi-system Script initialized");
-
         println!("Component initialized");
     }
 
--- crates/unavi-app/src/scripting/script.rs
+++ crates/unavi-app/src/scripting/script.rs
@@ -1,9 +1,20 @@
-use bevy::{prelude::*, tasks::Task};
-use wasm_bridge::{component::Linker, Config, Engine, Store};
+use std::sync::{Arc, Mutex};
+
+use bevy::{
+    ecs::system::CommandQueue,
+    prelude::*,
+    tasks::{AsyncComputeTaskPool, Task},
+};
+use wasm_bridge::{
+    component::{new_component_async, Linker},
+    AsContextMut, Config, Engine, Store,
+};
+use wasm_bridge_wasi::preview2::{command, Table, WasiCtx, WasiCtxBuilder, WasiView};
 
 use super::asset::Wasm;
 
 wasm_bridge::component::bindgen!({
+    async: true,
     path: "../../wired-protocol/spatial/wired-script/world.wit",
     world: "script",
 });
@@ -11,29 +22,57 @@ wasm_bridge::component::bindgen!({
 #[derive(Resource, Default)]
 pub struct ScriptLoadQueue(pub Vec<Handle<Wasm>>);
 
-#[derive(Component)]
-pub struct LoadComponentTask(Task<()>);
-
 #[derive(Resource)]
 pub struct WasmRuntime {
-    pub store: Store<()>,
+    pub store: Arc<Mutex<Store<RuntimeState>>>,
+}
+
+pub struct RuntimeState {
+    table: Table,
+    wasi: WasiCtx,
+}
+
+impl WasiView for RuntimeState {
+    fn table(&self) -> &Table {
+        &self.table
+    }
+    fn table_mut(&mut self) -> &mut Table {
+        &mut self.table
+    }
+    fn ctx(&self) -> &WasiCtx {
+        &self.wasi
+    }
+    fn ctx_mut(&mut self) -> &mut WasiCtx {
+        &mut self.wasi
+    }
 }
 
 impl Default for WasmRuntime {
     fn default() -> Self {
         let mut config = Config::default();
+        config.async_support(true);
         config.wasm_component_model(true);
 
         let engine = Engine::new(&config).expect("Failed to create engine");
-        let store = Store::new(&engine, ());
 
-        Self { store }
+        let table = Table::new();
+        let wasi = WasiCtxBuilder::new().build();
+        let state = RuntimeState { table, wasi };
+
+        let store = Store::new(&engine, state);
+
+        Self {
+            store: Arc::new(Mutex::new(store)),
+        }
     }
 }
 
+#[derive(Component)]
+struct LoadScript(Task<CommandQueue>);
+
 pub fn load_scripts(
     mut load_queue: ResMut<ScriptLoadQueue>,
-    mut runtime: ResMut<WasmRuntime>,
+    runtime: Res<WasmRuntime>,
     wasm_assets: Res<Assets<Wasm>>,
 ) {
     if load_queue.0.is_empty() {
@@ -42,32 +81,41 @@ pub fn load_scripts(
 
     let mut to_remove = Vec::new();
 
+    let pool = AsyncComputeTaskPool::get();
+
     for handle in &load_queue.0 {
         if let Some(wasm) = wasm_assets.get(handle) {
             to_remove.push(handle.clone());
 
-            let component =
-                match wasm_bridge::component::Component::new(runtime.store.engine(), &wasm.bytes) {
-                    Ok(c) => c,
-                    Err(e) => {
-                        error!("Failed to load component: {:?}", e);
-                        continue;
-                    }
-                };
+            let store = runtime.store.clone();
+            let bytes = wasm.bytes.clone();
 
-            let linker = Linker::new(runtime.store.engine());
+            let mut linker = Linker::new(store.lock().unwrap().engine());
+            if let Err(e) = command::add_to_linker(&mut linker) {
+                error!("Failed to add wasi to linker: {}", e);
+                continue;
+            }
 
-            let (script, _) = match Script::instantiate(&mut runtime.store, &component, &linker) {
-                Ok(s) => s,
-                Err(e) => {
-                    error!("Failed to instantiate component: {:?}", e);
-                    continue;
-                }
-            };
+            #[allow(clippy::await_holding_lock)]
+            pool.spawn_local(async move {
+                let mut store = store.lock().unwrap();
 
-            script.interface0.call_init(&mut runtime.store).unwrap();
+                let component = new_component_async(store.engine(), &bytes).await.unwrap();
 
-            info!("Script initialized");
+                let (script, _) =
+                    Script::instantiate_async(&mut store.as_context_mut(), &component, &linker)
+                        .await
+                        .unwrap();
+
+                script
+                    .interface0
+                    .call_init(&mut store.as_context_mut())
+                    .await
+                    .unwrap();
+
+                info!("Script initialized");
+            })
+            .detach();
         }
     }
 

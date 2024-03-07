e0cf9a88d7ef2da0ebbdb5b910777badc321509a
Kayh <basedlainpilled@gmail.com>
2024-03-06 23:15:40 -0500

    use wasm-bridge fork to create Store Resource

--- crates/unavi-app/src/scripting/script.rs
+++ crates/unavi-app/src/scripting/script.rs
@@ -14,7 +14,28 @@ pub struct ScriptLoadQueue(pub Vec<Handle<Wasm>>);
 #[derive(Component)]
 pub struct LoadComponentTask(Task<()>);
 
-pub fn load_scripts(mut load_queue: ResMut<ScriptLoadQueue>, wasm_assets: Res<Assets<Wasm>>) {
+#[derive(Resource)]
+pub struct WasmRuntime {
+    pub store: Store<()>,
+}
+
+impl Default for WasmRuntime {
+    fn default() -> Self {
+        let mut config = Config::default();
+        config.wasm_component_model(true);
+
+        let engine = Engine::new(&config).expect("Failed to create engine");
+        let store = Store::new(&engine, ());
+
+        Self { store }
+    }
+}
+
+pub fn load_scripts(
+    mut load_queue: ResMut<ScriptLoadQueue>,
+    mut runtime: ResMut<WasmRuntime>,
+    wasm_assets: Res<Assets<Wasm>>,
+) {
     if load_queue.0.is_empty() {
         return;
     }
@@ -25,30 +46,18 @@ pub fn load_scripts(mut load_queue: ResMut<ScriptLoadQueue>, wasm_assets: Res<As
         if let Some(wasm) = wasm_assets.get(handle) {
             to_remove.push(handle.clone());
 
-            let mut config = Config::default();
-            config.wasm_component_model(true);
+            let component =
+                match wasm_bridge::component::Component::new(runtime.store.engine(), &wasm.bytes) {
+                    Ok(c) => c,
+                    Err(e) => {
+                        error!("Failed to load component: {:?}", e);
+                        continue;
+                    }
+                };
 
-            let engine = match Engine::new(&config) {
-                Ok(e) => e,
-                Err(e) => {
-                    error!("Failed to create engine: {:?}", e);
-                    continue;
-                }
-            };
+            let linker = Linker::new(runtime.store.engine());
 
-            let mut store = Store::new(&engine, ());
-
-            let component = match wasm_bridge::component::Component::new(&engine, &wasm.bytes) {
-                Ok(c) => c,
-                Err(e) => {
-                    error!("Failed to load component: {:?}", e);
-                    continue;
-                }
-            };
-
-            let linker = Linker::new(&engine);
-
-            let (script, _) = match Script::instantiate(&mut store, &component, &linker) {
+            let (script, _) = match Script::instantiate(&mut runtime.store, &component, &linker) {
                 Ok(s) => s,
                 Err(e) => {
                     error!("Failed to instantiate component: {:?}", e);
@@ -56,7 +65,7 @@ pub fn load_scripts(mut load_queue: ResMut<ScriptLoadQueue>, wasm_assets: Res<As
                 }
             };
 
-            script.interface0.call_init(&mut store).unwrap();
+            script.interface0.call_init(&mut runtime.store).unwrap();
 
             info!("Script initialized");
         }

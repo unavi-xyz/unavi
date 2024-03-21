7765cc8e5e6cbb8515270228dd3c96e750421672
Kayh <basedlainpilled@gmail.com>
2024-03-21 16:24:12 -0400

    scripting wip

---
+++ crates/unavi-app/src/scripting/script/host.rs
@@ -0,0 +1,36 @@
+use wasm_bridge::{
+    component::{new_component_async, Linker},
+    AsContextMut, Store,
+};
+
+use super::StoreState;
+
+mod ui_imports {
+    wasm_bridge::component::bindgen!({
+        async: true,
+        path: "../../components/unavi-ui/wit/world.wit",
+        world: "imports",
+    });
+}
+
+mod ui_host {
+    wasm_bridge::component::bindgen!({
+        async: true,
+        path: "../../components/unavi-ui/wit/world.wit",
+        world: "host",
+    });
+}
+
+/// Add provided host components to the linker and store.
+pub async fn add_host_components(
+    linker: &mut Linker<StoreState>,
+    store: &mut Store<StoreState>,
+) -> Result<(), wasm_bridge::Error> {
+    // let bytes = UNAVI_UI.to_vec();
+    //
+    // let component = new_component_async(store.engine(), bytes).await?;
+    // let _ =
+    //     self::ui_host::Host_::instantiate_async(store.as_context_mut(), &component, linker).await?;
+
+    Ok(())
+}

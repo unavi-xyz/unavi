89e6fb982fccdf86ea088144f98768ff3a2a16c0
Kayh <basedlainpilled@gmail.com>
2024-03-21 22:35:17 -0400

    re-enable common.nix

--- deployments/nix/common.nix
+++ deployments/nix/common.nix
@@ -1,10 +1,9 @@
-{ ... }:
-{
-  # boot.tmp.cleanOnBoot = true;
-  # nix.settings.auto-optimise-store = true;
-  #
-  # services.journald.extraConfig = ''
-  #   SystemMaxUse=100M
-  #   MaxFileSec=30day
-  # '';
+{ ... }: {
+  boot.tmp.cleanOnBoot = true;
+  nix.settings.auto-optimise-store = true;
+
+  services.journald.extraConfig = ''
+    SystemMaxUse=100M
+    MaxFileSec=14day
+  '';
 }

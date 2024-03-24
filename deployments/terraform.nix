8b5425ff3d84f01f48db2ac25dcb0fb1fca752e7
Kayh <basedlainpilled@gmail.com>
2024-03-23 23:23:33 -0400

    use TF_WORKSPACE

--- deployments/terraform.nix
+++ deployments/terraform.nix
@@ -4,32 +4,21 @@ let
     system = localSystem;
     modules = [ ./config.nix ];
   };
-
-  workspaceInput = ''
-    if [ $# -eq 0 ]; then
-      workspace="default"
-    else
-      workspace=$1
-    fi
-  '';
 in {
   apps = {
     tf-apply = flake-utils.lib.mkApp {
       drv = pkgs.writeShellScriptBin "apply" ''
-        ${workspaceInput}
-
         if [[ -e config.tf.json ]]; then rm -f config.tf.json; fi
 
         cp ${terraformConfiguration} config.tf.json \
           && ${pkgs.terraform}/bin/terraform init \
-          && ${pkgs.terraform}/bin/terraform workspace select -or-create $workspace \
           && ${pkgs.terraform}/bin/terraform apply -auto-approve
 
-        mkdir -p deployments/terraform/$workspace
+        mkdir -p deployments/terraform/$TF_WORKSPACE
           
-        ${pkgs.terraform}/bin/terraform output -json > deployments/terraform/$workspace/terraform-output.json
+        ${pkgs.terraform}/bin/terraform output -json > deployments/terraform/$TF_WORKSPACE/terraform-output.json
 
-        jq -rc '.[].value.ip' deployments/terraform/$workspace/terraform-output.json | while read -r ip; do
+        jq -rc '.[].value.ip' deployments/terraform/$TF_WORKSPACE/terraform-output.json | while read -r ip; do
           echo "Adding $ip to known hosts"
           ssh-keyscan -H "$ip" >> "$HOME/.ssh/known_hosts"
         done
@@ -38,16 +27,13 @@ in {
 
     tf-destroy = flake-utils.lib.mkApp {
       drv = pkgs.writeShellScriptBin "destroy" ''
-        ${workspaceInput}
-
         if [[ -e config.tf.json ]]; then rm -f config.tf.json; fi
 
         cp ${terraformConfiguration} config.tf.json \
           && ${pkgs.terraform}/bin/terraform init \
-          && ${pkgs.terraform}/bin/terraform workspace select -or-create $workspace \
           && ${pkgs.terraform}/bin/terraform destroy -auto-approve
 
-        rm -rf deployments/terraform/$workspace
+        rm -rf deployments/terraform/$TF_WORKSPACE
       '';
     };
   };

7ba9acf79bd1e605d0cac2ea0922cf564e384235
Kayh <basedlainpilled@gmail.com>
2024-04-06 12:45:55 -0400

    update + simplify rcgen

--- crates/unavi-server/src/world_host/cert.rs
+++ crates/unavi-server/src/world_host/cert.rs
@@ -1,15 +1,13 @@
-use rcgen::{BasicConstraints, CertificateParams, IsCa, KeyUsagePurpose};
-use time::{Duration, OffsetDateTime};
 use wtransport::{
     tls::{Certificate, CertificateChain, PrivateKey},
     Identity,
 };
 
 pub fn generate_tls_identity() -> Identity {
-    let ca = new_ca();
+    let key = rcgen::generate_simple_self_signed(Vec::new()).unwrap();
 
-    let cert = ca.serialize_der().unwrap();
-    let key = ca.serialize_private_key_der();
+    let cert = key.cert.der().to_vec();
+    let key = key.key_pair.serialize_der();
 
     Identity::new(
         CertificateChain::new(vec![
@@ -17,26 +15,4 @@ pub fn generate_tls_identity() -> Identity {
         ]),
         PrivateKey::from_der_pkcs8(key),
     )
-}
-
-fn new_ca() -> rcgen::Certificate {
-    let mut params = CertificateParams::new(Vec::default());
-
-    let (yesterday, tomorrow) = validity_period();
-
-    params.is_ca = IsCa::Ca(BasicConstraints::Unconstrained);
-    params.key_usages.push(KeyUsagePurpose::DigitalSignature);
-    params.key_usages.push(KeyUsagePurpose::KeyCertSign);
-    params.key_usages.push(KeyUsagePurpose::CrlSign);
-    params.not_before = yesterday;
-    params.not_after = tomorrow;
-
-    rcgen::Certificate::from_params(params).unwrap()
-}
-
-fn validity_period() -> (OffsetDateTime, OffsetDateTime) {
-    let day = Duration::new(86400, 0);
-    let yesterday = OffsetDateTime::now_utc().checked_sub(day).unwrap();
-    let tomorrow = OffsetDateTime::now_utc().checked_add(day).unwrap();
-    (yesterday, tomorrow)
 }

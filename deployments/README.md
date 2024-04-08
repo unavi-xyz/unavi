96f25aee83f861b38f0034727fec2db009df43cb
Kayh <basedlainpilled@gmail.com>
2024-04-08 12:45:16 -0400

    remove terranix

--- deployments/README.md
+++ deployments/README.md
@@ -1,3 +1,3 @@
 # Deployments
 
-UNAVI NixOS deployments using [terranix](https://github.com/terranix/terranix) and [deploy-rs](https://github.com/serokell/deploy-rs).
+UNAVI NixOS deployments using [Terraform](https://github.com/hashicorp/terraform) and [deploy-rs](https://github.com/serokell/deploy-rs).
--- deployments/terraform.nix
+++ deployments/terraform.nix
@@ -1,24 +1,23 @@
-{ flake-utils, localSystem, pkgs, terranix, ... }:
-let
-  terraformConfiguration = terranix.lib.terranixConfiguration {
-    system = localSystem;
-    modules = [ ./config.nix ];
-  };
-in {
+{
+  flake-utils,
+  localSystem,
+  pkgs,
+  ...
+}:
+{
   apps = {
     tf-apply = flake-utils.lib.mkApp {
       drv = pkgs.writeShellScriptBin "apply" ''
-        if [[ -e config.tf.json ]]; then rm -f config.tf.json; fi
+        cd deployments
 
-        cp ${terraformConfiguration} config.tf.json \
-          && ${pkgs.terraform}/bin/terraform init \
+        ${pkgs.terraform}/bin/terraform init \
           && ${pkgs.terraform}/bin/terraform apply -auto-approve
 
-        mkdir -p deployments/terraform/$TF_WORKSPACE
+        mkdir -p terraform/$TF_WORKSPACE
           
-        ${pkgs.terraform}/bin/terraform output -json > deployments/terraform/$TF_WORKSPACE/terraform-output.json
+        ${pkgs.terraform}/bin/terraform output -json > terraform/$TF_WORKSPACE/terraform-output.json
 
-        jq -rc '.[].value.ip' deployments/terraform/$TF_WORKSPACE/terraform-output.json | while read -r ip; do
+        jq -rc '.[].value.ip' terraform/$TF_WORKSPACE/terraform-output.json | while read -r ip; do
           echo "Adding $ip to known hosts"
           ssh-keyscan -H "$ip" >> "$HOME/.ssh/known_hosts"
         done
@@ -27,16 +26,13 @@ in {
 
     tf-destroy = flake-utils.lib.mkApp {
       drv = pkgs.writeShellScriptBin "destroy" ''
-        if [[ -e config.tf.json ]]; then rm -f config.tf.json; fi
+        cd deployments
 
-        cp ${terraformConfiguration} config.tf.json \
-          && ${pkgs.terraform}/bin/terraform init \
+        ${pkgs.terraform}/bin/terraform init \
           && ${pkgs.terraform}/bin/terraform destroy -auto-approve
 
-        rm -rf deployments/terraform/$TF_WORKSPACE
+        rm -rf terraform/$TF_WORKSPACE
       '';
     };
   };
-
-  packages = { inherit terraformConfiguration; };
 }

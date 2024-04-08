5dc5ea21ca47ffe1b97d73994b8b4b18d7e20037
Kayh <basedlainpilled@gmail.com>
2024-04-08 16:26:00 -0400

    rename server

--- deployments/main.tf
+++ deployments/main.tf
@@ -36,7 +36,7 @@ resource "digitalocean_domain" "unavi_domain" {
 }
 
 resource "digitalocean_droplet" "unavi_server" {
-  name   = "server-${terraform.workspace}"
+  name   = "unavi-server-${terraform.workspace}"
   tags   = [terraform.workspace]
   region = "nyc3"
   size   = "s-1vcpu-1gb"

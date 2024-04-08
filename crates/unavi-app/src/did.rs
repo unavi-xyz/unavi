bad3b30e1ab233322cf3450c8fc05cc7a9763aaf
Kayh <basedlainpilled@gmail.com>
2024-04-07 22:46:08 -0400

    remove registry_protocol()

--- crates/unavi-app/src/did.rs
+++ crates/unavi-app/src/did.rs
@@ -5,7 +5,7 @@ use dwn::{
     store::SurrealStore,
 };
 use surrealdb::engine::local::Db;
-use wired_protocol::registry::{registry_protocol, REGISTRY_PROTOCOL_VERSION};
+use wired_protocol::registry::{registry_definition, REGISTRY_PROTOCOL_VERSION};
 
 const REGISTRY_DID: &str = "did:web:localhost%3A3000";
 
@@ -25,7 +25,7 @@ fn create_world(actor: Res<UserActor>, mut task_runner: AsyncTaskRunner<()>) {
 
     task_runner.start(async move {
         let mut create = actor.create_record().published(true).protocol(
-            registry_protocol(),
+            registry_definition().protocol,
             REGISTRY_PROTOCOL_VERSION,
             "world".to_string(),
         );

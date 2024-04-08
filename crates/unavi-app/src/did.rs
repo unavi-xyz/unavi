a1e1cade0efcd3eb61a1985dd10054866319b0a3
Kayh <basedlainpilled@gmail.com>
2024-04-07 22:01:53 -0400

    create wired-protocol crate

--- crates/unavi-app/src/did.rs
+++ crates/unavi-app/src/did.rs
@@ -2,17 +2,12 @@ use bevy::prelude::*;
 use bevy_async_task::AsyncTaskRunner;
 use dwn::{
     actor::{Actor, MessageBuilder},
-    message::descriptor::records::Version,
     store::SurrealStore,
 };
-use serde_json::Value;
 use surrealdb::engine::local::Db;
+use wired_protocol::registry::{registry_protocol, REGISTRY_PROTOCOL_VERSION};
 
 const REGISTRY_DID: &str = "did:web:localhost%3A3000";
-const REGISTRY_VERSION: &str = "0.0.1";
-const REGISTRY_PATH: &str = "world";
-const PROTOCOL_DEFINITION: &str =
-    include_str!("../../../wired-protocol/social/dwn/protocols/world-registry.json");
 
 pub struct DidPlugin;
 
@@ -26,16 +21,13 @@ impl Plugin for DidPlugin {
 pub struct UserActor(pub Actor<SurrealStore<Db>, SurrealStore<Db>>);
 
 fn create_world(actor: Res<UserActor>, mut task_runner: AsyncTaskRunner<()>) {
-    let value: Value = serde_json::from_str(PROTOCOL_DEFINITION).unwrap();
-    let protocol = value["protocol"].as_str().unwrap().to_string();
-
     let actor = actor.0.clone();
 
     task_runner.start(async move {
         let mut create = actor.create_record().published(true).protocol(
-            protocol,
-            Version::parse(REGISTRY_VERSION).unwrap(),
-            REGISTRY_PATH.to_string(),
+            registry_protocol(),
+            REGISTRY_PROTOCOL_VERSION,
+            "world".to_string(),
         );
 
         let reply = create.send(REGISTRY_DID).await;
---
+++ crates/wired-protocol/src/lib.rs
@@ -0,0 +1,3 @@
+//! Rust types for [wired-protocol](https://github.com/unavi-xyz/wired-protocol).
+
+pub mod registry;

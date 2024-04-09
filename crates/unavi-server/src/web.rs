f7583b9703a2869bcae1d9593b1faa795ad61c0b
Kayh <basedlainpilled@gmail.com>
2024-04-08 20:29:07 -0400

    route / to index.html

--- crates/unavi-server/src/web.rs
+++ crates/unavi-server/src/web.rs
@@ -1,9 +1,14 @@
-use axum::Router;
-use tower_http::services::ServeDir;
+use axum::{routing::get_service, Router};
+use tower_http::services::{ServeDir, ServeFile};
 
 pub fn router() -> Router {
     let mut path = std::env::current_dir().unwrap();
     path.push("web");
 
-    Router::new().fallback_service(ServeDir::new(path))
+    let mut index_path = path.clone();
+    index_path.push("index.html");
+
+    Router::new()
+        .route("/", get_service(ServeFile::new(index_path)))
+        .fallback_service(ServeDir::new(path))
 }

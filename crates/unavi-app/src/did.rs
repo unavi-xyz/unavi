403a4e9c777db0f6b94f2c34b84d8253e7d9b103
Kayh <basedlainpilled@gmail.com>
2024-04-09 14:37:11 -0400

    world creation

--- crates/unavi-app/src/did.rs
+++ crates/unavi-app/src/did.rs
@@ -1,11 +1,14 @@
 use bevy::prelude::*;
-use bevy_async_task::AsyncTaskRunner;
+use bevy_async_task::AsyncTask;
 use dwn::{
     actor::{Actor, MessageBuilder},
+    message::descriptor::iana_media_types::{Application, MediaType},
     store::SurrealStore,
 };
 use surrealdb::engine::local::Db;
-use wired_protocol::registry::{registry_definition, REGISTRY_PROTOCOL_VERSION};
+use wired_protocol::registry::{
+    registry_definition, registry_world_schema_url, World, REGISTRY_PROTOCOL_VERSION,
+};
 
 const REGISTRY_DID: &str = "did:web:localhost%3A3000";
 
@@ -20,21 +23,35 @@ impl Plugin for DidPlugin {
 #[derive(Resource)]
 pub struct UserActor(pub Actor<SurrealStore<Db>, SurrealStore<Db>>);
 
-fn create_world(actor: Res<UserActor>, mut task_runner: AsyncTaskRunner<()>) {
+fn create_world(actor: Res<UserActor>) {
     let actor = actor.0.clone();
 
-    task_runner.start(async move {
-        let mut create = actor.create_record().published(true).protocol(
-            registry_definition().protocol,
-            REGISTRY_PROTOCOL_VERSION,
-            "world".to_string(),
-        );
+    AsyncTask::new(async move {
+        let world = World {
+            name: Some("New World".to_string()),
+            ..Default::default()
+        };
 
-        match create.send(REGISTRY_DID).await {
+        match actor
+            .create_record()
+            .data(serde_json::to_vec(&world).unwrap())
+            .data_format(MediaType::Application(Application::Json))
+            .schema(registry_world_schema_url())
+            .protocol(
+                registry_definition().protocol,
+                REGISTRY_PROTOCOL_VERSION,
+                "world".to_string(),
+            )
+            .published(true)
+            .target(REGISTRY_DID.to_string())
+            .send(REGISTRY_DID)
+            .await
+        {
             Ok(reply) => info!("Created world: {:#?}", reply),
             Err(err) => {
                 error!("Failed to create world: {:#?}", err)
             }
         };
-    });
+    })
+    .blocking_recv();
 }

4d2ec490b70775cdde3ca848612197761b035609
Kayh <basedlainpilled@gmail.com>
2024-04-10 14:57:30 -0400

    fix host key verification

--- deployments/terraform.nix
+++ deployments/terraform.nix
@@ -39,7 +39,7 @@
 
         jq -rc '.[].value.ip' ../../output/$TF_WORKSPACE/terraform-output.json | while read -r ip; do
           echo "Adding $ip to known hosts"
-          ssh-keyscan -H "$ip" >> "$HOME/.ssh/known_hosts"
+          ssh-keyscan -H $ip >> $HOME/.ssh/known_hosts
         done
       '';
     };

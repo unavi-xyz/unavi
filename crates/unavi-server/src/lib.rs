6ea55570fd6347dda81277307b654f5a78ddee7c
Kayh <basedlainpilled@gmail.com>
2024-04-10 20:52:06 -0400

    refactor web deployment

--- crates/unavi-server/src/lib.rs
+++ crates/unavi-server/src/lib.rs
@@ -18,7 +18,6 @@ use surrealdb::{engine::local::SurrealKV, Surreal};
 use tracing::{info, info_span, Instrument};
 
 mod did_host;
-mod web;
 mod world_host;
 mod world_registry;
 
@@ -29,7 +28,6 @@ pub struct ServerOptions {
     pub domain: String,
     pub enable_did_host: bool,
     pub enable_dwn: bool,
-    pub enable_web: bool,
     pub enable_world_host: bool,
     pub enable_world_registry: bool,
     pub port: u16,
@@ -53,10 +51,6 @@ pub async fn start(opts: ServerOptions) -> std::io::Result<()> {
 
     if opts.enable_dwn {
         router = router.merge(dwn_server::router(dwn.clone()));
-    }
-
-    if opts.enable_web {
-        router = router.merge(web::router());
     }
 
     if opts.enable_world_host {
--- deployments/environments/channel/main.tf
+++ deployments/environments/channel/main.tf
@@ -18,7 +18,16 @@ resource "digitalocean_droplet" "unavi_server" {
   ]
 }
 
-resource "cloudflare_record" "unavi_subdomain" {
+resource "cloudflare_record" "subdomain_server" {
+  name    = terraform.workspace == "stable" ? "server" : "server-${terraform.workspace}"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+  value   = digitalocean_droplet.unavi_server.ipv4_address
+  zone_id = var.cloudflare_zone_id
+}
+
+resource "cloudflare_record" "subdomain_web" {
   name    = terraform.workspace == "stable" ? "app" : terraform.workspace
   proxied = true
   ttl     = 1
@@ -29,7 +38,8 @@ resource "cloudflare_record" "unavi_subdomain" {
 
 output "unavi_server" {
   value = {
-    domain = "${cloudflare_record.unavi_subdomain.name}.unavi.xyz"
+    domain_server = "${cloudflare_record.subdomain_server.name}.unavi.xyz"
+    domain_web = "${cloudflare_record.subdomain_web.name}.unavi.xyz"
     ip     = digitalocean_droplet.unavi_server.ipv4_address
     name   = digitalocean_droplet.unavi_server.name
   }

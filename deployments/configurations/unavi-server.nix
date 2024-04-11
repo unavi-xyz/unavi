c4f0f0daf884d9ceab38365995adee2f0b13cc54
Kayh <basedlainpilled@gmail.com>
2024-04-11 10:32:40 -0400

    maybe fix cors fr this time

--- deployments/configurations/unavi-server.nix
+++ deployments/configurations/unavi-server.nix
@@ -37,16 +37,18 @@
           "/" = {
             proxyPass = "http://localhost:3000";
             extraConfig = ''
-              add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
-              add_header 'Access-Control-Allow-Origin' '*';
-
               if ($request_method = 'OPTIONS') {
                   add_header 'Access-Control-Allow-Headers' '*';
+                  add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+                  add_header 'Access-Control-Allow-Origin' '*';
                   add_header 'Access-Control-Max-Age' 1728000;
                   add_header 'Content-Length' 0;
                   add_header 'Content-Type' 'text/plain; charset=utf-8';
                   return 204;
               }
+
+              add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+              add_header 'Access-Control-Allow-Origin' '*';
             '';
           };
         };

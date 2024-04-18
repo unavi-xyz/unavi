27feaf4f4f61f700ae2462adbc2ff9cb6523489a
Kayh <basedlainpilled@gmail.com>
2024-04-17 23:38:07 -0400

    process script commands

--- crates/unavi-app/src/scripting/script/state.rs
+++ crates/unavi-app/src/scripting/script/state.rs
@@ -3,30 +3,12 @@ use std::sync::mpsc::SyncSender;
 use wasm_bridge::component::ResourceTable;
 use wasm_bridge_wasi::{WasiCtx, WasiView};
 
+use super::commands::ScriptCommand;
+
 pub struct ScriptState {
     pub sender: SyncSender<ScriptCommand>,
     pub table: ResourceTable,
     pub wasi: WasiCtx,
-}
-
-pub enum ScriptCommand {
-    Query(Vec<ComponentId>),
-    RegisterComponent(Component),
-    SpawnEntity(SpawnEntity),
-}
-
-pub type ComponentId = u32;
-pub type EntityId = u32;
-pub type InstanceId = u32;
-
-pub struct Component {
-    pub id: ComponentId,
-    pub instance: InstanceId,
-}
-
-pub struct SpawnEntity {
-    pub components: Vec<Component>,
-    pub id: EntityId,
 }
 
 impl WasiView for ScriptState {

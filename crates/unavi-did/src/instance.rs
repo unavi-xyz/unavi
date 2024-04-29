5a2fffe862fa9cf6b410a7b7662ac68f5b62d887
Kayh <basedlainpilled@gmail.com>
2024-04-28 20:52:12 -0400

    set up unavi-did

--- crates/unavi-did/src/instance.rs
+++ crates/unavi-did/src/instance.rs
@@ -1,59 +1,32 @@
 use bevy::prelude::*;
 use bevy_async_task::{AsyncTaskRunner, AsyncTaskStatus};
 use dwn::{
-    actor::{Actor, MessageBuilder, ProcessMessageError},
+    actor::{MessageBuilder, ProcessMessageError},
     message::descriptor::iana_media_types::{Application, MediaType},
-    store::SurrealStore,
     MessageReply,
 };
-use surrealdb::engine::local::Db;
 use wired_protocol::{
     protocols::world_registry::{
-        registry_definition, registry_world_schema_url, REGISTRY_PROTOCOL_VERSION,
+        registry_definition, registry_instance_schema_url, REGISTRY_PROTOCOL_VERSION,
     },
-    schemas::world::World,
+    schemas::instance::Instance,
 };
 
-const ENV_REGISTRY_DID: Option<&str> = option_env!("UNAVI_REGISTRY_DID");
-const LOCAL_REGISTRY_DID: &str = "did:web:localhost%3A3000";
-
-fn registry_did() -> &'static str {
-    ENV_REGISTRY_DID.unwrap_or(LOCAL_REGISTRY_DID)
-}
-
-pub struct DidPlugin;
-
-impl Plugin for DidPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_event::<CreateWorld>()
-            .add_systems(Startup, create_initial_world)
-            .add_systems(Update, create_worlds);
-    }
-}
-
-#[derive(Resource)]
-pub struct UserActor(pub Actor<SurrealStore<Db>, SurrealStore<Db>>);
+use crate::{registry::registry_did, UserActor};
 
 #[derive(Event)]
-pub struct CreateWorld(pub World);
+pub struct CreateInstance(pub Instance);
 
-fn create_initial_world(mut _writer: EventWriter<CreateWorld>) {
-    // writer.send(CreateWorld(World {
-    //     name: Some("New World".to_string()),
-    //     ..Default::default()
-    // }));
-}
-
-fn create_worlds(
+pub fn handle_create_instance(
     actor: Res<UserActor>,
-    mut events: EventReader<CreateWorld>,
+    mut events: EventReader<CreateInstance>,
     mut task: AsyncTaskRunner<Result<MessageReply, ProcessMessageError>>,
 ) {
     match task.poll() {
         AsyncTaskStatus::Idle => {
             for event in events.read() {
                 let actor = actor.0.clone();
-                let world = event.0.clone();
+                let instance = event.0.clone();
 
                 task.start(async move {
                     let did = registry_did();
@@ -61,9 +34,9 @@ fn create_worlds(
 
                     actor
                         .create_record()
-                        .data(serde_json::to_vec(&world).unwrap())
+                        .data(serde_json::to_vec(&instance).unwrap())
                         .data_format(MediaType::Application(Application::Json))
-                        .schema(registry_world_schema_url())
+                        .schema(registry_instance_schema_url())
                         .protocol(
                             registry_definition().protocol,
                             REGISTRY_PROTOCOL_VERSION,

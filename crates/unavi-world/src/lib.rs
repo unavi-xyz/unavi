1f28dc9043bf4e9524b0a41ce01a7c51c4e2edbc
Kayh <basedlainpilled@gmail.com>
2024-04-30 22:27:36 -0400

    create world scene

--- crates/unavi-world/src/lib.rs
+++ crates/unavi-world/src/lib.rs
@@ -10,29 +10,18 @@ pub struct WorldPlugin;
 
 impl Plugin for WorldPlugin {
     fn build(&self, app: &mut App) {
-        app.init_state::<WorldState>()
-            .add_event::<JoinHome>()
-            .add_systems(Startup, home::join_home)
-            .add_systems(
-                OnEnter(WorldState::InWorld),
-                (scene::setup_scene, skybox::create_skybox),
-            )
+        app.add_event::<JoinHome>()
+            .add_systems(Startup, (home::join_home, scene::setup_lights))
             .add_systems(
                 Update,
                 (
                     home::handle_join_home,
-                    (skybox::add_skybox_to_cameras, skybox::process_cubemap)
-                        .run_if(in_state(WorldState::InWorld)),
+                    scene::create_world_scene,
+                    skybox::add_skybox_to_cameras,
+                    skybox::process_cubemap,
                 ),
             );
     }
-}
-
-#[derive(Clone, Debug, Default, Eq, Hash, PartialEq, States)]
-pub enum WorldState {
-    #[default]
-    LoadingWorld,
-    InWorld,
 }
 
 #[derive(Component)]
--- crates/unavi-world/src/scene.rs
+++ crates/unavi-world/src/scene.rs
@@ -1,15 +1,12 @@
 use bevy::{prelude::*, render::mesh::VertexAttributeValues};
+use bevy_vrm::mtoon::MtoonSun;
 use bevy_xpbd_3d::prelude::*;
 
-pub fn setup_scene(
-    asset_server: Res<AssetServer>,
-    mut ambient: ResMut<AmbientLight>,
-    mut commands: Commands,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    mut meshes: ResMut<Assets<Mesh>>,
-) {
-    ambient.color = Color::rgb(0.95, 0.95, 1.0);
+use crate::{WorldInstance, WorldRecord};
+
+pub fn setup_lights(mut commands: Commands, mut ambient: ResMut<AmbientLight>) {
     ambient.brightness = 40.0;
+    ambient.color = Color::rgb(0.95, 0.95, 1.0);
 
     commands.spawn((
         DirectionalLightBundle {
@@ -22,40 +19,60 @@ pub fn setup_scene(
             transform: Transform::from_xyz(-4.5, 10.0, 7.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..default()
         },
-        bevy_vrm::mtoon::MtoonSun,
+        MtoonSun,
     ));
+}
 
-    {
-        let ground_size = 30.0;
-        let ground_texture = asset_server.load("images/dev-white.png");
-        let ground_texture_scale = ground_size / 4.0;
+pub fn create_world_scene(
+    asset_server: Res<AssetServer>,
+    mut commands: Commands,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    worlds: Query<
+        Entity,
+        (
+            With<WorldRecord>,
+            With<WorldInstance>,
+            Without<Handle<Scene>>,
+        ),
+    >,
+) {
+    for entity in worlds.iter() {
+        let mut entity = commands.entity(entity);
+        entity.insert(SceneBundle::default());
 
-        let mut ground_mesh = Mesh::from(Cuboid {
-            half_size: Vec3::new(ground_size / 2.0, 0.05, ground_size / 2.0),
-        });
+        entity.with_children(|builder| {
+            let ground_size = 30.0;
+            let ground_texture = asset_server.load("images/dev-white.png");
+            let ground_texture_scale = ground_size / 4.0;
 
-        match ground_mesh.attribute_mut(Mesh::ATTRIBUTE_UV_0).unwrap() {
-            VertexAttributeValues::Float32x2(uvs) => {
-                for uv in uvs {
-                    uv[0] *= ground_texture_scale;
-                    uv[1] *= ground_texture_scale;
+            let mut ground_mesh = Mesh::from(Cuboid {
+                half_size: Vec3::new(ground_size / 2.0, 0.05, ground_size / 2.0),
+            });
+
+            match ground_mesh.attribute_mut(Mesh::ATTRIBUTE_UV_0).unwrap() {
+                VertexAttributeValues::Float32x2(uvs) => {
+                    for uv in uvs {
+                        uv[0] *= ground_texture_scale;
+                        uv[1] *= ground_texture_scale;
+                    }
                 }
+                _ => panic!(),
             }
-            _ => panic!(),
-        }
 
-        commands.spawn((
-            RigidBody::Static,
-            Collider::cuboid(ground_size, 0.05, ground_size),
-            PbrBundle {
-                mesh: meshes.add(ground_mesh),
-                material: materials.add(StandardMaterial {
-                    base_color_texture: Some(ground_texture.clone()),
+            builder.spawn((
+                RigidBody::Static,
+                Collider::cuboid(ground_size, 0.05, ground_size),
+                PbrBundle {
+                    mesh: meshes.add(ground_mesh),
+                    material: materials.add(StandardMaterial {
+                        base_color_texture: Some(ground_texture.clone()),
+                        ..default()
+                    }),
+                    transform: Transform::from_xyz(0.0, -0.1, 0.0),
                     ..default()
-                }),
-                transform: Transform::from_xyz(0.0, -0.1, 0.0),
-                ..default()
-            },
-        ));
+                },
+            ));
+        });
     }
 }

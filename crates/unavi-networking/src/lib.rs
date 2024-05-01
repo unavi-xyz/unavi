8921da314e74e9bd50dfa98dd2a920d5056396a5
Kayh <basedlainpilled@gmail.com>
2024-05-01 11:22:37 -0400

    refactor connection

--- crates/unavi-networking/src/lib.rs
+++ crates/unavi-networking/src/lib.rs
@@ -6,16 +6,6 @@ pub struct NetworkingPlugin;
 
 impl Plugin for NetworkingPlugin {
     fn build(&self, app: &mut App) {
-        app.add_event::<JoinWorld>()
-            .add_systems(Update, (join_world, connection::open_connection));
+        app.add_systems(Update, connection::poll_connection_thread);
     }
-}
-
-#[derive(Event)]
-pub struct JoinWorld {
-    pub did: String,
-}
-
-fn join_world(_commands: Commands, mut events: EventReader<JoinWorld>) {
-    for _event in events.read() {}
 }

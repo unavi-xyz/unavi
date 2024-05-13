4f6bab8789c48cc4cb8f61824fde193de1c123e5
Kayh <basedlainpilled@gmail.com>
2024-05-13 18:38:03 -0400

    simplify wtransport

--- crates/unavi-networking/src/native.rs
+++ crates/unavi-networking/src/native.rs
@@ -4,16 +4,9 @@ use xwt_core::endpoint::connect::{Connect, Connecting};
 use xwt_wtransport::Connection;
 
 pub async fn connect(addr: &str) -> Result<Connection> {
-    let config = wtransport::ClientConfig::builder().with_bind_default();
-
-    #[cfg(not(feature = "wtransport/dangerous-configuration"))]
-    let config = config.with_native_certs();
-
-    #[cfg(feature = "wtransport/dangerous-configuration")]
-    let config = {
-        tracing::warn!("WebTransport cert validation disabled for {}.", addr);
-        config.with_no_cert_validation()
-    };
+    let config = wtransport::ClientConfig::builder()
+        .with_bind_default()
+        .with_native_certs();
 
     let endpoint = wtransport::Endpoint::client(config.build())?;
     let endpoint = xwt_wtransport::Endpoint(endpoint);

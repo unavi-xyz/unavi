b06b7cbdd42f7eb110c76f117b72952e48b0e5aa
Kayh <basedlainpilled@gmail.com>
2024-05-16 17:35:16 -0400

    remove page rules

--- deployments/environments/universal/main.tf
+++ deployments/environments/universal/main.tf
@@ -7,22 +7,3 @@ resource "digitalocean_ssh_key" "kayh" {
   name       = "kayh"
   public_key = file("./keys/kayh.pub")
 }
-
-resource "cloudflare_page_rule" "cache_html" {
-  zone_id  = var.cloudflare_zone_id
-  target   = "*.unavi.xyz/*.html"
-  priority = 2
-  actions {
-    cache_level = "bypass"
-  }
-}
-
-resource "cloudflare_page_rule" "cache_all" {
-  zone_id  = var.cloudflare_zone_id
-  target   = "*.unavi.xyz/*"
-  priority = 1
-  actions {
-    cache_level    = "cache_everything"
-    edge_cache_ttl = 7200
-  }
-}

b5c0eb6c2d794b8d741520a2b93251b41147b6ec
Kayh <basedlainpilled@gmail.com>
2024-05-28 16:21:16 -0400

    add fallback despawn test

--- crates/unavi-avatar/src/lib.rs
+++ crates/unavi-avatar/src/lib.rs
@@ -8,8 +8,11 @@ pub struct AvatarPlugin;
 impl Plugin for AvatarPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(VrmPlugin)
-            .add_systems(Startup, fallback::init_fallback_material)
-            .add_systems(Update, fallback::spawn_fallbacks);
+            .add_systems(Startup, fallback::init_fallback_assets)
+            .add_systems(
+                Update,
+                (fallback::spawn_fallbacks, fallback::despawn_fallbacks),
+            );
     }
 }
 

3257eca66eb4e705e16259134772393013be591b
Kayh <basedlainpilled@gmail.com>
2024-05-28 17:22:03 -0400

    remove fallback avatar on vrm insert

--- crates/unavi-avatar/examples/avatar.rs
+++ crates/unavi-avatar/examples/avatar.rs
@@ -1,15 +1,22 @@
 use bevy::prelude::*;
 use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
-use unavi_avatar::{AvatarBundle, AvatarPlugin};
+use bevy_vrm::VrmBundle;
+use unavi_avatar::{AvatarPlugin, FallbackAvatar};
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, PanOrbitCameraPlugin, AvatarPlugin))
+        .add_plugins((
+            DefaultPlugins.set(AssetPlugin {
+                file_path: "../unavi-app/assets".to_string(),
+                ..Default::default()
+            }),
+            PanOrbitCameraPlugin,
+            AvatarPlugin,
+        ))
         .add_systems(Startup, (setup_avatar, setup_scene))
+        .add_systems(Update, load_avatar_two)
         .run();
 }
-
-const GROUND_SIZE: f32 = 10.0;
 
 fn setup_scene(
     mut ambient: ResMut<AmbientLight>,
@@ -21,20 +28,18 @@ fn setup_scene(
     ambient.color = Color::rgb(0.95, 0.95, 1.0);
 
     commands.spawn(DirectionalLightBundle {
-        transform: Transform::from_xyz(-4.5, 10.0, 7.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(4.5, 10.0, -7.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..Default::default()
     });
 
     commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(Cuboid {
-            half_size: Vec3::new(GROUND_SIZE / 2.0, 0.1, GROUND_SIZE / 2.0),
-        })),
+        mesh: meshes.add(Mesh::from(Cuboid::new(10.0, 0.1, 10.0))),
         material: materials.add(StandardMaterial::default()),
         transform: Transform::from_xyz(0.0, -0.1, 0.0),
         ..default()
     });
 
-    let mut transform = Transform::from_xyz(0.0, 3.0, 10.0);
+    let mut transform = Transform::from_xyz(0.0, 3.0, -10.0);
     transform.look_at(Vec3::new(0.0, 0.5, 0.0), Vec3::new(0.0, 1.0, 0.0));
 
     commands.spawn((
@@ -46,20 +51,50 @@ fn setup_scene(
     ));
 }
 
+#[derive(Component)]
+struct AvatarTwo;
+
 fn setup_avatar(mut commands: Commands) {
-    let mut scene = commands.spawn(SceneBundle::default());
+    commands.spawn((
+        FallbackAvatar,
+        SpatialBundle {
+            transform: Transform::from_xyz(-1.5, 0.0, 0.0),
+            ..Default::default()
+        },
+    ));
 
-    scene.with_children(|commands| {
-        commands.spawn((
-            AvatarBundle::default(),
-            TransformBundle::from_transform(Transform::from_xyz(-1.5, 0.0, 0.0)),
-            VisibilityBundle::default(),
-        ));
+    commands.spawn((
+        AvatarTwo,
+        FallbackAvatar,
+        SpatialBundle {
+            transform: Transform::from_xyz(1.5, 0.0, 0.0),
+            ..Default::default()
+        },
+    ));
+}
 
-        commands.spawn((
-            AvatarBundle::default(),
-            TransformBundle::from_transform(Transform::from_xyz(1.5, 0.0, 0.0)),
-            VisibilityBundle::default(),
-        ));
-    });
+/// Simulate an avatar that takes some time to load, showing a fallback in it's place.
+fn load_avatar_two(
+    asset_server: Res<AssetServer>,
+    mut commands: Commands,
+    mut done: Local<bool>,
+    query: Query<(Entity, &Transform), With<AvatarTwo>>,
+    time: Res<Time>,
+) {
+    if *done || time.elapsed_seconds() < 2.0 {
+        return;
+    }
+
+    *done = true;
+
+    for (entity, transform) in query.iter() {
+        commands.entity(entity).insert(VrmBundle {
+            scene_bundle: SceneBundle {
+                transform: transform.clone(),
+                ..Default::default()
+            },
+            vrm: asset_server.load("models/robot.vrm"),
+            ..Default::default()
+        });
+    }
 }

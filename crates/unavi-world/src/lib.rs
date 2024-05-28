46693a9d2a27c11be9eff153d3b172e10c326bac
Kayh <basedlainpilled@gmail.com>
2024-05-28 14:33:59 -0400

    add bevy_atmosphere

--- crates/unavi-world/src/lib.rs
+++ crates/unavi-world/src/lib.rs
@@ -1,27 +1,24 @@
 use bevy::prelude::*;
+use bevy_atmosphere::plugin::{AtmosphereCamera, AtmospherePlugin};
 use home::JoinHome;
 use wired_social::schemas::common::RecordLink;
 
 mod home;
 mod scene;
-mod skybox;
 
 pub struct WorldPlugin;
 
 impl Plugin for WorldPlugin {
     fn build(&self, app: &mut App) {
-        app.add_event::<JoinHome>()
-            .add_systems(
-                Startup,
-                (skybox::create_skybox, home::join_home, scene::setup_lights),
-            )
+        app.add_plugins(AtmospherePlugin)
+            .add_event::<JoinHome>()
+            .add_systems(Startup, (home::join_home, scene::setup_lights))
             .add_systems(
                 Update,
                 (
+                    add_atmosphere_cameras,
                     home::handle_join_home,
                     scene::create_world_scene,
-                    skybox::add_skybox_to_cameras,
-                    skybox::process_cubemap,
                 ),
             );
     }
@@ -35,3 +32,12 @@ pub struct InstanceRecord(pub RecordLink);
 
 #[derive(Component)]
 pub struct InstanceServer(pub String);
+
+fn add_atmosphere_cameras(
+    mut commands: Commands,
+    cameras: Query<Entity, (With<Camera3d>, Without<AtmosphereCamera>)>,
+) {
+    for entity in cameras.iter() {
+        commands.entity(entity).insert(AtmosphereCamera::default());
+    }
+}

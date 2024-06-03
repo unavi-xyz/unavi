067380b18f170d68a6be6fcc44508684d90bced9
Kayh <basedlainpilled@gmail.com>
2024-06-03 09:53:11 -0400

    add node parent / children

---
+++ wasm/test-wired-gltf/src/impls.rs
@@ -0,0 +1,29 @@
+use crate::bindings::wired::gltf::{
+    material::Material,
+    mesh::{Mesh, Primitive},
+    node::Node,
+};
+
+impl PartialEq for Material {
+    fn eq(&self, other: &Self) -> bool {
+        self.id() == other.id()
+    }
+}
+
+impl PartialEq for Mesh {
+    fn eq(&self, other: &Self) -> bool {
+        self.id() == other.id()
+    }
+}
+
+impl PartialEq for Node {
+    fn eq(&self, other: &Self) -> bool {
+        self.id() == other.id()
+    }
+}
+
+impl PartialEq for Primitive {
+    fn eq(&self, other: &Self) -> bool {
+        self.id() == other.id()
+    }
+}

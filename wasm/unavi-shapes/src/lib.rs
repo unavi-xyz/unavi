4b7ff61f2d868ee78a494b2046b99b0de20d2739
Kayh <basedlainpilled@gmail.com>
2024-06-07 18:54:56 -0400

    init unavi-shapes

---
+++ wasm/unavi-shapes/src/lib.rs
@@ -0,0 +1,42 @@
+use attributes::set_attributes;
+use bindings::{
+    exports::unavi::shapes::api::Guest,
+    wired::{
+        gltf::mesh::{create_mesh, Mesh},
+        math::types::Vec3,
+    },
+};
+use ncollide3d::na::Vector3;
+
+mod attributes;
+#[allow(warnings)]
+mod bindings;
+
+struct UnaviShapes;
+
+impl Guest for UnaviShapes {
+    fn cuboid(size: Vec3) -> Mesh {
+        let mesh = create_mesh();
+        let primitive = mesh.create_primitive();
+
+        let extents = Vector3::new(size.x, size.y, size.z);
+        let shape = ncollide3d::procedural::cuboid(&extents);
+
+        set_attributes(&primitive, shape);
+
+        mesh
+    }
+
+    fn sphere(radius: f32, sectors: u32, stacks: u32) -> Mesh {
+        let mesh = create_mesh();
+        let primitive = mesh.create_primitive();
+
+        let shape = ncollide3d::procedural::sphere(radius * 2.0, sectors, stacks, true);
+
+        set_attributes(&primitive, shape);
+
+        mesh
+    }
+}
+
+bindings::export!(UnaviShapes with_types_in bindings);

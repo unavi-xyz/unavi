216919563923e9f4e8a8979f08692afc8acf33fc
Kayh <basedlainpilled@gmail.com>
2024-06-20 23:24:30 -0400

    fix script testing

--- crates/unavi-scripting/src/host/mod.rs
+++ crates/unavi-scripting/src/host/mod.rs
@@ -1,8 +1,6 @@
 use anyhow::Result;
 use wasm_bridge::component::Linker;
 
-// use self::wired_gltf::{WiredGltfData, WiredGltfReceiver};
-
 use super::StoreState;
 
 pub mod wired_gltf;
@@ -27,7 +25,7 @@ mod tests {
         ScriptBundle,
     };
 
-    const UPDATES: usize = 5;
+    const UPDATES: usize = 4;
 
     pub async fn test_script(name: &str) {
         let mut app = App::new();
@@ -49,24 +47,28 @@ mod tests {
             Update,
             (
                 crate::load::load_scripts,
-                crate::execution::init_scripts,
                 crate::execution::update_scripts,
+                crate::execution::init_scripts,
                 super::wired_gltf::handler::handle_wired_gltf_actions,
             )
                 .chain(),
         );
 
         let asset_server = app.world.get_resource::<AssetServer>().unwrap();
-        app.world.spawn(ScriptBundle::load(name, asset_server));
+        let entity = app.world.spawn(ScriptBundle::load(name, asset_server)).id();
 
-        for _ in 0..UPDATES {
-            tokio::time::sleep(Duration::from_secs_f32(0.2)).await;
+        for _ in 0..3 {
+            tokio::time::sleep(Duration::from_secs_f32(0.5)).await;
             app.update();
         }
 
-        let mut loaded_scripts = app.world.query::<(Entity, &LoadedScript)>();
-        let len = loaded_scripts.iter(&app.world).len();
-        assert_eq!(len, 1);
+        let loaded_script = app.world.get::<LoadedScript>(entity);
+        assert!(loaded_script.is_some());
+
+        for _ in 0..UPDATES {
+            tokio::time::sleep(Duration::from_secs_f32(0.1)).await;
+            app.update();
+        }
     }
 
     #[tokio::test]
@@ -91,7 +93,7 @@ mod tests {
             }
 
             assert_eq!(found_constructs, 1);
-            assert_eq!(found_updates, UPDATES - 1);
+            assert_eq!(found_updates, UPDATES);
 
             Ok(())
         });
@@ -99,7 +101,7 @@ mod tests {
 
     #[tokio::test]
     #[traced_test]
-    async fn test_example_wired_gltf() {
+    async fn example_wired_gltf() {
         test_script("example:wired-gltf").await;
         assert!(!logs_contain("ERROR"));
         assert!(!logs_contain("error"));

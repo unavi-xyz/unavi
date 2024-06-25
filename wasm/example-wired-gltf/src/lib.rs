b4d44461201ee4c3bd11429cff9edbbdc1ac0c43
Kayh <basedlainpilled@gmail.com>
2024-06-25 14:53:53 -0400

    rainbow cube example!

--- wasm/example-wired-gltf/src/lib.rs
+++ wasm/example-wired-gltf/src/lib.rs
@@ -1,9 +1,11 @@
+use std::cell::RefCell;
+
 use bindings::{
     exports::wired::script::types::{Guest, GuestScript},
     unavi::shapes::api::create_cuboid,
     wired::{
         gltf::{
-            material::{create_material, Color},
+            material::{create_material, Color, Material},
             node::{create_node, Node},
         },
         log::api::{log, LogLevel},
@@ -17,6 +19,8 @@ mod bindings;
 mod impls;
 
 struct Script {
+    color_delta: RefCell<Color>,
+    material: Material,
     node: Node,
 }
 
@@ -47,10 +51,34 @@ impl GuestScript for Script {
             primitive.set_material(Some(&material));
         }
 
-        Script { node }
+        Script {
+            color_delta: RefCell::new(Color {
+                r: 0.01,
+                g: 0.002,
+                b: -0.003,
+                a: 0.0,
+            }),
+            material,
+            node,
+        }
     }
 
     fn update(&self, delta: f32) {
+        let mut color = self.material.color();
+        let mut color_delta = self.color_delta.borrow_mut();
+
+        color.r += color_delta.r;
+        color.g += color_delta.g;
+        color.b += color_delta.b;
+        color.a += color_delta.a;
+
+        color_delta.r = cycle(color_delta.r, color.r);
+        color_delta.g = cycle(color_delta.g, color.g);
+        color_delta.b = cycle(color_delta.b, color.b);
+        color_delta.a = cycle(color_delta.a, color.a);
+
+        self.material.set_color(color);
+
         let mut transform = self.node.transform();
 
         let mut quat = Quat::from_xyzw(
@@ -68,6 +96,14 @@ impl GuestScript for Script {
         transform.rotation.w = quat.w;
 
         self.node.set_transform(transform);
+    }
+}
+
+fn cycle(delta: f32, value: f32) -> f32 {
+    if value >= 1.0 || value <= 0.0 {
+        delta * -1.0
+    } else {
+        delta
     }
 }
 

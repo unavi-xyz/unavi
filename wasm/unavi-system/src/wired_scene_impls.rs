ac7a9db6d345ebe8034a8db159c243bc96537c20
Kayh <basedlainpilled@gmail.com>
2024-07-15 11:00:05 -0400

    create unavi-scene

--- wasm/unavi-system/src/wired_scene_impls.rs
+++ wasm/unavi-system/src/wired_scene_impls.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 
 use crate::bindings::wired::scene::{
+    gltf::Scene,
+    glxf::{GlxfNode, GlxfScene},
     material::Material,
     mesh::{Mesh, Primitive},
     node::Node,
@@ -25,6 +27,24 @@ impl PartialEq for Node {
 }
 
 impl PartialEq for Primitive {
+    fn eq(&self, other: &Self) -> bool {
+        self.id() == other.id()
+    }
+}
+
+impl PartialEq for Scene {
+    fn eq(&self, other: &Self) -> bool {
+        self.id() == other.id()
+    }
+}
+
+impl PartialEq for GlxfNode {
+    fn eq(&self, other: &Self) -> bool {
+        self.id() == other.id()
+    }
+}
+
+impl PartialEq for GlxfScene {
     fn eq(&self, other: &Self) -> bool {
         self.id() == other.id()
     }

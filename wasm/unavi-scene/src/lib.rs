2fc2f08cda4ea2c17146187be0f23a5635df796b
Kayh <basedlainpilled@gmail.com>
2024-07-15 11:00:05 -0400

    create unavi-scene

---
+++ wasm/unavi-scene/src/lib.rs
@@ -0,0 +1,91 @@
+use bindings::{
+    exports::unavi::scene::types::{Guest, Scene, SceneBorrow},
+    wired::scene::glxf::{get_root, Asset, AssetBorrow, Children, GlxfScene},
+};
+use scene::SceneImpl;
+
+#[allow(warnings)]
+mod bindings;
+
+mod scene;
+mod wired_math_impls;
+mod wired_scene_impls;
+
+struct GuestImpl;
+
+impl Guest for GuestImpl {
+    type Scene = SceneImpl;
+
+    fn list_scenes() -> Vec<Scene> {
+        let root = get_root();
+
+        let scene = match root.active_scene() {
+            Some(v) => v,
+            None => return Vec::new(),
+        };
+
+        scene
+            .nodes()
+            .into_iter()
+            .filter_map(|n| {
+                if let Some(Children::Asset(Asset::Gltf(asset))) = n.children() {
+                    Some((n, asset))
+                } else {
+                    None
+                }
+            })
+            .filter_map(|(node, asset)| {
+                let gltf = asset.document();
+                let scene = match gltf
+                    .active_scene()
+                    .or_else(|| gltf.default_scene())
+                    .or_else(|| gltf.list_scenes().into_iter().next())
+                {
+                    Some(v) => v,
+                    None => return None,
+                };
+
+                let scene = SceneImpl {
+                    asset,
+                    gltf,
+                    node,
+                    scene,
+                };
+
+                Some(Scene::new(scene))
+            })
+            .collect()
+    }
+
+    fn add_scene(value: SceneBorrow) {
+        let value = value.get::<SceneImpl>();
+
+        let root = get_root();
+        root.add_asset(&AssetBorrow::Gltf(&value.asset));
+        root.add_node(&value.node);
+
+        let scene = root.active_scene().unwrap_or_else(|| {
+            let value = GlxfScene::new();
+            root.add_scene(&value);
+            root.set_default_scene(&value);
+            root.set_active_scene(Some(&value));
+            value
+        });
+
+        scene.add_node(&value.node);
+    }
+
+    fn remove_scene(value: SceneBorrow) {
+        let value = value.get::<SceneImpl>();
+
+        let root = get_root();
+        root.remove_asset(&AssetBorrow::Gltf(&value.asset));
+        root.remove_node(&value.node);
+
+        for scene in root.list_scenes() {
+            scene.remove_node(&value.node)
+        }
+    }
+}
+
+bindings::export!(GuestImpl with_types_in bindings);
---
+++ wasm/unavi-scene/src/scene.rs
@@ -0,0 +1,71 @@
+use crate::bindings::{
+    exports::unavi::scene::types::GuestScene,
+    wired::{
+        math::types::Transform,
+        scene::{
+            gltf::{Gltf, Scene},
+            glxf::{AssetBorrow, AssetGltf, ChildrenBorrow, GlxfNode},
+            node::Node,
+        },
+    },
+};
+
+pub struct SceneImpl {
+    pub asset: AssetGltf,
+    pub gltf: Gltf,
+    pub node: GlxfNode,
+    pub scene: Scene,
+}
+
+impl GuestScene for SceneImpl {
+    fn new() -> Self {
+        let gltf = Gltf::new();
+        let scene = Scene::new();
+        gltf.add_scene(&scene);
+        gltf.set_default_scene(&scene);
+        gltf.set_active_scene(Some(&scene));
+
+        let asset = AssetGltf::new(&gltf);
+        let node = GlxfNode::new();
+        node.set_children(Some(&ChildrenBorrow::Asset(AssetBorrow::Gltf(&asset))));
+
+        Self {
+            asset,
+            gltf,
+            node,
+            scene,
+        }
+    }
+
+    fn active(&self) -> bool {
+        if let Some(v) = self.gltf.active_scene() {
+            v == self.scene
+        } else {
+            false
+        }
+    }
+    fn set_active(&self, value: bool) {
+        if value {
+            self.gltf.set_active_scene(Some(&self.scene));
+        } else {
+            self.gltf.set_active_scene(None);
+        }
+    }
+
+    fn transform(&self) -> Transform {
+        self.node.transform()
+    }
+    fn set_transform(&self, value: Transform) {
+        self.node.set_transform(value)
+    }
+
+    fn list_nodes(&self) -> Vec<Node> {
+        self.scene.nodes()
+    }
+    fn add_node(&self, value: &Node) {
+        self.scene.add_node(value)
+    }
+    fn remove_node(&self, value: &Node) {
+        self.scene.remove_node(value)
+    }
+}

838725be84b7f4e3d2292825e2876600fc741751
Kayh <basedlainpilled@gmail.com>
2024-07-31 17:58:41 -0400

    directional avatar movement

--- crates/unavi-avatar/src/animation/mod.rs
+++ crates/unavi-avatar/src/animation/mod.rs
@@ -1,5 +1,4 @@
-use std::time::Duration;
-
+use avian3d::parry::na::RealField;
 use bevy::{prelude::*, utils::HashMap};
 
 pub mod load;
@@ -19,8 +18,9 @@ pub enum AnimationName {
     WalkRight,
 }
 
+/// Defines animation clips for an avatar to use.
 #[derive(Component, Clone)]
-pub struct AvatarAnimations(pub HashMap<AnimationName, AvatarAnimation>);
+pub struct AvatarAnimationClips(pub HashMap<AnimationName, AvatarAnimation>);
 
 #[derive(Clone)]
 pub struct AvatarAnimation {
@@ -28,81 +28,125 @@ pub struct AvatarAnimation {
     pub gltf: Handle<Gltf>,
 }
 
-pub fn init_animation_transitions(
-    animation_nodes: Query<(&AvatarAnimationNodes, &Handle<AnimationGraph>)>,
-    mut animation_players: Query<(Entity, &mut AnimationPlayer, &Parent), Added<AnimationPlayer>>,
+pub fn init_animations(
+    animation_nodes: Query<&Handle<AnimationGraph>, With<AvatarAnimationNodes>>,
+    mut animation_players: Query<(Entity, &Parent), Added<AnimationPlayer>>,
     mut commands: Commands,
 ) {
-    for (entity, mut player, parent) in animation_players.iter_mut() {
-        if let Ok((nodes, graph)) = animation_nodes.get(parent.get()) {
-            let mut transitions = AnimationTransitions::default();
-
-            let animation = nodes.0[&AnimationName::Idle];
-            transitions
-                .play(&mut player, animation, Duration::ZERO)
-                .repeat();
-
-            commands.entity(entity).insert((graph.clone(), transitions));
+    for (entity, parent) in animation_players.iter_mut() {
+        if let Ok(graph) = animation_nodes.get(parent.get()) {
+            commands.entity(entity).insert(graph.clone());
         } else {
             error!(
-                "Failed to initialize AnimationTransitions. Animation nodes not found for animation player {}",
+                "Failed to initialize animation. Animation nodes not found for animation player {}",
                 entity
             );
         }
     }
 }
 
-const ANIMATION_DURATION: f32 = 0.0;
+const MAX_IDLE_VELOCITY: f32 = 0.3;
 
-pub fn play_target_animation(
-    mut avatars: Query<(&AvatarAnimationNodes, &TargetAnimation)>,
-    mut animation_players: Query<(&mut AnimationPlayer, &mut AnimationTransitions, &Parent)>,
+// Rate at which animations transition.
+const IDLE_RATE: f32 = 2.0;
+const WALK_RATE: f32 = 2.0;
+
+pub fn play_avatar_animations(
+    time: Res<Time>,
+    mut avatars: Query<(&AvatarAnimationNodes, &AverageVelocity, &Transform)>,
+    mut animation_players: Query<(&mut AnimationPlayer, &Parent)>,
 ) {
-    for (mut player, mut transitions, parent) in animation_players.iter_mut() {
-        if let Ok((nodes, target)) = avatars.get_mut(parent.get()) {
-            let new_animation = nodes.0[&target.name];
+    let delta = time.delta_seconds();
 
-            if Some(new_animation) != transitions.get_main_animation() {
-                info!("Playing animation: {:?}", target);
-
-                let animation = transitions
-                    .play(
-                        &mut player,
-                        new_animation,
-                        Duration::from_secs_f32(ANIMATION_DURATION),
-                    )
-                    .repeat();
-
-                if target.reverse {
-                    animation.set_speed(-1.0);
+    for (mut player, parent) in animation_players.iter_mut() {
+        if let Ok((nodes, avg, transform)) = avatars.get_mut(**parent) {
+            for node in nodes.0.values() {
+                if player.animation(*node).is_none() {
+                    let animation = player.play(*node).repeat();
+                    animation.set_weight(0.0);
                 }
             }
-        } else {
-            warn!("Target avatar animations not found");
+
+            // Idle.
+            let idle = player.animation_mut(nodes.0[&AnimationName::Idle]).unwrap();
+            let idle_weight = idle.weight();
+            // info!("idle: {}", idle_weight);
+
+            if avg.velocity.abs().element_sum() < MAX_IDLE_VELOCITY {
+                if idle_weight < 1.0 {
+                    idle.set_weight(idle_weight + (IDLE_RATE * delta));
+                }
+            } else if idle_weight > 0.0 {
+                idle.set_weight(idle_weight - (IDLE_RATE * delta));
+            }
+
+            // Walk.
+            let dir_forward = transform.rotation.mul_vec3(Vec3 {
+                x: 0.0,
+                y: 0.0,
+                z: -1.0,
+            });
+            let dir_left = transform.rotation.mul_vec3(Vec3 {
+                x: -1.0,
+                y: 0.0,
+                z: 0.0,
+            });
+
+            let vel_forward = avg.velocity * dir_forward;
+            let vel_left = avg.velocity * dir_left;
+
+            let forward = vel_forward.element_sum();
+            let left = vel_left.element_sum();
+
+            let walk = player.animation_mut(nodes.0[&AnimationName::Walk]).unwrap();
+            let walk_weight = walk.weight();
+            // info!("walk: {}", walk_weight);
+
+            let forward_bool = forward.is_sign_positive();
+
+            if forward_bool {
+                walk.set_speed(1.0);
+            } else {
+                walk.set_speed(-1.0);
+            }
+
+            if forward.abs() > MAX_IDLE_VELOCITY {
+                if walk_weight < 1.0 {
+                    walk.set_weight(walk_weight + (WALK_RATE * delta));
+                }
+            } else if walk_weight > 0.0 {
+                walk.set_weight(walk_weight - (WALK_RATE * delta));
+            }
+
+            // Left walk.
+            let l_walk = player
+                .animation_mut(nodes.0[&AnimationName::WalkLeft])
+                .unwrap();
+            let l_walk_weight = l_walk.weight();
+            // info!("l walk: {}", l_walk_weight);
+
+            if left > MAX_IDLE_VELOCITY {
+                if l_walk_weight < 1.0 {
+                    l_walk.set_weight(l_walk_weight + (WALK_RATE * delta));
+                }
+            } else if l_walk_weight > 0.0 {
+                l_walk.set_weight(l_walk_weight - (WALK_RATE * delta));
+            }
+
+            // Right walk.
+            let r_walk = player
+                .animation_mut(nodes.0[&AnimationName::WalkRight])
+                .unwrap();
+            let r_walk_weight = r_walk.weight();
+            // info!("r walk: {}", r_walk_weight);
+
+            if (-left) > MAX_IDLE_VELOCITY {
+                if r_walk_weight < 1.0 {
+                    r_walk.set_weight(r_walk_weight + (WALK_RATE * delta));
+                }
+            } else if r_walk_weight > 0.0 {
+                r_walk.set_weight(r_walk_weight - (WALK_RATE * delta));
+            }
         }
-    }
-}
-
-const MAX_IDLE_VELOCITY: f32 = 0.2;
-
-#[derive(Component, Debug, Default)]
-pub struct TargetAnimation {
-    pub name: AnimationName,
-    pub reverse: bool,
-}
-
-pub fn play_avatar_animations(mut avatars: Query<(&AverageVelocity, &mut TargetAnimation)>) {
-    for (avg, mut current_target) in avatars.iter_mut() {
-        let (name, reverse) = if avg.velocity.abs().element_sum() < MAX_IDLE_VELOCITY {
-            (AnimationName::Idle, false)
-        } else {
-            (
-                AnimationName::Walk,
-                avg.velocity.element_sum().is_sign_positive(),
-            )
-        };
-
-        current_target.name = name;
-        current_target.reverse = reverse;
     }
 }
--- crates/unavi-avatar/src/lib.rs
+++ crates/unavi-avatar/src/lib.rs
@@ -1,4 +1,4 @@
-use animation::{AvatarAnimations, TargetAnimation};
+use animation::AvatarAnimationClips;
 use avian3d::prelude::*;
 use bevy::prelude::*;
 use bevy_vrm::VrmPlugins;
@@ -17,10 +17,9 @@ impl Plugin for AvatarPlugin {
             .add_systems(
                 Update,
                 (
-                    animation::init_animation_transitions,
+                    animation::init_animations,
                     animation::load::load_animation_nodes,
                     animation::play_avatar_animations,
-                    animation::play_target_animation,
                     fallback::despawn_fallback_children,
                     fallback::remove_fallback_avatar,
                     fallback::spawn_fallback_children,
@@ -32,12 +31,11 @@ impl Plugin for AvatarPlugin {
 
 #[derive(Bundle)]
 pub struct AvatarBundle {
-    pub animations: AvatarAnimations,
+    pub animations: AvatarAnimationClips,
     pub collider: Collider,
     pub fallback: FallbackAvatar,
     pub rigid_body: RigidBody,
     pub spatial: SpatialBundle,
-    pub target_animation: TargetAnimation,
     pub velocity: AverageVelocity,
 }
 
@@ -45,14 +43,13 @@ const PLAYER_RADIUS: f32 = 0.2;
 const PLAYER_HEIGHT: f32 = 0.8;
 
 impl AvatarBundle {
-    pub fn new(animations: AvatarAnimations) -> Self {
+    pub fn new(animations: AvatarAnimationClips) -> Self {
         Self {
             animations,
             collider: Collider::capsule(PLAYER_RADIUS, PLAYER_HEIGHT),
             fallback: FallbackAvatar,
             rigid_body: RigidBody::Kinematic,
             spatial: SpatialBundle::default(),
-            target_animation: TargetAnimation::default(),
             velocity: AverageVelocity::default(),
         }
     }

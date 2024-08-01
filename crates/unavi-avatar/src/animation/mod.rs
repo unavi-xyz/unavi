e54c101a037bcf91f830ee540919d8821212e87e
Kayh <basedlainpilled@gmail.com>
2024-08-01 11:49:25 -0400

    interpolate animation weights

--- crates/unavi-avatar/src/animation/mod.rs
+++ crates/unavi-avatar/src/animation/mod.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*, utils::HashMap};
+use bevy::{animation::ActiveAnimation, prelude::*, utils::HashMap};
 
 pub mod load;
 mod mixamo;
@@ -17,7 +17,9 @@ pub enum AnimationName {
     WalkRight,
 }
 
-/// Defines animation clips for an avatar to use.
+#[derive(Component, Clone, Default)]
+pub struct AnimationWeights(pub HashMap<AnimationName, f32>);
+
 #[derive(Component, Clone)]
 pub struct AvatarAnimationClips(pub HashMap<AnimationName, AvatarAnimation>);
 
@@ -34,7 +36,9 @@ pub fn init_animations(
 ) {
     for (entity, parent) in animation_players.iter_mut() {
         if let Ok(graph) = animation_nodes.get(parent.get()) {
-            commands.entity(entity).insert(graph.clone());
+            commands
+                .entity(entity)
+                .insert((AnimationWeights::default(), graph.clone()));
         } else {
             error!(
                 "Failed to initialize animation. Animation nodes not found for animation player {}",
@@ -44,18 +48,24 @@ pub fn init_animations(
     }
 }
 
+const ALPHA_FACTOR: f32 = 0.01;
+const VELOCITY_FACTOR: f32 = 2.0;
 const WEIGHT_THRESHOLD: f32 = 0.02;
 
 pub fn play_avatar_animations(
+    time: Res<Time>,
     mut avatars: Query<(&AvatarAnimationNodes, &AverageVelocity, &Transform)>,
-    mut animation_players: Query<(&mut AnimationPlayer, &Parent)>,
+    mut animation_players: Query<(&mut AnimationWeights, &mut AnimationPlayer, &Parent)>,
 ) {
-    for (mut player, parent) in animation_players.iter_mut() {
+    let alpha = time.delta_seconds() * ALPHA_FACTOR;
+
+    for (mut weights, mut player, parent) in animation_players.iter_mut() {
         if let Ok((nodes, avg, transform)) = avatars.get_mut(**parent) {
-            for node in nodes.0.values() {
+            for (name, node) in nodes.0.iter() {
                 if player.animation(*node).is_none() {
                     let animation = player.play(*node).repeat();
                     animation.set_weight(0.0);
+                    weights.0.insert(*name, 0.0);
                 }
             }
 
@@ -73,43 +83,48 @@ pub fn play_avatar_animations(
             let vel_forward = avg.velocity * dir_forward;
             let vel_left = avg.velocity * dir_left;
 
-            let forward = vel_forward.element_sum();
-            let left = vel_left.element_sum();
+            let forward = vel_forward.element_sum() * VELOCITY_FACTOR;
+            let left = vel_left.element_sum() * VELOCITY_FACTOR;
 
             // Left walk.
             let mut l_walk_weight = left.max(0.0);
 
-            if l_walk_weight < WEIGHT_THRESHOLD {
-                l_walk_weight = 0.0;
-            }
-
-            player
-                .animation_mut(nodes.0[&AnimationName::WalkLeft])
-                .unwrap()
-                .set_weight(l_walk_weight);
+            apply_weight(
+                AnimationName::WalkLeft,
+                &mut l_walk_weight,
+                alpha,
+                &mut player,
+                nodes,
+                &mut weights,
+            );
 
             // Right walk.
             let mut r_walk_weight = left.min(0.0).abs();
 
-            if r_walk_weight < WEIGHT_THRESHOLD {
-                r_walk_weight = 0.0;
-            }
-
-            player
-                .animation_mut(nodes.0[&AnimationName::WalkRight])
-                .unwrap()
-                .set_weight(r_walk_weight);
+            apply_weight(
+                AnimationName::WalkRight,
+                &mut r_walk_weight,
+                alpha,
+                &mut player,
+                nodes,
+                &mut weights,
+            );
 
             // Walk.
             let mut walk_weight = forward.abs();
 
             walk_weight -= left.abs();
+            walk_weight -= l_walk_weight;
+            walk_weight -= r_walk_weight;
 
-            if walk_weight < WEIGHT_THRESHOLD {
-                walk_weight = 0.0;
-            }
-
-            let walk = player.animation_mut(nodes.0[&AnimationName::Walk]).unwrap();
+            let walk = apply_weight(
+                AnimationName::Walk,
+                &mut walk_weight,
+                alpha,
+                &mut player,
+                nodes,
+                &mut weights,
+            );
 
             if forward.is_sign_positive() {
                 walk.set_speed(1.0);
@@ -117,20 +132,47 @@ pub fn play_avatar_animations(
                 walk.set_speed(-1.0);
             }
 
-            walk.set_weight(walk_weight);
-
             // Idle.
             let mut idle_weight = 1.0;
             idle_weight -= l_walk_weight;
             idle_weight -= r_walk_weight;
             idle_weight -= walk_weight;
 
-            if idle_weight < WEIGHT_THRESHOLD {
-                idle_weight = 0.0;
-            }
+            apply_weight(
+                AnimationName::Idle,
+                &mut idle_weight,
+                alpha,
+                &mut player,
+                nodes,
+                &mut weights,
+            );
 
-            let idle = player.animation_mut(nodes.0[&AnimationName::Idle]).unwrap();
-            idle.set_weight(idle_weight);
+            info!("idle {}", idle_weight);
+            info!("walk {}", walk_weight);
         }
     }
+}
+
+fn apply_weight<'a>(
+    name: AnimationName,
+    weight: &mut f32,
+    alpha: f32,
+    player: &'a mut AnimationPlayer,
+    nodes: &AvatarAnimationNodes,
+    weights: &mut AnimationWeights,
+) -> &'a mut ActiveAnimation {
+    let prev = weights.0[&name];
+    *weight *= (1.0 - alpha) + prev * alpha;
+
+    if *weight < WEIGHT_THRESHOLD {
+        *weight = 0.0;
+    }
+
+    *weight = weight.min(1.0);
+
+    let animation = player.animation_mut(nodes.0[&name]).unwrap();
+    animation.set_weight(*weight);
+    weights.0.insert(name, *weight);
+
+    animation
 }

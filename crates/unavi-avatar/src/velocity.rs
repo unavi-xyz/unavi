5850cc7c8e7e13e3f330041617f2a80d57a9a0ff
Kayh <basedlainpilled@gmail.com>
2024-08-02 01:07:17 -0400

    fix player avatar alignment

--- crates/unavi-avatar/src/velocity.rs
+++ crates/unavi-avatar/src/velocity.rs
@@ -4,9 +4,12 @@ use bevy::prelude::*;
 /// via changes in [Transform].
 #[derive(Component)]
 pub struct AverageVelocity {
-    initialized: bool,
-    prev_translation: Vec3,
     pub alpha: f32,
+    pub initialized: bool,
+    pub prev_translation: Vec3,
+    /// If None, will use the current entity.
+    /// The target entity to track the velocity of.
+    pub target: Option<Entity>,
     pub velocity: Vec3,
 }
 
@@ -16,18 +19,26 @@ impl Default for AverageVelocity {
             alpha: 0.1,
             initialized: false,
             prev_translation: Vec3::default(),
+            target: None,
             velocity: Vec3::default(),
         }
     }
 }
 
 pub fn calc_average_velocity(
-    mut velocities: Query<(&mut AverageVelocity, &Transform)>,
+    mut velocities: Query<(Entity, &mut AverageVelocity)>,
     time: Res<Time>,
+    transforms: Query<&Transform>,
 ) {
     let delta_t = time.delta_seconds();
 
-    for (mut avg, transform) in velocities.iter_mut() {
+    for (entity, mut avg) in velocities.iter_mut() {
+        let target = avg.target.unwrap_or(entity);
+
+        let Ok(transform) = transforms.get(target) else {
+            continue;
+        };
+
         if !avg.initialized {
             avg.prev_translation.clone_from(&transform.translation);
             avg.initialized = true;

6bc694bcc9934bd80b83a39274bd9a5bbd16dbc2
Kayh <basedlainpilled@gmail.com>
2024-08-16 09:36:04 -0400

    setup wired-player api

--- crates/unavi-scripting/src/api/wired_player/mod.rs
+++ crates/unavi-scripting/src/api/wired_player/mod.rs
@@ -1,3 +1,8 @@
+use anyhow::Result;
+use wasm_bridge::component::{Linker, Resource};
+
+use crate::state::StoreState;
+
 mod player;
 
 wasm_bridge::component::bindgen!({
@@ -8,3 +13,18 @@ wasm_bridge::component::bindgen!({
         "wired:scene/node/node": crate::api::wired_scene::gltf::node::NodeRes,
     }
 });
+
+pub fn add_to_linker(linker: &mut Linker<StoreState>) -> Result<()> {
+    wired::player::api::add_to_linker(linker, |s| s)?;
+    Ok(())
+}
+
+impl wired::player::api::Host for StoreState {
+    fn list_players(&mut self) -> wasm_bridge::Result<Vec<Resource<player::Player>>> {
+        Ok(Vec::default())
+    }
+
+    fn local_player(&mut self) -> wasm_bridge::Result<Resource<player::Player>> {
+        todo!()
+    }
+}

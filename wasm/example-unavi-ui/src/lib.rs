9430e7a6d943d6dc567a5a7a2cfd0cd685c35ec0
Kayh <basedlainpilled@gmail.com>
2024-09-15 15:08:37 -0400

    add text box word wrap

--- wasm/example-unavi-ui/src/lib.rs
+++ wasm/example-unavi-ui/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{cell::Cell, f32::consts::FRAC_PI_2};
+use std::cell::Cell;
 
 use bindings::{
     exports::wired::script::types::{Guest, GuestScript},
@@ -14,7 +14,7 @@ use bindings::{
     },
     wired::{
         log::api::{log, LogLevel},
-        math::types::{Quat, Transform, Vec2, Vec3},
+        math::types::{Transform, Vec2, Vec3},
         scene::material::{Color, Material},
     },
 };
@@ -25,11 +25,11 @@ mod wired_math_impls;
 
 struct Script {
     button: Button,
-    clock: TextBox,
+    clock: Text,
     time: Cell<f32>,
 }
 
-const LENGTH: f32 = 5.0;
+const LENGTH: f32 = 10.0;
 const HEIGHT: f32 = 3.0;
 
 impl GuestScript for Script {
@@ -37,61 +37,64 @@ impl GuestScript for Script {
         let scene = Scene::new();
 
         // Background container
-        let container = Container::new(Vec3::new(LENGTH, HEIGHT, 0.1));
-        container.set_align_z(Alignment::End);
-        container.root().set_transform(Transform {
-            translation: Vec3::new(0.0, 0.2, -8.0),
-            rotation: Quat::from_rotation_x(FRAC_PI_2),
-            ..Default::default()
-        });
+        let bg = Container::new(Vec3::new(LENGTH, HEIGHT, 0.1));
+        bg.root()
+            .set_transform(Transform::from_translation(Vec3::new(0.0, 0.2, -8.0)));
+        scene.add_node(&bg.root());
 
-        scene.add_node(&container.root());
+        {
+            let rect = Rectangle::new(Vec2::new(LENGTH, HEIGHT)).to_physics_node();
+            bg.root().add_child(&rect);
 
-        let bg = Rectangle::new(Vec2::new(LENGTH, HEIGHT)).to_physics_node();
-        container.inner().add_child(&bg);
-
-        let material = Material::new();
-        material.set_color(Color {
-            r: 0.1,
-            g: 0.1,
-            b: 0.2,
-            a: 1.0,
-        });
-        bg.mesh()
-            .unwrap()
-            .list_primitives()
-            .iter()
-            .for_each(|p| p.set_material(Some(&material)));
+            let material = Material::new();
+            material.set_color(Color {
+                r: 0.1,
+                g: 0.1,
+                b: 0.2,
+                a: 1.0,
+            });
+            rect.mesh()
+                .unwrap()
+                .list_primitives()
+                .iter()
+                .for_each(|p| p.set_material(Some(&material)));
+        }
 
         // Button
         let button = Button::new(Container::new(Vec3::new(1.0, 0.5, 0.2)));
-        button
-            .root()
-            .inner()
-            .set_transform(Transform::from_rotation(Quat::from_rotation_x(-FRAC_PI_2)));
-        container.add_child(&button.root());
-
-        // TextBox
-        let clock_container = Container::new(Vec3::new(2.0, 0.5, 0.01));
-        clock_container.inner().set_transform(Transform {
-            translation: Vec3::new(1.5, 0.01, -1.0),
-            rotation: Quat::from_rotation_x(-FRAC_PI_2),
-            ..Default::default()
-        });
-        container.add_child(&clock_container);
-
-        let clock = TextBox::new(clock_container);
+        bg.add_child(&button.root());
 
         // Text
-        let label_text = Text::new("Elapsed:");
-        let label = scene.create_node();
-        clock.root().root().add_child(&label);
-        label.set_mesh(Some(&label_text.mesh()));
-        label.set_transform(Transform {
-            translation: Vec3::new(0.5, 0.01, -1.0),
-            rotation: Quat::from_rotation_x(-FRAC_PI_2),
-            ..Default::default()
-        });
+        let clock = {
+            let text = Text::new("");
+            let node = scene.create_node();
+            node.set_mesh(Some(&text.mesh()));
+            node.set_transform(Transform::from_translation(Vec3::new(4.0, 1.0, 0.01)));
+            bg.inner().add_child(&node);
+            text
+        };
+
+        // TextBox
+        {
+            let size = Vec2::new(1.5, 1.0);
+
+            let container = Container::new(Vec3::new(size.x, size.y, 0.01));
+            container.set_align_x(Alignment::Start);
+            container.set_align_y(Alignment::End);
+            container
+                .root()
+                .set_transform(Transform::from_translation(Vec3::new(2.0, 0.0, 0.01)));
+            bg.root().add_child(&container.root());
+
+            let outline = Rectangle::new(size).to_physics_node();
+            outline.set_mesh(None);
+            container.root().add_child(&outline);
+
+            let text = TextBox::new(container);
+            text.set_text("The quick brown fox jumps over the lazy dog.");
+
+            // TODO: Add buttons for controlling text settings
+        }
 
         Root::add_scene(&scene);
 
@@ -108,7 +111,7 @@ impl GuestScript for Script {
         let time = self.time.get();
         self.time.set(time + delta);
 
-        self.clock.text().set_text(&format!("{:.0}", time));
+        self.clock.set_text(&format!("{:.0}", time));
 
         if self.button.pressed() {
             log(LogLevel::Info, "Button pressed!");

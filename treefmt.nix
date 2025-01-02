4c69c07d570b0f21448b317fa12750dfec604bdb
Kayh <basedlainpilled@gmail.com>
2025-01-02 16:40:42 -0500

    update formatters

--- treefmt.nix
+++ treefmt.nix
@@ -2,10 +2,12 @@
 {
   projectRootFile = "flake.nix";
   programs = {
+    actionlint.enable = true;
+    mdformat.enable = true;
     nixfmt.enable = true;
-    prettier.enable = true;
     rustfmt.enable = true;
     taplo.enable = true;
     terraform.enable = true;
+    yamlfmt.enable = true;
   };
 }

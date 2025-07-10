3e039e8ba3f4cec8f06897a52b006719e8a3f2d6
Kayh <basedlainpilled@gmail.com>
2025-07-10 11:43:16 -0400

    add atmosphere + dev texture

--- crates/unavi-player/examples/player.rs
+++ crates/unavi-player/examples/player.rs
@@ -3,8 +3,10 @@ use avian3d::{
     PhysicsPlugins,
 };
 use bevy::{
-    color::palettes::tailwind::{BLUE_400, GRAY_200},
+    color::palettes::tailwind::BLUE_400,
+    pbr::{light_consts::lux, CascadeShadowConfigBuilder},
     prelude::*,
+    render::mesh::VertexAttributeValues,
 };
 use unavi_input::InputPlugin;
 use unavi_player::{PlayerPlugin, PlayerSpawner};
@@ -12,7 +14,10 @@ use unavi_player::{PlayerPlugin, PlayerSpawner};
 fn main() {
     App::new()
         .add_plugins((
-            DefaultPlugins,
+            DefaultPlugins.set(AssetPlugin {
+                file_path: "../unavi-app/assets".to_string(),
+                ..Default::default()
+            }),
             PhysicsPlugins::default(),
             InputPlugin,
             PlayerPlugin,
@@ -21,7 +26,11 @@ fn main() {
         .run();
 }
 
+const SIZE: f32 = 32.0;
+
 fn setup_scene(
+    asset_server: Res<AssetServer>,
+    mut ambient: ResMut<AmbientLight>,
     mut commands: Commands,
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -30,23 +39,48 @@ fn setup_scene(
     PlayerSpawner::default().spawn(&mut commands);
 
     // Lighting
-    commands.spawn((PointLight::default(), Transform::from_xyz(5.0, 5.0, 5.0)));
+    ambient.brightness = lux::OVERCAST_DAY;
     commands.spawn((
+        CascadeShadowConfigBuilder {
+            maximum_distance: SIZE * 1.2,
+            ..Default::default()
+        }
+        .build(),
         DirectionalLight {
-            illuminance: 4000.0,
+            illuminance: lux::RAW_SUNLIGHT,
             shadows_enabled: true,
             ..Default::default()
         },
-        Transform::default().looking_at(-Vec3::Y, Vec3::Z),
+        Transform::from_xyz(1.0, 0.4, 0.1).looking_at(Vec3::ZERO, Vec3::Y),
     ));
 
     // Ground
+    let ground_texture = asset_server.load("images/dev-white.png");
+
+    let mut ground_mesh = Plane3d::default().mesh().size(SIZE, SIZE).build();
+    match ground_mesh.attribute_mut(Mesh::ATTRIBUTE_UV_0).unwrap() {
+        VertexAttributeValues::Float32x2(uvs) => {
+            const TEXTURE_SCALE: f32 = 4.0;
+
+            let uv_scale = SIZE / TEXTURE_SCALE;
+
+            for uv in uvs {
+                uv[0] *= uv_scale;
+                uv[1] *= uv_scale;
+            }
+        }
+        _ => panic!(),
+    }
+
     commands.spawn((
         Collider::half_space(Vec3::Y),
-        Mesh3d(meshes.add(Plane3d::default().mesh().size(32.0, 32.0))),
-        MeshMaterial3d(materials.add(Color::from(GRAY_200))),
+        Mesh3d(meshes.add(ground_mesh)),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color_texture: Some(ground_texture),
+            perceptual_roughness: 0.8,
+            ..Default::default()
+        })),
         RigidBody::Static,
-        Transform::from_xyz(0.0, -1.0, 0.0),
     ));
 
     // Platform
@@ -55,6 +89,6 @@ fn setup_scene(
         Mesh3d(meshes.add(Cuboid::new(4.0, 1.0, 4.0))),
         MeshMaterial3d(materials.add(Color::from(BLUE_400))),
         RigidBody::Static,
-        Transform::from_xyz(-3.0, 0.0, -6.0),
+        Transform::from_xyz(-3.0, 0.75, -6.0),
     ));
 }

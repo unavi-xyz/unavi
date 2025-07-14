f2279745432720eb4a1d278b1622fa871d7e6155
Kayh <basedlainpilled@gmail.com>
2025-07-14 19:15:35 -0400

    script execution + logging

--- crates/unavi-script/src/lib.rs
+++ crates/unavi-script/src/lib.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
+use wasmtime::Config;
 
-mod engine;
+mod execute;
 mod load;
 mod wasm;
 
@@ -10,19 +11,33 @@ impl Plugin for ScriptPlugin {
     fn build(&self, app: &mut App) {
         app.register_asset_loader(wasm::WasmLoader)
             .init_asset::<wasm::Wasm>()
+            .init_non_send_resource::<execute::Runtimes>()
             .add_systems(Startup, add_unavi_system)
-            .add_systems(Update, (engine::tick_engine, load::load_scripts));
+            .add_systems(
+                Update,
+                (execute::execute_script_updates, load::load_scripts),
+            );
     }
 }
 
 fn add_unavi_system(asset_server: Res<AssetServer>, mut commands: Commands) {
-    let engine = commands.spawn(WasmEngine::default()).id();
+    let engine = {
+        let mut config = Config::new();
+        config.async_support(true).epoch_interruption(true);
+
+        let Ok(engine) = wasmtime::Engine::new(&config) else {
+            error!("Error creating wasmtime engine");
+            return;
+        };
+
+        commands.spawn(WasmEngine(engine)).id()
+    };
 
     let bin = asset_server.load("wasm/unavi/system.wasm");
-    commands.spawn((Script(engine), WasmBinary(bin)));
+    commands.spawn((Script(engine), WasmBinary(bin), Name::new("unavi:system")));
 }
 
-#[derive(Component, Default)]
+#[derive(Component)]
 #[require(EngineScripts)]
 pub struct WasmEngine(wasmtime::Engine);
 
@@ -33,9 +48,6 @@ pub struct EngineScripts(Vec<Entity>);
 #[derive(Component)]
 #[relationship(relationship_target = EngineScripts)]
 pub struct Script(pub Entity);
-
-#[derive(Component)]
-pub struct ScriptStore(pub wasmtime::Store<()>);
 
 #[derive(Component)]
 pub struct WasmBinary(pub Handle<wasm::Wasm>);

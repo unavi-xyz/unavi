41f0afe0fdcb232dc66b0db0420f57082583d2af
Kayh <basedlainpilled@gmail.com>
2025-10-01 00:54:30 -0400

    validate conflicting mutable queries

--- crates/wired-ecs/src/app.rs
+++ crates/wired-ecs/src/app.rs
@@ -4,7 +4,7 @@ use crate::{
     host_api::{register_system, write_component},
     param::{ParamGroup, ParamMeta},
     system::{BlindSystem, IntoSystem, System, function_system::FunctionSystem},
-    types::{Param as WParam, ParamData, Schedule, System as WSystem, SystemId},
+    types::{Param as WParam, ParamData, Query as WQuery, Schedule, System as WSystem, SystemId},
 };
 
 struct SystemCache {
@@ -32,6 +32,39 @@ impl App {
         let mutability = In::mutability();
         let metas = In::meta();
         let params: Vec<WParam> = In::register_params().into_iter().flatten().collect();
+
+        // Validate parameters.
+        let mut seen = Vec::<WQuery>::new();
+        for (param, meta) in params.iter().zip(metas.iter()) {
+            match param {
+                // Multiple queries within a system cannot hold mutable references to the same
+                // component.
+                WParam::Query(q) => {
+                    let Some(ParamMeta::Query { component_mut, .. }) = meta else {
+                        panic!("Invalid param meta")
+                    };
+
+                    let components = q
+                        .components
+                        .iter()
+                        .zip(component_mut.iter())
+                        .filter_map(|(c, is_mut)| if *is_mut { Some(*c) } else { None })
+                        .collect();
+
+                    for c in &components {
+                        if let Some(q1) = seen.iter().find(|x| x.components.contains(c)) {
+                            panic!(
+                                "Conflicting Query parameters (component {c}):\n- {q1:?}\n- {q:?}"
+                            )
+                        }
+                    }
+
+                    let mut q1 = q.clone();
+                    q1.components = components;
+                    seen.push(q1);
+                }
+            }
+        }
 
         let id = match register_system(&WSystem {
             schedule,

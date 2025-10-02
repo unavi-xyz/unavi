9984c25baafa78771f4fa75a72a962d01fbef995
Kayh <basedlainpilled@gmail.com>
2025-10-02 05:21:23 -0400

    refactor register system

--- crates/wired-ecs/src/app.rs
+++ crates/wired-ecs/src/app.rs
@@ -1,19 +1,15 @@
 use std::collections::BTreeMap;
 
 use crate::{
-    host_api::{register_system, write_component},
-    param::{ConcreteConstraint, ParamGroup, ParamMeta},
-    system::{BlindSystem, IntoSystem, System, function_system::FunctionSystem},
-    types::{Param as WParam, ParamData, Query as WQuery, Schedule, System as WSystem, SystemId},
+    host_api::write_component,
+    param::{ParamGroup, ParamMeta},
+    system::{
+        System,
+        function_system::FunctionSystem,
+        register_system::{RegisterSystem, SystemCache},
+    },
+    types::{Param as WParam, ParamData, Schedule, SystemId},
 };
-
-struct SystemCache {
-    immutable: bool,
-    mutability: Vec<bool>,
-    metas: Vec<Option<ParamMeta>>,
-    params: Vec<WParam>,
-    system: Box<dyn BlindSystem>,
-}
 
 #[derive(Default)]
 pub struct App {
@@ -23,151 +19,14 @@ pub struct App {
 impl App {
     pub fn add_system<F, In>(&mut self, schedule: Schedule, f: F) -> &mut Self
     where
+        F: 'static,
         In: ParamGroup + 'static,
-        F: IntoSystem<FunctionSystem<F, In>> + 'static,
         FunctionSystem<F, In>: System<In = In>,
     {
-        let system = F::into_system(f);
-
-        let mutability = In::mutability();
-        let metas = In::meta();
-        let params: Vec<WParam> = In::register_params().into_iter().flatten().collect();
-
-        // Validate parameters.
-        for (i, (p1, m1)) in params.iter().zip(metas.iter()).enumerate() {
-            match p1 {
-                WParam::Query(q1) => {
-                    let Some(ParamMeta::Query {
-                        component_mut: c_mut_1,
-                        constraints: cons_1,
-                        ..
-                    }) = m1
-                    else {
-                        panic!("Invalid param meta")
-                    };
-
-                    // A query cannot have conflicting constraints.
-                    for (a, con1) in cons_1.iter().enumerate() {
-                        for (b, con2) in cons_1.iter().enumerate() {
-                            if a == b {
-                                continue;
-                            }
-
-                            if con1 == con2 {
-                                println!("Query has duplicate constraint:\n- {q1:?}")
-                            }
-                        }
-
-                        match con1 {
-                            ConcreteConstraint::With { component } => {
-                                if q1.components.contains(component) {
-                                    println!("Query has redundent constraint:\n- {q1:?}");
-                                }
-                            }
-                            ConcreteConstraint::Without { component } => {
-                                if q1.components.contains(component) {
-                                    panic!("Query has conflicting constraint:\n- {q1:?}")
-                                }
-
-                                for con2 in cons_1 {
-                                    if let ConcreteConstraint::With { component: c2 } = con2
-                                        && *c2 == *component
-                                    {
-                                        panic!("Query has conflicting constraint:\n- {q1:?}")
-                                    }
-                                }
-                            }
-                        };
-                    }
-
-                    // If a query contains a mutable reference to a component, it must hold exclusive
-                    // access to that component for each entity. Any queries referencing that
-                    // same component must be mutually exculsive in their constraints.
-                    for (j, p2) in params.iter().enumerate() {
-                        if i == j {
-                            continue;
-                        }
-
-                        let WParam::Query(q2) = p2;
-
-                        let Some(ParamMeta::Query {
-                            constraints: cons_2,
-                            ..
-                        }) = &metas[j]
-                        else {
-                            panic!("Invalid param meta")
-                        };
-
-                        for (c1, is_mut) in q1.components.iter().zip(c_mut_1.iter()) {
-                            if !is_mut {
-                                continue;
-                            }
-
-                            if !q2.components.contains(c1) {
-                                continue;
-                            }
-
-                            // Both queries contain c1.
-                            // Ensure exclusivity.
-                            let mut with_1 = q1.components.clone();
-                            let mut without_1 = Vec::new();
-                            for con1 in cons_1 {
-                                match con1 {
-                                    ConcreteConstraint::With { component } => {
-                                        with_1.push(*component)
-                                    }
-                                    ConcreteConstraint::Without { component } => {
-                                        without_1.push(*component)
-                                    }
-                                }
-                            }
-
-                            let mut with_2 = q2.components.clone();
-                            let mut without_2 = Vec::new();
-                            for con2 in cons_2 {
-                                match con2 {
-                                    ConcreteConstraint::With { component } => {
-                                        with_2.push(*component)
-                                    }
-                                    ConcreteConstraint::Without { component } => {
-                                        without_2.push(*component)
-                                    }
-                                }
-                            }
-
-                            let is_exclusive = with_1.iter().any(|w1| without_2.contains(w1))
-                                || with_2.iter().any(|w2| without_1.contains(w2));
-
-                            if !is_exclusive {
-                                panic!(
-                                    "Query has mutable reference to component {c1}, but not exclusive access:\n- {q1:?}\n- {q2:?}"
-                                )
-                            }
-                        }
-                    }
-                }
-            }
+        let f = FunctionSystem::new(f);
+        for (id, sys) in f.register_system(schedule) {
+            self.systems.insert(id, sys);
         }
-
-        let id = match register_system(&WSystem {
-            schedule,
-            params: params.clone(),
-        }) {
-            Ok(id) => id,
-            Err(e) => panic!("Failed to register system: {e}"),
-        };
-
-        self.systems.insert(
-            id,
-            SystemCache {
-                immutable: !mutability.iter().any(|x| *x),
-                mutability,
-                metas,
-                params,
-                system: Box::new(system),
-            },
-        );
-
         self
     }
 

2b3ee8d9b5cc4f3a3e622e563c5cfc2ba1b1276b
Kayh <basedlainpilled@gmail.com>
2025-10-13 08:07:29 -0400

    create unavi plugin

--- crates/unavi/src/lib.rs
+++ crates/unavi/src/lib.rs
@@ -2,10 +2,10 @@ use std::sync::LazyLock;
 
 use bevy::prelude::*;
 use directories::ProjectDirs;
-use unavi_script::LoadScriptAsset;
 
-pub mod icon;
-pub mod scene;
+mod auth;
+mod icon;
+mod scene;
 
 pub static DIRS: LazyLock<ProjectDirs> = LazyLock::new(|| {
     let dirs = ProjectDirs::from("", "UNAVI", "unavi").expect("project dirs");
@@ -13,13 +13,34 @@ pub static DIRS: LazyLock<ProjectDirs> = LazyLock::new(|| {
     dirs
 });
 
-pub fn add_scripts(mut events: EventWriter<LoadScriptAsset>) {
-    events.write(LoadScriptAsset {
-        namespace: "unavi",
-        package: "system",
-    });
-    events.write(LoadScriptAsset {
-        namespace: "unavi",
-        package: "ui",
-    });
+pub struct UnaviPlugin;
+
+impl Plugin for UnaviPlugin {
+    fn build(&self, app: &mut App) {
+        DefaultPlugins
+            .build()
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    name: Some("unavi".to_string()),
+                    title: "UNAVI".to_string(),
+                    ..default()
+                }),
+                ..default()
+            })
+            .finish(app);
+
+        app.add_plugins((
+            avian3d::PhysicsPlugins::default(),
+            unavi_input::InputPlugin,
+            unavi_player::PlayerPlugin,
+        ))
+        .add_systems(
+            Startup,
+            (
+                icon::set_window_icon,
+                scene::spawn_lights,
+                scene::spawn_scene,
+            ),
+        );
+    }
 }

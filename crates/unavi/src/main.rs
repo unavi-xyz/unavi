11bfc8d6b2a23b3c734e90e54f119a28427fc3c3
Kayh <basedlainpilled@gmail.com>
2025-10-13 08:07:29 -0400

    create unavi plugin

--- crates/unavi/src/main.rs
+++ crates/unavi/src/main.rs
@@ -1,33 +1,7 @@
-use avian3d::PhysicsPlugins;
 use bevy::prelude::*;
-use unavi_input::InputPlugin;
-use unavi_player::PlayerPlugin;
 
 fn main() {
     let mut app = App::new();
-
-    DefaultPlugins
-        .build()
-        .set(WindowPlugin {
-            primary_window: Some(Window {
-                name: Some("unavi".to_string()),
-                title: "UNAVI".to_string(),
-                ..default()
-            }),
-            ..default()
-        })
-        .finish(&mut app);
-
-    app.add_plugins((PhysicsPlugins::default(), InputPlugin, PlayerPlugin));
-
-    app.add_systems(
-        Startup,
-        (
-            unavi::icon::set_window_icon,
-            unavi::scene::spawn_lights,
-            unavi::scene::spawn_scene,
-        ),
-    );
-
+    app.add_plugins(unavi::UnaviPlugin);
     app.run();
 }

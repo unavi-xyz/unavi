b8089a02bfe1e0967959a0842f942b0ecfac006b
Kayh <basedlainpilled@gmail.com>
2025-10-19 19:02:51 -0400

    serve did web

--- crates/unavi-server/src/lib.rs
+++ crates/unavi-server/src/lib.rs
@@ -1,8 +1,16 @@
 use std::{net::SocketAddr, sync::LazyLock};
 
 use anyhow::Context;
+use axum::{Json, Router};
 use directories::ProjectDirs;
+use p256::pkcs8::EncodePrivateKey;
+use spki::der::Encode;
 use tracing::{error, info};
+use xdid::core::{
+    did::{Did, MethodId, MethodName},
+    did_url::{DidUrl, RelativeDidUrl, RelativeDidUrlPath},
+    document::{Document, VerificationMethod, VerificationMethodMap},
+};
 use xwt_wtransport::wtransport;
 
 use crate::{cert::CertRes, server::Server};
@@ -13,14 +21,17 @@ mod server;
 pub static DIRS: LazyLock<ProjectDirs> = LazyLock::new(|| {
     let dirs = ProjectDirs::from("", "UNAVI", "unavi-server").expect("project dirs");
     std::fs::create_dir_all(dirs.config_dir()).expect("config dir");
+    std::fs::create_dir_all(dirs.data_local_dir()).expect("data local dir");
     dirs
 });
 
 pub async fn run_server(addr: SocketAddr) -> anyhow::Result<()> {
-    let CertRes { cert, private_key } = cert::get_or_generate_cert().await.context("get cert")?;
+    let CertRes { cert, key } = cert::get_or_generate_cert().await.context("get cert")?;
 
-    let private_key = wtransport::tls::PrivateKey::from_der_pkcs8(private_key);
-    let cert = wtransport::tls::Certificate::from_der(cert).context("create tls certificate")?;
+    let private_key =
+        wtransport::tls::PrivateKey::from_der_pkcs8(key.to_pkcs8_der()?.to_bytes().to_vec());
+    let cert =
+        wtransport::tls::Certificate::from_der(cert.to_der()?).context("create tls certificate")?;
     let cert_chain = wtransport::tls::CertificateChain::single(cert);
     let identity = wtransport::Identity::new(cert_chain, private_key);
 
@@ -31,18 +42,66 @@ pub async fn run_server(addr: SocketAddr) -> anyhow::Result<()> {
     let endpoint = wtransport::Endpoint::server(cfg).context("create wtranspart endpoint")?;
     let endpoint = xwt_wtransport::Endpoint(endpoint);
 
-    let svr = Server::new().await?;
+    let server_handler = Server::new().await?;
 
-    info!("UNAVI server running on {addr}");
+    tokio::spawn(async move {
+        info!("WebTransport listening on {addr}");
+        loop {
+            let incoming = endpoint.accept().await;
+            let svr = server_handler.clone();
+            tokio::spawn(async move {
+                if let Err(e) = svr.handle(incoming).await {
+                    error!("Handling error: {e:?}");
+                }
+            });
+        }
+    });
 
-    loop {
-        let incoming = endpoint.accept().await;
+    let app = Router::new().route("/.well-known/did.json", axum::routing::get(serve_did_json));
 
-        let svr = svr.clone();
-        tokio::spawn(async move {
-            if let Err(e) = svr.handle(incoming).await {
-                error!("Handling error: {e:?}");
-            }
-        });
-    }
+    info!("HTTP listening on {addr}");
+
+    axum_server::bind(addr)
+        .serve(app.into_make_service())
+        .await?;
+
+    Ok(())
+}
+
+async fn serve_did_json() -> Json<Document> {
+    let domain = std::env::var("DOMAIN").unwrap_or_else(|_| "localhost".into());
+    let did = Did {
+        method_name: MethodName("web".to_string()),
+        method_id: MethodId(domain),
+    };
+
+    let doc = Document {
+        id: did.clone(),
+        also_known_as: None,
+        assertion_method: Some(vec![VerificationMethod::RelativeUrl(RelativeDidUrl {
+            path: RelativeDidUrlPath::Empty,
+            query: None,
+            fragment: Some("owner".to_string()),
+        })]),
+        authentication: None,
+        capability_delegation: None,
+        capability_invocation: None,
+        controller: None,
+        key_agreement: None,
+        service: None,
+        verification_method: Some(vec![VerificationMethodMap {
+            id: DidUrl {
+                did: did.clone(),
+                fragment: Some("owner".to_string()),
+                query: None,
+                path_abempty: String::new(),
+            },
+            controller: did,
+            typ: "JsonWebKey2020".to_string(),
+            public_key_multibase: None,
+            public_key_jwk: None,
+        }]),
+    };
+
+    Json(doc)
 }

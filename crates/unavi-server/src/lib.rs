a16439fa7cad9ad819c21c5c51f43cee7ad53b97
Kayh <basedlainpilled@gmail.com>
2025-10-20 13:30:20 -0400

    server auth progress

--- crates/unavi-server/src/lib.rs
+++ crates/unavi-server/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{net::SocketAddr, sync::LazyLock};
+use std::{net::SocketAddr, sync::LazyLock, time::Duration};
 
 use anyhow::Context;
 use axum::{Json, Router};
@@ -6,16 +6,23 @@ use directories::ProjectDirs;
 use p256::pkcs8::EncodePrivateKey;
 use spki::der::Encode;
 use tracing::{error, info};
-use xdid::core::{
-    did::{Did, MethodId, MethodName},
-    did_url::{DidUrl, RelativeDidUrl, RelativeDidUrlPath},
-    document::{Document, VerificationMethod, VerificationMethodMap},
+use xdid::{
+    core::{
+        did::{Did, MethodId, MethodName},
+        did_url::{DidUrl, RelativeDidUrl, RelativeDidUrlPath},
+        document::{Document, VerificationMethod, VerificationMethodMap},
+    },
+    methods::key::{DidKeyPair, PublicKey},
 };
 use xwt_wtransport::wtransport;
 
-use crate::{cert::CertRes, server::Server};
+use crate::{
+    cert::CertRes,
+    server::{KEY_FRAGMENT, Server},
+};
 
 mod cert;
+mod key_pair;
 mod server;
 
 pub static DIRS: LazyLock<ProjectDirs> = LazyLock::new(|| {
@@ -26,6 +33,16 @@ pub static DIRS: LazyLock<ProjectDirs> = LazyLock::new(|| {
 });
 
 pub async fn run_server(addr: SocketAddr) -> anyhow::Result<()> {
+    let domain = std::env::var("DOMAIN")
+        .unwrap_or_else(|_| addr.to_string())
+        .replace("127.0.0.1", "localhost")
+        .replace(":", "%3A");
+    let did = Did {
+        method_name: MethodName("web".to_string()),
+        method_id: MethodId(domain.clone()),
+    };
+    info!("Running server as {did}");
+
     let CertRes { cert, key } = cert::get_or_generate_cert().await.context("get cert")?;
 
     let private_key =
@@ -42,22 +59,77 @@ pub async fn run_server(addr: SocketAddr) -> anyhow::Result<()> {
     let endpoint = wtransport::Endpoint::server(cfg).context("create wtranspart endpoint")?;
     let endpoint = xwt_wtransport::Endpoint(endpoint);
 
-    let server_handler = Server::new().await?;
+    let vc = key_pair::get_or_create_key()?;
 
-    tokio::spawn(async move {
-        info!("WebTransport listening on {addr}");
-        loop {
-            let incoming = endpoint.accept().await;
-            let svr = server_handler.clone();
-            tokio::spawn(async move {
-                if let Err(e) = svr.handle(incoming).await {
-                    error!("Handling error: {e:?}");
+    {
+        let did = did.clone();
+        let vc = vc.clone();
+        tokio::spawn(async move {
+            // Wait for did:web route to come online.
+            tokio::time::sleep(Duration::from_secs(5)).await;
+
+            loop {
+                let server_handler = match Server::new(did.clone(), vc.clone()).await {
+                    Ok(s) => s,
+                    Err(e) => {
+                        error!("Failed to crate sever handler: {e:?}");
+                        tokio::time::sleep(Duration::from_secs(30)).await;
+                        continue;
+                    }
+                };
+
+                info!("WebTransport listening on {addr}");
+                loop {
+                    let incoming = endpoint.accept().await;
+                    let svr = server_handler.clone();
+                    tokio::spawn(async move {
+                        if let Err(e) = svr.handle(incoming).await {
+                            error!("Handling error: {e:?}");
+                        }
+                    });
                 }
-            });
-        }
-    });
+            }
+        });
+    }
 
-    let app = Router::new().route("/.well-known/did.json", axum::routing::get(serve_did_json));
+    let app = Router::new().route(
+        "/.well-known/did.json",
+        axum::routing::get(|| async move {
+            let doc = Document {
+                id: did.clone(),
+                also_known_as: None,
+                assertion_method: Some(vec![VerificationMethod::RelativeUrl(RelativeDidUrl {
+                    path: RelativeDidUrlPath::Empty,
+                    query: None,
+                    fragment: Some(KEY_FRAGMENT.to_string()),
+                })]),
+                authentication: Some(vec![VerificationMethod::RelativeUrl(RelativeDidUrl {
+                    path: RelativeDidUrlPath::Empty,
+                    query: None,
+                    fragment: Some(KEY_FRAGMENT.to_string()),
+                })]),
+                capability_delegation: None,
+                capability_invocation: None,
+                controller: None,
+                key_agreement: None,
+                service: None,
+                verification_method: Some(vec![VerificationMethodMap {
+                    id: DidUrl {
+                        did: did.clone(),
+                        fragment: Some(KEY_FRAGMENT.to_string()),
+                        query: None,
+                        path_abempty: String::new(),
+                    },
+                    controller: did,
+                    typ: "JsonWebKey2020".to_string(),
+                    public_key_multibase: None,
+                    public_key_jwk: Some(vc.public().to_jwk()),
+                }]),
+            };
+
+            Json(doc)
+        }),
+    );
 
     info!("HTTP listening on {addr}");
 
@@ -66,42 +138,4 @@ pub async fn run_server(addr: SocketAddr) -> anyhow::Result<()> {
         .await?;
 
     Ok(())
-}
-
-async fn serve_did_json() -> Json<Document> {
-    let domain = std::env::var("DOMAIN").unwrap_or_else(|_| "localhost".into());
-    let did = Did {
-        method_name: MethodName("web".to_string()),
-        method_id: MethodId(domain),
-    };
-
-    let doc = Document {
-        id: did.clone(),
-        also_known_as: None,
-        assertion_method: Some(vec![VerificationMethod::RelativeUrl(RelativeDidUrl {
-            path: RelativeDidUrlPath::Empty,
-            query: None,
-            fragment: Some("owner".to_string()),
-        })]),
-        authentication: None,
-        capability_delegation: None,
-        capability_invocation: None,
-        controller: None,
-        key_agreement: None,
-        service: None,
-        verification_method: Some(vec![VerificationMethodMap {
-            id: DidUrl {
-                did: did.clone(),
-                fragment: Some("owner".to_string()),
-                query: None,
-                path_abempty: String::new(),
-            },
-            controller: did,
-            typ: "JsonWebKey2020".to_string(),
-            public_key_multibase: None,
-            public_key_jwk: None,
-        }]),
-    };
-
-    Json(doc)
 }

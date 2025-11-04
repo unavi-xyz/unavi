c109cb12bf6cd088489573e13bf10063e47367de
Kayh <basedlainpilled@gmail.com>
2025-11-03 22:07:37 -0500

    fix asset downloading in dev and parallelize downloads

--- crates/unavi-client/src/main.rs
+++ crates/unavi-client/src/main.rs
@@ -3,9 +3,10 @@
 use bevy::prelude::*;
 
 fn main() {
-    unavi_client::asset_download::ensure_model_assets().expect("failed to download model assets");
-
     let mut app = App::new();
     app.add_plugins(unavi_client::UnaviPlugin);
+
+    unavi_client::asset_download::ensure_model_assets().expect("failed to download model assets");
+
     app.run();
 }

ffb8fb11a57528ea7d4a1a9c35aa1fc847a1c9fe
Kayh <basedlainpilled@gmail.com>
2025-11-14 11:35:30 -0500

    transform stream prep

--- crates/unavi-client/src/space/stream/transform.rs
+++ crates/unavi-client/src/space/stream/transform.rs
@@ -1,9 +1,24 @@
-use bevy::prelude::*;
+use std::sync::mpsc::Sender;
+
+use bevy::{prelude::*, tasks::futures_lite::StreamExt};
+use tarpc::tokio_util::codec::LengthDelimitedCodec;
 use tokio::io::AsyncReadExt;
-use unavi_server_service::from_server::TransformMeta;
+use unavi_server_service::{
+    TRANSFORM_LENGTH_FIELD_LENGTH, TRANSFORM_MAX_FRAME_LENGTH, TrackingUpdate,
+    from_server::TransformMeta,
+};
 use wtransport::RecvStream;
 
-pub async fn recv_transform_stream(mut stream: RecvStream) -> anyhow::Result<()> {
+use crate::space::Space;
+
+pub struct RecievedTransform {
+    player: u64,
+}
+
+pub async fn recv_transform_stream(
+    mut stream: RecvStream,
+    transform_tx: Sender<RecievedTransform>,
+) -> anyhow::Result<()> {
     let meta_len = stream.read_u16().await? as usize;
 
     let mut meta_buf = vec![0; meta_len];
@@ -12,7 +27,36 @@ pub async fn recv_transform_stream(mut stream: RecvStream) -> anyhow::Result<()>
     let (meta, _) =
         bincode::decode_from_slice::<TransformMeta, _>(&meta_buf, bincode::config::standard())?;
 
-    info!("Got transform stream: {meta:?}");
+    let mut framed = LengthDelimitedCodec::builder()
+        .little_endian()
+        .length_field_length(TRANSFORM_LENGTH_FIELD_LENGTH)
+        .max_frame_length(TRANSFORM_MAX_FRAME_LENGTH)
+        .new_read(stream);
+
+    while let Some(frame) = framed.next().await {
+        let bytes = frame?;
+
+        let (update, _) = bincode::serde::decode_from_slice::<TrackingUpdate, _>(
+            &bytes,
+            bincode::config::standard(),
+        )?;
+
+        transform_tx.send(RecievedTransform {
+            player: meta.player,
+        })?;
+    }
 
     Ok(())
+}
+
+pub fn apply_player_transforms(spaces: Query<&Space>) {
+    for space in spaces.iter() {
+        let Ok(rx) = space.transform_rx.lock() else {
+            continue;
+        };
+
+        while let Ok(_update) = rx.try_recv() {
+            //
+        }
+    }
 }

f3c1234f4b50c9b52cb626de7a766546e574187b
Kayh <basedlainpilled@gmail.com>
2025-12-22 17:16:35 -0500

    init actor blob upload api

--- crates/wds/src/actor.rs
+++ crates/wds/src/actor.rs
@@ -1,13 +1,49 @@
+use blake3::Hash;
+use bytes::Bytes;
+use irpc::Client;
 use xdid::{core::did::Did, methods::key::p256::P256KeyPair};
+
+use crate::api::{ApiService, UploadBlob};
 
 pub struct Actor {
     did: Did,
     signing_key: P256KeyPair,
+    client: Client<ApiService>,
+    authenticated: bool,
 }
 
 impl Actor {
     #[must_use]
+    pub const fn new(did: Did, signing_key: P256KeyPair, client: Client<ApiService>) -> Self {
+        Self {
+            did,
+            signing_key,
+            client,
+            authenticated: false,
+        }
+    }
+
+    #[must_use]
     pub const fn did(&self) -> &Did {
         &self.did
+    }
+
+    /// Uplods bytes as to the WDS as a blob.
+    /// Returns the blob hash.
+    ///
+    /// # Errors
+    ///  
+    /// Errors if the blob could not be uploaded, such as if the client disconnects
+    /// or the storage quota is hit.
+    pub async fn upload_blob(&self, bytes: Bytes) -> anyhow::Result<Hash> {
+        let (tx, rx) = self.client.client_streaming(UploadBlob, 4).await?;
+
+        tx.send(bytes).await?;
+
+        let hash = rx
+            .await?
+            .map_err(|e| anyhow::anyhow!("upload failed: {e}"))?;
+
+        Ok(hash)
     }
 }
--- crates/wds/tests/blob.rs
+++ crates/wds/tests/blob.rs
@@ -1,1 +1,28 @@
+use rand::RngCore;
+use rstest::rstest;
 
+use crate::common::{DataStoreCtx, ctx};
+
+mod common;
+
+#[rstest]
+#[tokio::test]
+#[case(1)]
+#[case(32)]
+#[case(1024)]
+#[case(1024 * 1024)]
+#[awt]
+async fn test_blob_upload(
+    #[future] ctx: DataStoreCtx,
+    #[case] blob_len: usize,
+) -> anyhow::Result<()> {
+    let mut bytes = vec![0u8; blob_len];
+    rand::rng().fill_bytes(&mut bytes);
+
+    let hash = blake3::hash(&bytes);
+    let recieved_hash = ctx.alice.upload_blob(bytes.into()).await?;
+
+    assert_eq!(recieved_hash, hash);
+
+    Ok(())
+}

4d593fa62cc7b48bc6b6229fd5512c368c9f1a43
Kayh <basedlainpilled@gmail.com>
2025-12-28 01:37:16 -0500

    delete dwn + old server

--- crates/unavi-constants/src/lib.rs
+++ crates/unavi-constants/src/lib.rs
@@ -1,16 +1,6 @@
-use semver::Version;
-
-/// Wired Protocol version number.
-pub const WP_VERSION: Version = Version::new(0, 1, 0);
-
-pub const REMOTE_DWN_URL: &str = match option_env!("DWN_URL") {
+pub const REMOTE_WDS_URL: &str = match option_env!("WDS_URL") {
     Some(url) => url,
     None => "http://localhost:8080",
-};
-
-pub const SPACE_HOST_DID: &str = match option_env!("SPACE_DID") {
-    Some(did) => did,
-    None => "did:web:localhost%3A5000",
 };
 
 pub const CDN_ASSETS_URL: &str = "https://unavi.nyc3.cdn.digitaloceanspaces.com/assets";
--- crates/unavi-server/src/lib.rs
+++ crates/unavi-server/src/lib.rs
@@ -1,40 +1,21 @@
 use std::{
     net::{Ipv4Addr, SocketAddr, SocketAddrV4},
     sync::LazyLock,
-    time::Duration,
 };
 
-use anyhow::Context;
 use axum::{Json, Router};
 use directories::ProjectDirs;
-use tracing::{error, info};
-use wtransport::{Endpoint, Identity, ServerConfig};
+use tracing::info;
 use xdid::{
     core::{
         did::{Did, MethodId, MethodName},
         did_url::{DidUrl, RelativeDidUrl, RelativeDidUrlPath},
         document::{Document, VerificationMethod, VerificationMethodMap},
     },
-    methods::{
-        key::{DidKeyPair, PublicKey},
-        web::reqwest::Url,
-    },
+    methods::key::{DidKeyPair, PublicKey},
 };
 
-use crate::{
-    internal_msg::InternalMessage,
-    session::{KEY_FRAGMENT, SessionSpawner, SpawnerOptions},
-};
-
-const MAX_IDLE_TIMEOUT: Duration = Duration::from_mins(2);
-const KEEP_ALIVE_INTERVAL: Duration = Duration::from_secs(15);
-
-const INITIAL_WEB_TRANSPORT_DELAY: Duration = Duration::from_secs(2);
-const SPACE_RETRY_DELAY: Duration = Duration::from_secs(30);
-
-mod internal_msg;
 mod key_pair;
-mod session;
 
 pub static DIRS: LazyLock<ProjectDirs> = LazyLock::new(|| {
     let dirs = ProjectDirs::from("", "UNAVI", "unavi-server").expect("project dirs");
@@ -43,7 +24,6 @@ pub static DIRS: LazyLock<ProjectDirs> = LazyLock::new(|| {
 });
 
 pub struct ServerOptions {
-    pub remote_dwn: Url,
     pub in_memory: bool,
     pub port: u16,
 }
@@ -58,30 +38,7 @@ fn create_did(port: u16) -> (Did, String) {
     (did, domain)
 }
 
-fn create_server_identity(domain: &str) -> anyhow::Result<(Identity, String)> {
-    let identity = Identity::self_signed_builder()
-        .subject_alt_names([domain])
-        .from_now_utc()
-        .validity_days(14)
-        .build()?;
-    let cert_hash = identity.certificate_chain().as_slice()[0]
-        .hash()
-        .to_string();
-    Ok((identity, cert_hash))
-}
-
-fn create_server_config(
-    identity: Identity,
-    port: u16,
-) -> anyhow::Result<Endpoint<wtransport::endpoint::endpoint_side::Server>> {
-    let cfg = ServerConfig::builder()
-        .with_bind_default(port)
-        .with_identity(identity)
-        .max_idle_timeout(Some(MAX_IDLE_TIMEOUT))?
-        .keep_alive_interval(Some(KEEP_ALIVE_INTERVAL))
-        .build();
-    Endpoint::server(cfg).context("create wtransport endpoint")
-}
+const KEY_FRAGMENT: &str = "owner";
 
 fn create_did_document_route(did: Did, vc: &impl DidKeyPair) -> Router {
     let vc_public = vc.public().to_jwk();
@@ -133,73 +90,10 @@ fn create_did_document_route(did: Did, vc: &impl DidKeyPair) -> Router {
 pub async fn run_server(opts: ServerOptions) -> anyhow::Result<()> {
     let port = opts.port;
 
-    let (did, domain) = create_did(port);
+    let (did, _domain) = create_did(port);
     info!("Running server as {did}");
 
-    let (identity, cert_hash) = create_server_identity(&domain)?;
-    let endpoint = create_server_config(identity, port)?;
-
     let vc = key_pair::get_or_create_key(opts.in_memory)?;
-
-    let (msg_tx, msg_rx) = flume::bounded(16);
-
-    let spawner_opts = SpawnerOptions {
-        did: did.clone(),
-        domain,
-        in_memory: opts.in_memory,
-        msg_tx: msg_tx.clone(),
-        remote: opts.remote_dwn,
-        vc: vc.clone(),
-    };
-
-    // Internal message handler.
-    tokio::spawn(async move {
-        internal_msg::internal_message_handler(msg_rx).await;
-    });
-
-    // WebTransport handler.
-    tokio::spawn(async move {
-        // Wait for did:web route to come online.
-        tokio::time::sleep(INITIAL_WEB_TRANSPORT_DELAY).await;
-
-        loop {
-            let spawner = match SessionSpawner::new(spawner_opts.clone()).await {
-                Ok(s) => s,
-                Err(e) => {
-                    error!("Failed to create session spawner: {e:?}");
-                    tokio::time::sleep(SPACE_RETRY_DELAY).await;
-                    continue;
-                }
-            };
-
-            if let Err(e) = msg_tx
-                .send_async(InternalMessage::SetActor(spawner.ctx.actor.clone()))
-                .await
-            {
-                error!("Failed to set actor: {e:?}");
-                tokio::time::sleep(SPACE_RETRY_DELAY).await;
-                continue;
-            }
-
-            if let Err(e) = spawner.init_space_host(cert_hash.clone()).await {
-                error!("Failed to init space host: {e:?}");
-                tokio::time::sleep(SPACE_RETRY_DELAY).await;
-                continue;
-            }
-
-            info!("WebTransport listening on port {port}");
-            loop {
-                let incoming = endpoint.accept().await;
-                info!("Incoming session");
-                let spawner = spawner.clone();
-                tokio::spawn(async move {
-                    if let Err(e) = spawner.handle_session(incoming).await {
-                        error!("Handling error: {e:?}");
-                    }
-                });
-            }
-        }
-    });
 
     let app = create_did_document_route(did, &vc);
 

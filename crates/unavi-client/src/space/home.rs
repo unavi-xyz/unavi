aa84294080ee85a9fe502bb026bebec78169397f
Kayh <basedlainpilled@gmail.com>
2026-01-06 04:20:48 -0500

    beacon initial space discovery

--- crates/unavi-client/src/space/home.rs
+++ crates/unavi-client/src/space/home.rs
@@ -1,9 +1,13 @@
 use bevy::prelude::*;
-use wds::record::schema::{SCHEMA_HOME, SCHEMA_SPACE};
+use time::OffsetDateTime;
+use wds::record::schema::{SCHEMA_BEACON, SCHEMA_HOME, SCHEMA_SPACE};
 
-use crate::networking::{
-    WdsActors,
-    thread::{NetworkCommand, NetworkingThread},
+use crate::{
+    networking::{
+        WdsActors,
+        thread::{NetworkCommand, NetworkingThread},
+    },
+    space::beacon::Beacon,
 };
 
 pub fn join_home_space(actors: Res<WdsActors>, nt: Res<NetworkingThread>) {
@@ -18,11 +22,74 @@ pub fn join_home_space(actors: Res<WdsActors>, nt: Res<NetworkingThread>) {
             .expect("build tokio runtime");
 
         rt.block_on(async move {
-            if let Err(e) = join_home_space_inner(actors, command_tx).await {
+            if let Err(e) = discover_or_home(actors, command_tx).await {
                 error!("Failed to join home space: {e:?}");
             }
         });
     });
+}
+
+/// Attempt to discover a populated space to join.
+/// Else, join the user's home.
+/// Temporary measure until proper space traversal exists.
+async fn discover_or_home(
+    actors: WdsActors,
+    command_tx: flume::Sender<NetworkCommand>,
+) -> anyhow::Result<()> {
+    // Query for beacons.
+    let mut beacons = Vec::new();
+
+    if let Some(remote_actor) = &actors.remote {
+        let found = remote_actor
+            .query()
+            .schema(SCHEMA_BEACON.hash)
+            .send()
+            .await?;
+
+        let remote_host = *remote_actor.host();
+        let now = OffsetDateTime::now_utc().unix_timestamp();
+
+        for id in found {
+            match actors
+                .local
+                .read(id)
+                .sync_from(remote_host.into())
+                .send()
+                .await
+            {
+                Ok(doc) => {
+                    let Ok(beacon) = Beacon::load(&doc) else {
+                        debug!("invalid beacon documnt");
+                        continue;
+                    };
+
+                    if now >= beacon.expires {
+                        continue;
+                    }
+
+                    beacons.push(beacon);
+                }
+                Err(e) => {
+                    warn!("failed to sync beacon: {e:?}");
+                }
+            }
+        }
+    }
+
+    beacons.sort_by(|a, b| a.expires.cmp(&b.expires));
+
+    if let Some(beacon) = beacons.first() {
+        // Join most recent beacon.
+        info!("Found populated space: {}", beacon.space);
+        command_tx
+            .send_async(NetworkCommand::Join(beacon.space))
+            .await?;
+    } else {
+        // Join home.
+        join_home_space_inner(actors, command_tx).await?;
+    }
+
+    Ok(())
 }
 
 async fn join_home_space_inner(

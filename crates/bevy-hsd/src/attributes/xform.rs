ed92c895abc40e777174d1a6e6cb1dc7454ef213
Kayh <basedlainpilled@gmail.com>
2026-01-19 16:51:13 -0500

    make xform fields optional

--- crates/bevy-hsd/src/attributes/xform.rs
+++ crates/bevy-hsd/src/attributes/xform.rs
@@ -1,22 +1,23 @@
-use bevy::{
-    math::{Quat, Vec3},
-    transform::components::Transform,
-};
+use bevy::math::{Quat, Vec3};
 use loro::LoroValue;
 use loro_surgeon::Hydrate;
 
 use crate::attributes::Attribute;
 
-pub struct Xform(pub Transform);
+pub struct Xform {
+    pub rotate: Option<Quat>,
+    pub scale: Option<Vec3>,
+    pub translate: Option<Vec3>,
+}
 
 #[derive(Debug, Clone, Default, Hydrate)]
 struct XformRaw {
-    #[loro(with = "wired_schemas::conv::float_slice")]
-    rotate: [f64; 4],
-    #[loro(with = "wired_schemas::conv::float_slice")]
-    scale: [f64; 3],
-    #[loro(with = "wired_schemas::conv::float_slice")]
-    translate: [f64; 3],
+    #[loro(with = "wired_schemas::conv::float_slice::optional")]
+    rotate: Option<[f64; 4]>,
+    #[loro(with = "wired_schemas::conv::float_slice::optional")]
+    scale: Option<[f64; 3]>,
+    #[loro(with = "wired_schemas::conv::float_slice::optional")]
+    translate: Option<[f64; 3]>,
 }
 
 impl Hydrate for Xform {
@@ -24,30 +25,31 @@ impl Hydrate for Xform {
     fn hydrate(value: &LoroValue) -> Result<Self, loro_surgeon::HydrateError> {
         let raw = XformRaw::hydrate(value)?;
 
-        let tr = Transform::default()
-            .with_rotation(Quat::from_xyzw(
-                raw.rotate[0] as f32,
-                raw.rotate[1] as f32,
-                raw.rotate[2] as f32,
-                raw.rotate[3] as f32,
-            ))
-            .with_scale(Vec3::new(
-                raw.scale[0] as f32,
-                raw.scale[1] as f32,
-                raw.scale[2] as f32,
-            ))
-            .with_translation(Vec3::new(
-                raw.translate[0] as f32,
-                raw.translate[1] as f32,
-                raw.translate[2] as f32,
-            ));
-
-        Ok(Self(tr))
+        Ok(Self {
+            rotate: raw
+                .rotate
+                .map(|v| Quat::from_xyzw(v[0] as f32, v[1] as f32, v[2] as f32, v[3] as f32)),
+            scale: raw
+                .scale
+                .map(|v| Vec3::new(v[0] as f32, v[1] as f32, v[2] as f32)),
+            translate: raw
+                .translate
+                .map(|v| Vec3::new(v[0] as f32, v[1] as f32, v[2] as f32)),
+        })
     }
 }
 
 impl Attribute for Xform {
-    fn merge(self, next: Self) -> Self {
+    fn merge(mut self, next: Self) -> Self {
+        if let Some(v) = next.rotate {
+            self.rotate = Some(v);
+        }
+        if let Some(v) = next.scale {
+            self.scale = Some(v);
+        }
+        if let Some(v) = next.translate {
+            self.translate = Some(v);
+        }
         next
     }
 }

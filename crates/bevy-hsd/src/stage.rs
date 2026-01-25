55251b4b488ae3ba42fd716c42394f82ebc06390
Kayh <basedlainpilled@gmail.com>
2026-01-25 08:39:12 -0500

    begin flatten stage attributes

--- crates/bevy-hsd/src/stage.rs
+++ crates/bevy-hsd/src/stage.rs
@@ -1,4 +1,4 @@
-use loro::LoroMapValue;
+use blake3::Hash;
 use loro_surgeon::Hydrate;
 
 #[derive(Debug, Clone, Hydrate)]
@@ -14,6 +14,29 @@ pub struct LayerData {
 
 #[derive(Debug, Clone, Hydrate)]
 pub struct OpinionData {
-    pub attrs: LoroMapValue,
+    pub attrs: Attrs,
     pub node: i64,
+}
+
+#[derive(Debug, Clone, Hydrate)]
+pub struct Attrs {
+    #[loro(rename = "mesh/colors", with = "wired_schemas::conv::hash::optional")]
+    mesh_colors: Option<Hash>,
+    #[loro(rename = "xform/parent")]
+    xform_parent: Option<i64>,
+    #[loro(
+        rename = "xform/pos",
+        with = "wired_schemas::conv::float_slice::optional"
+    )]
+    xform_pos: Option<[f64; 3]>,
+    #[loro(
+        rename = "xform/rot",
+        with = "wired_schemas::conv::float_slice::optional"
+    )]
+    xform_rot: Option<[f64; 4]>,
+    #[loro(
+        rename = "xform/scale",
+        with = "wired_schemas::conv::float_slice::optional"
+    )]
+    xform_scale: Option<[f64; 3]>,
 }

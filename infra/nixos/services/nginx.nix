8b52038debdf657be1e155eaf5063b59d21e3b2e
Kayh <basedlainpilled@gmail.com>
2026-01-30 11:00:46 -0500

    deploy web client

--- infra/nixos/services/nginx.nix
+++ infra/nixos/services/nginx.nix
@@ -2,20 +2,37 @@
   config,
   lib,
   deployInfo,
+  pkgs,
+  self,
   ...
 }:
 let
   inherit (config.services.unavi-server) channel;
   inherit (deployInfo.${channel}) services;
 
-  virtualHosts = lib.mapAttrs (_serviceName: serviceConfig: {
+  # Split services into proxy (with port) and static (with static flag).
+  proxyServices = lib.filterAttrs (_: c: c ? port) services;
+  staticServices = lib.filterAttrs (_: c: c ? static && c.static) services;
+
+  webClientPkg = self.packages.${pkgs.system}."unavi-client-web-${channel}";
+
+  proxyHosts = lib.mapAttrs (_: cfg: {
     enableACME = true;
     forceSSL = true;
     locations."/" = {
-      proxyPass = "http://127.0.0.1:${toString serviceConfig.port}";
+      proxyPass = "http://127.0.0.1:${toString cfg.port}";
       proxyWebsockets = true;
     };
-  }) (lib.mapAttrs' (_name: config: lib.nameValuePair config.domain config) services);
+  }) (lib.mapAttrs' (_: c: lib.nameValuePair c.domain c) proxyServices);
+
+  staticHosts = lib.mapAttrs (_: _cfg: {
+    enableACME = true;
+    forceSSL = true;
+    root = webClientPkg;
+    locations."/" = {
+      tryFiles = "$uri $uri/ /index.html";
+    };
+  }) (lib.mapAttrs' (_: c: lib.nameValuePair c.domain c) staticServices);
 in
 {
   services.nginx = {
@@ -25,7 +42,7 @@ in
     recommendedOptimisation = true;
     recommendedGzipSettings = true;
 
-    inherit virtualHosts;
+    virtualHosts = proxyHosts // staticHosts;
   };
 
   security.acme = {

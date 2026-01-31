9caa3bd21b9b6362234e58b23c5b31db4e8125a5
Kayh <basedlainpilled@gmail.com>
2026-01-30 21:54:37 -0500

    parse args from url search params

--- crates/unavi-client/src/main.rs
+++ crates/unavi-client/src/main.rs
@@ -37,7 +37,46 @@ struct Args {
 }
 
 fn main() {
+    #[cfg(not(target_family = "wasm"))]
     let args = Args::parse();
+
+    #[cfg(target_family = "wasm")]
+    let args = {
+        web_sys::console::log_1(&"parsing url params".into());
+
+        let window = web_sys::window().expect("get window");
+        let search = window.location().search().expect("get search");
+        let params = web_sys::UrlSearchParams::new_with_str(&search).expect("parse search params");
+
+        let mut argv = vec!["app".to_string()];
+
+        for key in params.keys() {
+            let Ok(key) = key else {
+                continue;
+            };
+            let Some(key) = key.as_string() else {
+                continue;
+            };
+            let Some(value) = params.get(&key) else {
+                continue;
+            };
+            argv.push(format!("--{}", key));
+
+            if !value.is_empty() {
+                argv.push(value);
+            }
+        }
+
+        web_sys::console::log_1(&format!("{argv:?}").into());
+
+        match Args::try_parse_from(argv) {
+            Ok(a) => a,
+            Err(err) => {
+                web_sys::console::warn_1(&format!("Error parsing params: {err}").into());
+                Args::parse()
+            }
+        }
+    };
 
     let log_level = if args.debug_log {
         Level::DEBUG

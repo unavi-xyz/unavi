f7729a3e757a06cb3c8db579db27a1a71029663e
Kayh <basedlainpilled@gmail.com>
2026-02-11 03:54:10 -0500

    raycast input reading

---
+++ crates/unavi-input/src/raycast.rs
@@ -0,0 +1,49 @@
+use avian3d::prelude::*;
+use bevy::prelude::*;
+use schminput::BoolActionValue;
+
+use crate::{SqueezeDown, SqueezeUp, actions::CoreActions};
+
+#[derive(Component)]
+pub struct PrimaryRaycastInput;
+
+pub(crate) fn read_raycast_input(
+    mut commands: Commands,
+    core_actions: Res<CoreActions>,
+    ray_hits: Query<&RayHits, With<PrimaryRaycastInput>>,
+    bool_input: Query<&BoolActionValue>,
+    mut is_squeezing: Local<bool>,
+    mut squeeze_target: Local<Option<Entity>>,
+) {
+    let action = bool_input
+        .get(core_actions.squeeze_right)
+        .expect("squeeze action not found");
+
+    let was_squeezing = *is_squeezing;
+    *is_squeezing = action.any;
+
+    if was_squeezing
+        && !*is_squeezing
+        && let Some(target) = *squeeze_target
+    {
+        commands.entity(target).trigger(SqueezeUp);
+    }
+
+    let Ok(ray_hits) = ray_hits.single() else {
+        return;
+    };
+
+    // Max hits should be set to 1.
+    debug_assert!(ray_hits.iter().count() <= 1);
+
+    let Some(hit) = ray_hits.iter().next() else {
+        return;
+    };
+
+    // TODO render crosshair
+
+    if !was_squeezing && *is_squeezing {
+        commands.entity(hit.entity).trigger(SqueezeDown);
+        *squeeze_target = Some(hit.entity);
+    }
+}

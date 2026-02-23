31d0fcbfdcc9c4fb2369ab88fccfbe26c92a9ae0
Kayh <basedlainpilled@gmail.com>
2026-02-23 11:50:12 -0500

    more model changes

--- crates/unavi-script/src/api/wired/scene/node.rs
+++ crates/unavi-script/src/api/wired/scene/node.rs
@@ -7,25 +7,124 @@ pub struct HostNode {
 }
 
 impl super::bindings::wired::scene::types::HostNode for WiredSceneRt {
-    async fn new(&mut self) -> wasmtime::Result<wasmtime::component::Resource<HostNode>> {
-        let id = "todo".into();
-        let res = self.table.push(HostNode { id })?;
-        Ok(res)
-    }
-
-    async fn document(
+    async fn name(
         &mut self,
         self_: wasmtime::component::Resource<HostNode>,
-    ) -> wasmtime::Result<wasmtime::component::__internal::Vec<u8>> {
+    ) -> wasmtime::Result<Option<wasmtime::component::__internal::String>> {
+        todo!()
+    }
+    async fn set_name(
+        &mut self,
+        self_: wasmtime::component::Resource<HostNode>,
+        value: Option<wasmtime::component::__internal::String>,
+    ) -> wasmtime::Result<()> {
         todo!()
     }
 
-    async fn id(
+    async fn parent(
         &mut self,
         self_: wasmtime::component::Resource<HostNode>,
-    ) -> wasmtime::Result<String> {
-        let node = self.table.get(&self_)?;
-        Ok(node.id.to_string())
+    ) -> wasmtime::Result<Option<wasmtime::component::Resource<HostNode>>> {
+        todo!()
+    }
+    async fn children(
+        &mut self,
+        self_: wasmtime::component::Resource<HostNode>,
+    ) -> wasmtime::Result<
+        wasmtime::component::__internal::Vec<wasmtime::component::Resource<HostNode>>,
+    > {
+        todo!()
+    }
+    async fn add_child(
+        &mut self,
+        self_: wasmtime::component::Resource<HostNode>,
+        child: wasmtime::component::Resource<HostNode>,
+    ) -> wasmtime::Result<()> {
+        todo!()
+    }
+    async fn remove_child(
+        &mut self,
+        self_: wasmtime::component::Resource<HostNode>,
+        child: wasmtime::component::Resource<HostNode>,
+    ) -> wasmtime::Result<()> {
+        todo!()
+    }
+
+    async fn rotation(
+        &mut self,
+        self_: wasmtime::component::Resource<HostNode>,
+    ) -> wasmtime::Result<super::bindings::wired::scene::types::Quat> {
+        todo!()
+    }
+    async fn scale(
+        &mut self,
+        self_: wasmtime::component::Resource<HostNode>,
+    ) -> wasmtime::Result<super::bindings::wired::scene::types::Vec3> {
+        todo!()
+    }
+    async fn transform(
+        &mut self,
+        self_: wasmtime::component::Resource<HostNode>,
+    ) -> wasmtime::Result<super::bindings::wired::scene::types::Transform> {
+        todo!()
+    }
+    async fn translation(
+        &mut self,
+        self_: wasmtime::component::Resource<HostNode>,
+    ) -> wasmtime::Result<super::bindings::wired::scene::types::Vec3> {
+        todo!()
+    }
+    async fn set_rotation(
+        &mut self,
+        self_: wasmtime::component::Resource<HostNode>,
+        value: super::bindings::wired::scene::types::Quat,
+    ) -> wasmtime::Result<()> {
+        todo!()
+    }
+    async fn set_scale(
+        &mut self,
+        self_: wasmtime::component::Resource<HostNode>,
+        value: super::bindings::wired::scene::types::Vec3,
+    ) -> wasmtime::Result<()> {
+        todo!()
+    }
+    async fn set_transform(
+        &mut self,
+        self_: wasmtime::component::Resource<HostNode>,
+        value: super::bindings::wired::scene::types::Transform,
+    ) -> wasmtime::Result<()> {
+        todo!()
+    }
+    async fn set_translation(
+        &mut self,
+        self_: wasmtime::component::Resource<HostNode>,
+        value: super::bindings::wired::scene::types::Vec3,
+    ) -> wasmtime::Result<()> {
+        todo!()
+    }
+    async fn global_transform(
+        &mut self,
+        self_: wasmtime::component::Resource<HostNode>,
+    ) -> wasmtime::Result<super::bindings::wired::scene::types::Transform> {
+        todo!()
+    }
+
+    async fn material(
+        &mut self,
+        self_: wasmtime::component::Resource<HostNode>,
+    ) -> wasmtime::Result<
+        Option<wasmtime::component::Resource<super::bindings::wired::scene::types::Material>>,
+    > {
+        todo!()
+    }
+    async fn set_material(
+        &mut self,
+        self_: wasmtime::component::Resource<HostNode>,
+        value: Option<
+            wasmtime::component::Resource<super::bindings::wired::scene::types::Material>,
+        >,
+    ) -> wasmtime::Result<()> {
+        todo!()
     }
 
     async fn mesh(
@@ -40,6 +139,33 @@ impl super::bindings::wired::scene::types::HostNode for WiredSceneRt {
         &mut self,
         self_: wasmtime::component::Resource<HostNode>,
         value: Option<wasmtime::component::Resource<super::bindings::wired::scene::types::Mesh>>,
+    ) -> wasmtime::Result<()> {
+        todo!()
+    }
+
+    async fn collider(
+        &mut self,
+        self_: wasmtime::component::Resource<HostNode>,
+    ) -> wasmtime::Result<Option<super::bindings::wired::scene::types::Collider>> {
+        todo!()
+    }
+    async fn set_collider(
+        &mut self,
+        self_: wasmtime::component::Resource<HostNode>,
+        value: Option<super::bindings::wired::scene::types::Collider>,
+    ) -> wasmtime::Result<()> {
+        todo!()
+    }
+    async fn rigid_body(
+        &mut self,
+        self_: wasmtime::component::Resource<HostNode>,
+    ) -> wasmtime::Result<Option<super::bindings::wired::scene::types::RigidBodyKind>> {
+        todo!()
+    }
+    async fn set_rigid_body(
+        &mut self,
+        self_: wasmtime::component::Resource<HostNode>,
+        value: Option<super::bindings::wired::scene::types::RigidBodyKind>,
     ) -> wasmtime::Result<()> {
         todo!()
     }

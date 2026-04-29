0c0bf6eea245fd94aea4b49c790bb62da9316bb6
Kayh <basedlainpilled@gmail.com>
2026-04-29 14:33:24 -0400

    instantiate web scripts

--- crates/unavi-script/runtime.ts
+++ crates/unavi-script/runtime.ts
@@ -1,19 +1,74 @@
-import { generate, GenerateOptions } from "@bytecodealliance/jco/component";
+import { generate, GenerateOptions, Transpiled} from "@bytecodealliance/jco/component";
+import { WASIShim } from "@bytecodealliance/preview2-shim/instantiation";
 
 export async function build_script(bytes: Uint8Array, name: string): Promise<void> {
   console.log("Building script", name);
 
-  let options: GenerateOptions = {
+  const options: GenerateOptions = {
     asyncMode: { tag: "jspi", val: { imports: [], exports: [] } },
+    instantiation: { tag: "async" },
     name,
+    noNodejsCompat: true,
     noTypescript: true,
     strict: true,
   };
 
   try {
-    let result = await generate(bytes, options);
-    console.log("Built script", result);
+    const result = await (generate(bytes, options) as unknown as Promise<Transpiled>);
+    console.log("Generated script", name, result);
+
+    const jsFile = result.files.find(([name]) => name.endsWith(".js"));
+    if (jsFile == undefined) {
+      console.warn("Transpiled JS not found");
+      return;
+    }
+    const jsCode = new TextDecoder().decode(jsFile[1]);
+    const blob = new Blob([jsCode], { type: "text/javascript" });
+    const url = URL.createObjectURL(blob);
+
+    const mod = await import(url);
+
+    const fileMap = new Map(result.files);
+
+    async function getCoreModule(path: string): Promise<WebAssembly.Module> {
+      const bytes = fileMap.get(path);
+      if (!bytes) {
+        throw new Error(`Missing wasm module: ${path}`);
+      }
+      return await WebAssembly.compile(bytes as BufferSource);
+    }
+
+    const shim = new WASIShim({
+      sandbox: {
+        preopens: {},
+        env: {},
+        args: [],
+        enableNetwork: false,
+      }
+    });
+    const wasi = shim.getImportObject();
+
+    const imports = {
+      ...wasi,
+      "wired:scene/api": {
+        "self-node": () => {
+          console.log("Hello from self-node");
+        },
+      }
+    };
+
+    const instance = await mod.instantiate(
+      getCoreModule,
+      imports,
+    );
+    console.log("Instantiated script", name, instance);
+
+    const script = new instance["wired:script/guest-api"].script();
+    script.tick();
+    script.render();
+    script.drop();
   } catch (err) {
     console.error("Failed to build script", err);
   }
 }
+

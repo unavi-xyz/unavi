ab0974a1bdca1edb11b79af9059ac2ca0b6164d9
Kayh <basedlainpilled@gmail.com>
2026-05-11 13:29:19 -0400

    respawning

--- crates/unavi-client/src/scene/mod.rs
+++ crates/unavi-client/src/scene/mod.rs
@@ -7,6 +7,7 @@ use unavi_agent::LocalAgent;
 
 mod home;
 mod limbo;
+mod respawn;
 mod system_scripts;
 
 pub struct ScenePlugin;
@@ -15,12 +16,14 @@ impl Plugin for ScenePlugin {
     fn build(&self, app: &mut App) {
         app.init_state::<SceneState>()
             .add_observer(limbo::exit_limbo_on_space_join)
-            .add_systems(Startup, (spawn_sun, home::join_home))
+            .add_observer(respawn::respawn)
             .add_systems(
                 OnEnter(SceneState::Limbo),
                 (limbo::spawn_limbo, spawn_local_agent),
             )
-            .add_systems(OnExit(SceneState::Limbo), limbo::despawn_limbo);
+            .add_systems(OnExit(SceneState::Limbo), limbo::despawn_limbo)
+            .add_systems(Startup, (spawn_sun, home::join_home))
+            .add_systems(FixedUpdate, respawn::teleport_from_void);
     }
 }
 

8ff69eb7e56a1a93ea6b9db5a8d98737cbd5b74f
Kayh <basedlainpilled@gmail.com>
2026-08-22 10:33:55 -0400

    launcher optimization

--- crates/unavi-launcher/build.rs
+++ crates/unavi-launcher/build.rs
@@ -2,8 +2,4 @@ fn main() {
     println!("cargo:rerun-if-changed=../../assets/icon-nobg.png");
     std::fs::copy("../../assets/icon-nobg.png", "assets/icon-nobg.png")
         .expect("copy icon-nobg.png");
-
-    if std::env::var("CARGO_CFG_TARGET_OS").expect("CARGO_CFG_TARGET_OS not set") == "windows" {
-        let _ = embed_resource::compile("../../assets/icon-rounded.rc", embed_resource::NONE);
-    }
 }
